4b3e903b1bdc85d02c21aa66a3194d0f316f0905
Jay McCarthy <jay.mccarthy@gmail.com>
2019-09-23 21:44:22 -0400

    beauty

--- examples/rps/demo.mjs
+++ examples/rps/demo.mjs
@@ -17,13 +17,11 @@ export const makeDrawFirstHand = first => {
 const wagerInEth  = '1.5';
 const escrowInEth = '0.15';
 
-const makeDemo = async (doWhile, drawFirst) => {
-  const label = doWhile ? `[Loop]:` : `[Single]:`;
-
-  console.log(`${label} Alice initiates a new game.`);
+const makeDemo = async (theRPS, drawFirst) => {
+  console.log(`Alice initiates a new game.`);
 
   const interactWith = (name, handf) => {
-    const log = (msg) => () => { console.log(`${label} ${msg}`); return true; };
+    const log = (msg) => () => { console.log(`${msg}`); return true; };
     return { params: log(`${name} publishes parameters of game: wager of ${wagerInEth}ETH and escrow of ${escrowInEth}ETH.`),
              accepts: (wagerAmount, escrowAmount) => log(`${name} accepts the terms: wager of ${wagerAmount}WEI and escrow of ${escrowAmount}WEI.`)(),
              getHand: () => { const res = handf(); log(`(local: ${name} plays ${res}.)`)(); return res; },
@@ -31,8 +29,6 @@ const makeDemo = async (doWhile, drawFirst) => {
              shows: log(`${name} sends hand in clear.`),
              reveals: (handB) => log(`${name} reveals salt and hand, after learning B played ${handB}.`)(),
              outcome: log(`${name} agrees that game is over.`) }; };
-
-  const theRPS = doWhile ? RPSW : RPS;
 
   const shared = randomHand();
 
@@ -46,17 +42,22 @@ const makeDemo = async (doWhile, drawFirst) => {
               , interactWith('Bob', makeWhichHand())
               , wagerInEth
               , escrowInEth);
-  console.log(`${label} Alice thinks outcome is ${gs.outcomeAlice}.`);
-  console.log(`${label} Bob thinks outcome is ${gs.outcomeBob}.`);
-  console.log(`${label} Done!`);
+  console.log(`Alice thinks outcome is ${gs.outcomeAlice}.`);
+  console.log(`Bob thinks outcome is ${gs.outcomeBob}.`);
+  console.log(`Done!`);
 
   return; };
 
 ( async () => {
-  await makeDemo(false, true);
-  await makeDemo(false, false);
+  console.log(`\nRunning games that may Draw\n`);
+  await makeDemo(RPS, true);
+  await makeDemo(RPS, false);
+
+  // XXX Enable loop all of the time
   if ( process.env.RPS_WHILE ) {
-    // XXX Enable loop all of the time
-    await makeDemo(true, true); }
+    console.log(`\nRunning games that may not Draw\n`);
+    await makeDemo(RPSW, true); }
+
+  console.log(`\nAll games are complete!\n`);
   process.exit(0);
 })();
