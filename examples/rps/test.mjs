9bdede2ce73adedfa3cb04ab75189b41c21559f6
Jay McCarthy <jay.mccarthy@gmail.com>
2019-09-24 07:02:18 -0400

    sane testing

--- examples/rps/test.mjs
+++ examples/rps/test.mjs
@@ -1,16 +1,37 @@
-import Jasmine from 'jasmine';
-import JasmineConsoleReporter from 'jasmine-console-reporter';
-const jasmine = new Jasmine();
-jasmine.env.clearReporters();
+import * as RPS        from './build/rps.mjs';
+import { runGameWith } from './index.mjs';
+import { runTests, assert } from '@reach-sh/stdlib/tester.mjs';
 
-jasmine.addReporter(new JasmineConsoleReporter(
-  { colors:     false
-  , cleanStack: true
-  , verbosity:  4
-  , listStyle:  'indent'
-  , activity:   'dots'
-  }));
+const interactWithAlice =
+      ({ params: () => true,
+         getHand: () => 'SCISSORS',
+         commits: () => true,
+         reveals: (handB) => (void(handB), true),
+         outcome: () => true });
+const interactWithBob =
+      ({ accepts: (wagerAmount, escrowAmount) => (void(wagerAmount, escrowAmount), true),
+         getHand: () => 'PAPER',
+         shows: () => true,
+         outcome: () => true });
+const wagerInEth  = '1.5';
+const escrowInEth = '0.15';
 
-import('./spec/index.mjs')
-  .then(() => jasmine.execute())
-  .catch(e => console.error(e));
+runTests(async () => {
+  console.log(`Running game...`);
+  const g = await runGameWith(RPS, interactWithAlice, interactWithBob, wagerInEth, escrowInEth);
+
+  const { balanceStartAlice, balanceStartBob, balanceEndAlice, balanceEndBob } = g;
+
+  assert.deepEqual(g.outcomeAlice, g.outcomeBob,
+                   `outcomes disagree: (${g.outcomeAlice}) != (${g.outcomeBob})`);
+
+  assert.deepEqual(g.outcomeAlice, ['Alice wins']);
+
+  const [ balStartWinner, balStartLoser, balEndWinner, balEndLoser ] =
+        [ balanceStartAlice, balanceStartBob, balanceEndAlice, balanceEndBob ];
+  const winnerGte = balEndWinner.gte(balStartWinner.add(g.wagerInWei));
+  const loserLt = balEndLoser.lt(balStartLoser.sub(g.wagerInWei));
+
+  assert.ok(winnerGte, `winner'(${balEndWinner}) >= winner(${balStartWinner}) + wager(${g.wagerInWei})`);
+  assert.ok(loserLt, `loser' < loser - wager`);
+});
