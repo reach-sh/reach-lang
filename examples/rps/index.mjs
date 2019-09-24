e88d83ad3226e9576781cb1dd739e99ad3bcd914
Jay McCarthy <jay.mccarthy@gmail.com>
2019-09-24 15:34:55 -0400

    clean

--- examples/rps/index.mjs
+++ examples/rps/index.mjs
@@ -1,20 +1,50 @@
-import { connect  } from '@reach-sh/stdlib';
+import { connect } from '@reach-sh/stdlib';
+import * as RPS        from './build/rps.mjs';
+import * as RPSW       from './build/rps_while.mjs';
+
 const uri = process.env.ETH_NODE_URI || 'http://localhost:8545';
 
-export const runGameWith = async (theRPS, interactWithAlice, interactWithBob, wagerInEth, escrowInEth) => {
-  const stdlib = connect(uri);
+const randomArray = a => a[ Math.floor(Math.random() * a.length) ];
+export const randomHand = () => randomArray([ 'ROCK', 'PAPER', 'SCISSORS' ]);
 
+const onceThen = (first, after) => {
+  let called = 0;
+  return () => {
+    if (called == 2) {
+      return after();
+    } else {
+      called++;
+      return first();
+    }
+  };
+};
+
+const staticHand = (hand) => () => hand;
+
+const wagerInEth  = '1.5';
+const escrowInEth = '0.15';
+
+const demo = async (theRPS, getHand) => {
+  console.log(`Alice initiates a new game.`);
+
+  const interactWith = (name) => {
+    const log = (msg) => () => { console.log(`${msg}`); return true; };
+    return { params: log(`${name} publishes parameters of game: wager of ${wagerInEth}ETH and escrow of ${escrowInEth}ETH.`),
+             accepts: (wagerAmount, escrowAmount) => log(`${name} accepts the terms: wager of ${wagerAmount}WEI and escrow of ${escrowAmount}WEI.`)(),
+             getHand: () => { const res = getHand(); log(`(local: ${name} plays ${res}.)`)(); return res; },
+             commits: log(`${name} commits to play with (hidden) hand.`),
+             shows: log(`${name} sends hand in clear.`),
+             reveals: (handB) => log(`${name} reveals salt and hand, after learning B played ${handB}.`)(),
+             outcome: log(`${name} agrees that game is over.`) }; };
+
+  const stdlib = connect(uri);
   const wagerInWei = stdlib.toBN(stdlib.toWei(wagerInEth,  'ether'));
   const escrowInWei = stdlib.toBN(stdlib.toWei(escrowInEth, 'ether'));
 
-  const { balanceOf } = stdlib;
-
   const startingBalance = stdlib.toBN(stdlib.toWei('100', 'ether'));
-
   const alice = await stdlib.newTestAccount(startingBalance);
   const bob = await stdlib.newTestAccount(startingBalance);
-  const balanceStartAlice = await balanceOf(alice);
-  const balanceStartBob = await balanceOf(bob);
+
   const ctors = [ alice.userAddress, bob.userAddress ];
 
   const ctcAlice =
@@ -25,14 +55,27 @@ export const runGameWith = async (theRPS, interactWithAlice, interactWithBob, wa
 
   const [ outcomeBob, outcomeAlice ] =
         await Promise.all([
-          theRPS.B(ctcBob, interactWithBob),
-          theRPS.A(ctcAlice, interactWithAlice,
+          theRPS.B(ctcBob, interactWith('Bob')),
+          theRPS.A(ctcAlice, interactWith('Alice'),
                    wagerInWei, escrowInWei)]);
 
-  const balanceEndAlice = await balanceOf(alice);
-  const balanceEndBob = await balanceOf(bob);
+  console.log(`Alice thinks outcome is ${outcomeAlice}.`);
+  console.log(`Bob thinks outcome is ${outcomeBob}.`);
+  console.log(`Done!`);
 
-  return { wagerInWei, outcomeAlice, outcomeBob,
-           balanceStartAlice, balanceStartBob,
-           balanceEndAlice, balanceEndBob };
-};
+  return; };
+
+( async () => {
+  console.log(`\nRunning game that will Draw\n`);
+  await demo(RPS, staticHand('ROCK'));
+
+  console.log(`\nRunning game that may Draw\n`);
+  await demo(RPS, randomHand);
+
+  if ( process.env.RPS_WHILE ) {
+    console.log(`\nRunning game that may not Draw\n`);
+    await demo(RPSW, onceThen(staticHand('PAPER'), randomHand)); }
+
+  console.log(`\nAll games are complete!\n`);
+  process.exit(0);
+})();
--- examples/rps/test.mjs
+++ examples/rps/test.mjs
@@ -1,6 +1,7 @@
 import * as RPS        from './build/rps.mjs';
-import { runGameWith } from './index.mjs';
 import { runTests, assert } from '@reach-sh/stdlib/tester.mjs';
+import { connect } from '@reach-sh/stdlib';
+const uri = process.env.ETH_NODE_URI || 'http://localhost:8545';
 
 const log = (msg, ret = true) => () => { console.log(`...${msg}`); return ret; };
 const interactWithAlice =
@@ -19,18 +20,44 @@ const escrowInEth = '0.15';
 
 runTests(async () => {
   console.log(`Running game...`);
-  const g = await runGameWith(RPS, interactWithAlice, interactWithBob, wagerInEth, escrowInEth);
 
-  const { balanceStartAlice, balanceStartBob, balanceEndAlice, balanceEndBob } = g;
+  const stdlib = connect(uri);
 
-  assert.deepEqual(g.outcomeAlice, g.outcomeBob,
-                   `outcomes disagree: (${g.outcomeAlice}) != (${g.outcomeBob})`);
+  const wagerInWei = stdlib.toBN(stdlib.toWei(wagerInEth, 'ether'));
+  const escrowInWei = stdlib.toBN(stdlib.toWei(escrowInEth, 'ether'));
 
-  assert.deepEqual(g.outcomeAlice, ['Alice wins']);
+  const { balanceOf } = stdlib;
+
+  const startingBalance = stdlib.toBN(stdlib.toWei('100', 'ether'));
+
+  const alice = await stdlib.newTestAccount(startingBalance);
+  const bob = await stdlib.newTestAccount(startingBalance);
+  const balanceStartAlice = await balanceOf(alice);
+  const balanceStartBob = await balanceOf(bob);
+  const ctors = [ alice.userAddress, bob.userAddress ];
+
+  const ctcAlice =
+        await alice.deploy(RPS.ABI, RPS.Bytecode, ctors);
+  const ctcBob =
+        await bob.attach(RPS.ABI, ctors, ctcAlice.address,
+                         ctcAlice.creation_block);
+
+  const [ outcomeBob, outcomeAlice ] =
+        await Promise.all([
+          RPS.B(ctcBob, interactWithBob),
+          RPS.A(ctcAlice, interactWithAlice,
+                wagerInWei, escrowInWei)]);
+
+  const balanceEndAlice = await balanceOf(alice);
+  const balanceEndBob = await balanceOf(bob);
+
+  assert.deepEqual(outcomeAlice, outcomeBob, `outcomes disagree`);
+
+  assert.deepEqual(outcomeAlice, ['Alice wins']);
 
   const [ balStartWinner, balStartLoser, balEndWinner, balEndLoser ] =
         [ balanceStartAlice, balanceStartBob, balanceEndAlice, balanceEndBob ];
 
-  assert.ok(balEndWinner.gte(balStartWinner), `winner'(${balEndWinner}) >= winner(${balStartWinner}) + wager(${g.wagerInWei})`);
-  assert.ok(balEndLoser.lt(balStartLoser), `loser' < loser - wager`);
+  assert.ok(balEndWinner.gte(balStartWinner), `winner' >= winner`);
+  assert.ok(balEndLoser.lt(balStartLoser), `loser' < loser`);
 });
