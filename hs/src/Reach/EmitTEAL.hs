4bc1718bd2725a6cb2f035c776914a0884694512
Jay McCarthy <jay.mccarthy@gmail.com>
2020-03-26 14:22:16 -0400

    TEAL - Jumps and loops

--- hs/src/Reach/EmitTEAL.hs
+++ hs/src/Reach/EmitTEAL.hs
@@ -6,7 +6,7 @@ import qualified Data.Map.Strict as M
 import Data.List
 
 import Reach.AST
-import Reach.Util
+--import Reach.Util
 import Reach.EmitSol
   ( CCounts
   , usesCTail )
@@ -51,6 +51,10 @@ cs_var_loc cs bv = (loc, cs')
   where cs' = ( lab, loc + 1, M.insert bv (VR_Slot loc) vmap )
         ( lab, loc, vmap ) = cs
 
+cs_var_locs :: CompileSt a -> [BLVar] -> CompileSt a
+cs_var_locs cs bvs = foldl h cs bvs
+  where h cs0 bv = snd $ cs_var_loc cs0 bv 
+
 cs_var_set :: CompileSt a -> BLVar -> VarRHS a -> CompileSt a
 cs_var_set cs bv vr = cs'
   where cs' = ( lab, loc, M.insert bv vr vmap )
@@ -65,7 +69,7 @@ cs_var_arg cs bv i = cs_var_set cs bv (VR_Arg i)
 cs_var_args :: CompileSt a -> [ BLVar ] -> CompileSt a
 cs_var_args cs as = cs'
   where h (cs0, i) a = ((cs_var_arg cs0 a i), i+1)
-        (cs', _) = foldl h (cs, 0) as
+        (cs', _) = foldl h (cs, 1) as
 
 cs_label :: CompileSt a -> String
 cs_label ( lab, _, _ ) = lab
@@ -106,7 +110,7 @@ comp_blvar :: CompileSt a -> BLVar -> LabelM ann TEALs
 comp_blvar cs bv = 
   case M.lookup bv vmap of
     Nothing ->
-      impossible $ "unbound variable: " ++ show bv
+      return $ xxx $ "unbound variable: " ++ show bv
     Just (VR_Arg a) ->
       return $ code "arg" [ show a ]
     Just (VR_Slot s) ->
@@ -153,8 +157,12 @@ comp_cexpr cs e =
             ++ true_ls
             ++ label cont_lab
           where [ a_cond, a_true, a_false ] = as
-        BALANCE -> return $ xxx "balance"
-        TXN_VALUE -> return $ xxx "txn value"
+        BALANCE ->
+          --- FIXME Algorand ASC1 cannot inspect the balance
+          return $ comp_con $ Con_I 1
+        TXN_VALUE ->
+          --- FIXME What does this mean in Algorand?
+          return $ code "txn" [ "Amount" ]
         BYTES_EQ -> p_op "=="
      where p_op o = do
              asl <- concatMapM (comp_blarg cs) as
@@ -210,8 +218,17 @@ comp_ctail ccs cs t =
       ds_ls <- comp_cstmt cs ds
       kt_ls <- comp_ctail ccs cs kt
       return $ ds_ls ++ kt_ls
-    C_Jump _ _which _vs _ _as ->
-      return $ xxx "jump"
+    C_Jump loc which vs _ as -> do
+      args <- concatMapM (comp_blarg cs) $ (map (BL_Var loc) vs) ++ as
+      return $ args
+        ++ stack_to_slot ((length vs) + (length as)) 
+        ++ code "jump" [ "l" ++ show which ]
+      where stack_to_slot n =
+              if n == 0 then
+                []
+              else
+                code "store" [ show $ n - 1 ]
+                ++ stack_to_slot (n - 1)
 
 comp_chandler :: CHandler a -> TEALs
 comp_chandler (C_Handler _ _from_spec _is_timeout (_last_i, svs) msg _delay body i) = bracket ("Handler " ++ show i) $ label lab ++ (xxx "handler") ++ labelRun (comp_ctail (usesCTail body) cs body)
@@ -222,15 +239,15 @@ comp_chandler (C_Loop _ _svs _args _inv _body _i) = []
 
 comp_cloop :: CHandler a -> TEALs
 comp_cloop (C_Handler _ _from_spec _is_timeout _ _msg _delay _body _i) = []
-comp_cloop (C_Loop _ svs args _inv body i) = bracket ("Loop " ++ show i) $ label lab ++ (xxx "loop pre") ++ labelRun (comp_ctail (usesCTail body) cs body)
+comp_cloop (C_Loop _ svs args _inv body i) = bracket ("Loop " ++ show i) $ label lab ++ labelRun (comp_ctail (usesCTail body) cs body)
   where lab = "l" ++ show i
         cs0 = cs_init lab
-        cs = cs_var_args cs0 $ svs ++ args
+        cs = cs_var_locs cs0 $ svs ++ args
 
 cp_to_teal :: CProgram a -> TEAL
 cp_to_teal (C_Prog _ hs) = TEAL ls
   where ls = dispatch_ls ++ handlers_ls ++ loop_ls ++ standard_ls
-        dispatch_ls = bracket "Dispatcher" $ xxx "dispatcher"
+        dispatch_ls = bracket "Dispatcher" $ code "jump" [ "h1" ]
         handlers_ls = bracket "Handlers" $ concatMap comp_chandler hs
         loop_ls = bracket "Loops" $ concatMap comp_cloop hs
         standard_ls = bracket "Standard" $ revert_ls ++ halt_ls
