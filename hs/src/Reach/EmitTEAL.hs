65ba6e2e66b72c8e092f6738442f5b024359d6bf
Jay McCarthy <jay.mccarthy@gmail.com>
2020-04-30 09:57:21 -0400

    TEAL - check argument count

--- hs/src/Reach/EmitTEAL.hs
+++ hs/src/Reach/EmitTEAL.hs
@@ -338,14 +338,18 @@ comp_chandler :: String -> CHandler a -> (String, TEALs)
 comp_chandler next_lab (C_Handler loc from_spec is_timeout (last_i, svs) msg delay body i) = (lab, bracket ("Handler " ++ show i) $ label lab ++ pre_ls ++ labelRun bodym)
   where lab = "h" ++ show i
         cs0 = cs_init lab
-        --- XXX Should I check the number of arguments with txn NumAppArgs?
-        cs1 = cs_var_args cs0 $ svs ++ msg
+        all_args = svs ++ msg
+        cs1 = cs_var_args cs0 $ all_args
         cs = case from_spec of
                FS_Join from ->
                  cs_var_set cs1 from (VR_Code (code "gtxn" [ "0", "Sender" ]))
                FS_From _ -> cs1
                FS_Any -> cs1
-        pre_ls = code "txna ApplicationArgs" [ "0" ]
+        pre_ls = code "txn" [ "NumAppArgs" ]
+          ++ (comp_con $ Con_I $ fromIntegral $ length all_args)
+          ++ code "==" []
+          ++ code "bz" [ next_lab ]
+          ++ code "txna ApplicationArgs" [ "0" ]
           ++ code "btoi" []
           ++ (comp_con $ Con_I $ fromIntegral i)
           ++ code "!=" []
