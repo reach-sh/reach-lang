0a0040ca605cfca2bbd385f690fee718010578dd
Dan Burton <danburton.email@gmail.com>
2020-07-30 20:45:49 -0400

    devnet - failed attempts

--- scripts/ethereum-devnet/run.sh
+++ scripts/ethereum-devnet/run.sh
@@ -3,7 +3,22 @@
 PORT=30303
 RPCPORT=8545
 
+PREFUNDER_ACCOUNT=${PREFUNDER_ACCOUNT:?"PREFUNDER_ACCOUNT must be set"}
+PREFUNDER_ENC_KEY=${PREFUNDER_ENC_KEY:?"PREFUNDER_ENC_KEY must be set"}
+
+# PREFUNDER_AMOUNT=${PREFUNDER_AMOUNT:?"PREFUNDER_AMOUNT must be set"}
+
+# sed -i s/__PREFUNDER_ACCOUNT__/$PREFUNDER_ACCOUNT/g genesis.json
+# sed -i s/__PREFUNDER_AMOUNT__/$PREFUNDER_AMOUNT/g genesis.json
+
+# geth --dev init genesis.json
+
+mkdir -p datadir/keystore/
+echo $PREFUNDER_ENC_KEY > datadir/keystore/key.json
+
 geth --allow-insecure-unlock \
+     --datadir datadir \
+     --miner.etherbase $PREFUNDER_ACCOUNT \
      --dev \
      --dev.period=0 \
      --mine \
