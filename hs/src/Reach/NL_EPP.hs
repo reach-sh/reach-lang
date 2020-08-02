6a506f48e44e8f8ee8a5021e44fd136a4cece19b
Jay McCarthy <jay.mccarthy@gmail.com>
2020-08-02 10:16:05 -0400

    NL - epp super type man

--- hs/src/Reach/NL_EPP.hs
+++ hs/src/Reach/NL_EPP.hs
@@ -96,7 +96,9 @@ data ProRes_ a = ProRes_ Counts a
 data ProResL = ProResL (ProRes_ PLTail)
   deriving (Eq, Show)
 
-data ProResC = ProResC (M.Map SLPart (ProRes_ ETail)) (ProRes_ CTail)
+type SLPartETs = (M.Map SLPart (ProRes_ ETail))
+
+data ProResC = ProResC SLPartETs (ProRes_ CTail)
   deriving (Eq, Show)
 
 data ProSt s =
@@ -106,145 +108,13 @@ data ProSt s =
         , pst_interval :: CInterval
         , pst_parts :: [SLPart] }
   deriving (Eq)
-data ProResS = ProResS (M.Map SLPart (ProRes_ ETail)) Counts
+data ProResS = ProResS SLPartETs Counts
   deriving (Eq, Show)
 
-{-
-pro_com :: ProSt -> (ProSt -> a -> ProResS ETail) -> Bool -> LLCommon a -> Counts -> ProResS ETail
-pro_com st iter isConsensus l topk_me_cs =
-  case l of
-    LL_Return at ->
-      ProResS topk_me_cs mempty $ ET_Com $ PL_Return at
-    LL_Let at dv de k ->
-      ProResS me_cs con_cs s
-      where
-        (mde_cs, s) =
-          case get_count dv (if isConsensus then k_con_cs else k_me_cs) of
-            C_Never ->
-              case expr_pure de of
-                True -> (mempty, k')
-                False -> (de_cs, ET_Com $ PL_Eff at de k')
-            C_Once -> doLet PL_Once
-            C_Many -> doLet PL_Many
-        doLet lc = (de_cs, ET_Com $ PL_Let at lc dv de k')
-        de_cs = counts de
-        me_cs = mde_cs <> count_rms [dv] k_me_cs
-        con_cs = mde_cons_cs <> count_rms [dv] k_con_cs
-        mde_cons_cs = if isConsensus then mde_cs else mempty
-        ProResS k_me_cs k_con_cs k' = iter st k
-    LL_Var at dv k ->
-      ProResS me_cs cons_cs s'
-      where s' = ET_Com $ PL_Var at dv k'
-            ProResS k_me_cs k_cons_cs k' = iter st k
-            me_cs = count_rms [dv] k_me_cs
-            cons_cs = count_rms [dv] k_cons_cs
-    LL_Set at dv aa k ->
-      ProResS me_cs cons_cs s'
-      where s' = ET_Com $ PL_Set at dv aa k'
-            ProResS k_me_cs k_cons_cs k' = iter st k
-            me_cs = aa_cs <> k_me_cs
-            aa_cs = counts aa
-            cons_cs = maa_cons_cs <> k_cons_cs
-            maa_cons_cs = if isConsensus then me_cs else mempty
-    LL_Claim at f ct a k ->
-      case ct of
-        CT_Assert -> skip
-        CT_Possible -> skip
-        _ ->
-          ProResS me_cs cons_cs s'
-          where s' = ET_Com $ PL_Claim at f ct a k'
-                ProResS k_me_cs k_cons_cs k' = iter st k
-                me_cs = k_me_cs <> a_cs
-                a_cs = counts a
-                ma_cons_cs = if isConsensus then a_cs else mempty
-                cons_cs = k_cons_cs <> ma_cons_cs
-      where skip = iter st k
-    LL_LocalIf at ca t f k ->
-      ProResS me_cs con_cs s'
-      where
-        me_cs = counts ca <> t_me_cs <> f_me_cs <> k_me_cs
-        con_cs = t_con_cs <> f_con_cs <> k_cons_cs
-        s' = ET_Com $ PL_LocalIf at ca t' f' k'
-        ProResS t_me_cs t_con_cs t' = iter st t
-        ProResS f_me_cs f_con_cs f' = iter st f
-        ProResS k_me_cs k_cons_cs k' = iter st k
-        
-pro_con :: SLPart -> ProSt -> LLConsensus -> ProResS ETail
-pro_con me st l =
-  case l of
-    LLC_Com c -> pro_com st (pro_con me) True c mempty
-    LLC_If at ca t f -> ProResS me_cs con_cs s'
-      where
-        me_cs = counts ca <> t_me_cs <> f_me_cs
-        con_cs = t_con_cs <> f_con_cs
-        s' = ET_If at ca t' f'
-        ProResS t_me_cs t_con_cs t' = pro_con me st t
-        ProResS f_me_cs f_con_cs f' = pro_con me st f
-    LLC_FromConsensus _ _ s -> pro_s me st s
-    LLC_Transfer _ _XXX_to aa k ->
-      ProResS k_me_cs con_cs k'
-      where
-        con_cs = k_con_cs <> counts aa
-        ProResS k_me_cs k_con_cs k' = pro_con me st k
-    LLC_While _XXX_at _XXX_init _inv _XXX_cond _XXX_body _XXX_k ->
-      error "XXX"
-    LLC_Stop {} ->
-      impossible "pro_con stop"
-    LLC_Continue at da ->
-      ProResS mempty (counts da) $ ET_Continue at da
-
-pro_l :: Counts -> ProSt -> LLLocal -> ProResS ETail
-pro_l me_cs st (LLL_Com c) = pro_com st (pro_l me_cs) False c me_cs
-
-pro_s :: SLPart -> ProSt -> LLStep -> ProResS ETail
-pro_s me st s =
-  case s of
-    LLS_Com c -> pro_com st (pro_s me) False c mempty
-    LLS_Stop at da ->
-      ProResS (counts da) mempty $ ET_Stop at da
-    LLS_Only at who loc k ->
-      ProResS me_cs k_con_cs s'
-      where
-        me_cs = k_me_cs <> loc_me_cs
-        s' = ET_Seqn at loc' k'
-        ProResS k_me_cs k_con_cs k' = pro_s me st k
-        ProResS loc_me_cs _ loc' =
-          case who == me of
-            False -> ProResS mempty mempty $ ET_Com $ PL_Return at
-            True -> pro_l k_me_cs st loc
-    LLS_ToConsensus at who _XXX_fs send msg amt mtime k ->
-      ProResS me_cs con_cs s'
-      where
-        s' = if isMe then mk_amt_s' else tc_s'
-        mk_amt_s' = ET_Seqn amt_at amt_s' tc_s'
-        ProResS amt_me_cs amt_con_cs amt_s' = pro_l (counts amt_da) st amt_l
-        tc_s' = ET_ToConsensus at msend msg mtime' run_k'
-        --- sim = error $ "XXX sim "
-        LLBlock amt_at amt_l amt_da = amt
-        (time_me_cs, time_cons_cs, mtime') =
-          case mtime of
-            Nothing -> (mempty, mempty, Nothing)
-            Just (delay_a, delay_s) ->
-              (time_me_cs_, time_cons_cs_, Just (delay_a, delay_t))
-              where ProResS time_me_cs_ time_cons_cs_ delay_t = pro_s me st delay_s
-        msend = if isMe then Just (send, amt_da) else Nothing
-        isMe = who == me
-        me_cs = time_me_cs <> send_cs <> count_rms msg (k_me_cs <> (if isMe then amt_me_cs else mempty))
-        con_cs = time_cons_cs <> count_rms msg (k_con_cs <> (if isMe then amt_me_cs else mempty) <> amt_con_cs)
-        send_cs = counts msend
-        ProResS k_me_cs k_con_cs run_k' = pro_con me st k
-
-project :: SLPart -> LLStep -> ETail
-project me s = et
-  where ProResS _ _ et = pro_s me st s
-        st = ProSt ()
-
-contract :: LLStep -> (Seq.Seq CHandler)
-contract _s = error "XXX"
--}
+type MBack a res = (Counts -> (forall c . c -> PLCommon c) -> a -> res)
 
 epp_m ::
-  (Counts -> (c -> PLCommon c) -> a -> res)
+  MBack a res
   -> (a -> res)
   -> (a -> ((Counts -> PLCommon d -> lookres) -> (Counts -> d -> lookres) -> Counts -> d -> lookres) -> res)
   -> LLCommon a
@@ -273,13 +143,32 @@ epp_m back skip look c =
           where cs' = counts ca
     LL_LocalIf {} -> error "XXX"
 
-epp_l :: LLLocal -> Counts -> ST s ProResL
-epp_l (LLL_Com com) _XXX_cs = epp_m (error "XXX back") (error "XXX skip") (error "XXX look") com
+epp_l :: forall s . LLLocal -> Counts -> ST s ProResL
+epp_l (LLL_Com com) _XXX_cs = epp_m back (error "XXX skip") (error "XXX look") com
+  where back :: MBack LLLocal (ST s ProResL)
+        back _XXX_cs' _XXX_mkpl _XXX_k = (error "XXX back")
 
-epp_n :: ProSt s -> LLConsensus -> ST s ProResC
+extend_locals :: Counts -> (forall c . c -> PLCommon c) -> SLPartETs -> SLPartETs
+extend_locals cs' mkpl p_prts_s = M.map add p_prts_s
+  where add :: ProRes_ ETail -> ProRes_ ETail
+        add (ProRes_ p_cs p_et) =
+          ProRes_ (cs' <> p_cs) (ET_Com $ mkpl p_et)
+
+epp_n :: forall s . ProSt s -> LLConsensus -> ST s ProResC
 epp_n st n =
   case n of
-    LLC_Com c -> epp_m (error "XXX back") (error "XXX skip") (error "XXX look") c
+    LLC_Com c ->
+      epp_m back skip look c
+      where back :: MBack LLConsensus (ST s ProResC)
+            back cs' mkpl k = do
+              ProResC p_prts_s (ProRes_ cs_k ct_k) <- skip k
+              let p_prts_s' = extend_locals cs' mkpl p_prts_s
+              let cs_k' = cs' <> cs_k
+              let ct_k' = CT_Com $ mkpl ct_k
+              return $ ProResC p_prts_s' (ProRes_ cs_k' ct_k')
+            skip k = epp_n st k
+            look _XXX_k _XXX_common =
+              error "XXX"
     LLC_If {} -> error "XXX"
     LLC_Transfer {} -> error "XXX"
     LLC_FromConsensus at1 _at2 s -> do
@@ -291,16 +180,15 @@ epp_n st n =
     LLC_Stop {} -> error "XXX"
     LLC_Continue {} -> error "XXX"
 
-epp_s :: ProSt s -> LLStep -> ST s ProResS
+epp_s :: forall s . ProSt s -> LLStep -> ST s ProResS
 epp_s st s =
   case s of
     LLS_Com c ->
       epp_m back skip look c
-      where back cs' mkpl k = do
+      where back :: MBack LLStep (ST s ProResS)
+            back cs' mkpl k = do
               ProResS p_prts_s cr <- skip k
-              let add (ProRes_ p_cs p_et) =
-                    ProRes_ (cs' <> p_cs) (ET_Com $ mkpl p_et)
-              let p_prts_s' = M.map add p_prts_s
+              let p_prts_s' = extend_locals cs' mkpl p_prts_s
               return $ ProResS p_prts_s' cr
             skip k = epp_s st k
             look k common = do
