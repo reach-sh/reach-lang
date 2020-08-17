a41570375adb9604eeab99aa3b937ecf26cf55fa
Jay McCarthy <jay.mccarthy@gmail.com>
2020-08-17 15:34:33 -0400

    17 works

--- hs/src/Reach/Connector/ETH_Solidity.hs
+++ hs/src/Reach/Connector/ETH_Solidity.hs
@@ -46,7 +46,7 @@ solContract :: String -> Doc a -> Doc a
 solContract s body = "contract" <+> pretty s <+> solBraces body
 
 solVersion :: Doc a
-solVersion = "pragma solidity ^0.5.16;"
+solVersion = "pragma solidity ^0.5.17;"
 
 solStdLib :: Doc a
 solStdLib = pretty $ B.unpack stdlib_sol
