7537fcb47faa2ef7dabcbdd97bd09f5d3b7d9280
Dan Burton <danburton.email@gmail.com>
2020-08-19 13:31:53 -0400

    docs tests - no need for subdirs

--- hs/test/Reach/Test_Compiler.hs
+++ hs/test/Reach/Test_Compiler.hs
@@ -1,6 +1,5 @@
 module Reach.Test_Compiler
-  ( test_docs_compiles
-  , test_docs_errors
+  ( test_docs
   , test_language_non_features
   , test_language_features
   )
@@ -31,24 +30,31 @@ testCompile fp = capture $
         , intermediateFiles = False
         }
 
+-- Left = compile fail, Right = compile success
+testCompileOut :: FilePath -> IO (Either BL.ByteString BL.ByteString)
+testCompileOut fp =
+  testCompile fp >>= \case
+    (s, Left e) -> return $ Left $ BL.fromStrict $ bpack $ s <> "\n\nFailed with: " <> show e
+    (s, Right ()) -> return $ Right $ BL.fromStrict $ bpack $ s
+
 testCompileExpectFail :: FilePath -> IO BL.ByteString
 testCompileExpectFail fp =
-  testCompile fp >>= \case
-    (s, Right ()) ->
-      fail $
-        "Expected failure, but did not fail."
-          <> ("stdout: \n" <> s)
-    (s, Left e) -> return $ BL.fromStrict $ bpack $ s <> "\n\nFailed with: " <> show e
+  testCompileOut fp >>= \case
+    (Left s) -> return s
+    (Right s) ->
+      fail . bunpack . BL.toStrict $
+        "Expected failure, but did not fail.\n" <> s
 
 testCompileExpectSuccess :: FilePath -> IO BL.ByteString
 testCompileExpectSuccess fp =
-  testCompile fp >>= \case
-    (s, Left e) ->
-      fail $
-        "Expected success, but failed.\n"
-          <> ("stdout: \n" <> s)
-          <> ("\n\nFailed with: " <> show e)
-    (s, Right ()) -> return $ BL.fromStrict $ bpack $ s
+  testCompileOut fp >>= \case
+    (Left s) ->
+      fail . bunpack . BL.toStrict $
+        "Expected success, but failed.\n" <> s
+    (Right s) -> return s
+
+testCompileExpectAny :: FilePath -> IO BL.ByteString
+testCompileExpectAny fp = either id id <$> testCompileOut fp
 
 compileTestFail :: FilePath -> TestTree
 compileTestFail = stdoutStripAbs ".txt" testCompileExpectFail
@@ -56,14 +62,14 @@ compileTestFail = stdoutStripAbs ".txt" testCompileExpectFail
 compileTestSuccess :: FilePath -> TestTree
 compileTestSuccess = stdoutStripAbs ".txt" testCompileExpectSuccess
 
+compileTestAny :: FilePath -> TestTree
+compileTestAny = stdoutStripAbs ".txt" testCompileExpectAny
+
 test_language_features :: IO TestTree
 test_language_features = goldenTests compileTestSuccess ".rsh" "features"
 
 test_language_non_features :: IO TestTree
 test_language_non_features = goldenTests compileTestFail ".rsh" "non-features"
 
-test_docs_compiles :: IO TestTree
-test_docs_compiles = goldenTests compileTestSuccess ".rsh" "../../docs-src/x/compiles"
-
-test_docs_errors :: IO TestTree
-test_docs_errors = goldenTests compileTestFail ".rsh" "../../docs-src/x/errors"
+test_docs :: IO TestTree
+test_docs = goldenTests compileTestAny ".rsh" "../../docs-src/x"
