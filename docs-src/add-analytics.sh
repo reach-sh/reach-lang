fc756680054c0109fb93d77ed13180137c449893
Dan Burton <danburton.email@gmail.com>
2020-08-31 18:23:30 -0400

    add-analytics script portability

--- docs-src/add-analytics.sh
+++ docs-src/add-analytics.sh
@@ -4,5 +4,8 @@ ANALYTICS=$(cat analytics.html)
 
 find ../docs -name '*.html' |
     while read -r HTML ; do
-        sed -i '' -e "s#<head>#<head>${ANALYTICS}#" "$HTML"
+        # Portable on mac & linux which have different -i behavior
+        # https://stackoverflow.com/a/22084103/208257
+        sed -i.bak -e "s#<head>#<head>${ANALYTICS}#" "$HTML"
+        rm "$HTML".bak
     done
