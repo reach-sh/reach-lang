0a8790a8d12809578e219f8da57d7a2c0334f6ff
Dan Burton <danburton.email@gmail.com>
2020-08-30 22:04:02 -0400

    tagpush latest

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -6,14 +6,15 @@ HERE=$(dirname "$0")
 IMAGE="$1"
 LATEST_TAG="${IMAGE}:latest"
 
-docker push "${LATEST_TAG}"
 
 tagpush() {
     docker tag "${LATEST_TAG}" "${IMAGE}:$1"
-    if ! [ "x${TAG_ONLY}" = "x1" ] ; then        docker push "${IMAGE}:$1"
+    if ! [ "x${TAG_ONLY}" = "x1" ] ; then
+      docker push "${IMAGE}:$1"
     fi
 }
 
+tagpush "latest"
 tagpush "${MAJOR}.${MINOR}.${PATCH}"
 tagpush "${MAJOR}.${MINOR}"
 tagpush "${MAJOR}"
