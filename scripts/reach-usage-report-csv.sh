f2769b26cb85caa49f0a399164125990bf0f1ac3
Dan Burton <danburton.email@gmail.com>
2020-09-14 22:02:59 -0400

    UP_TO arg for usage report

--- scripts/reach-usage-report-csv.sh
+++ scripts/reach-usage-report-csv.sh
@@ -3,13 +3,21 @@
 # Requires curl, awscli, and jq
 # usage: ./reach-usage-report-csv.sh
 
+# If you specify the optional argument,
+# it MUST be in the form YYYY-MM-DD
+
 HERE=$(dirname "$(realpath "$0")")
 REPORT_FILE='report.csv'
+UP_TO="$1"
 
 # https://docs.docker.com/registry/spec/api/
 
-UsageReport=$("$HERE/reach-usage-report.sh")
+UsageReport=$("$HERE/reach-usage-report.sh" "$UP_TO")
 
 echo 'report_date,pull_count,row_count,unique_users' > $REPORT_FILE
-echo "$UsageReport" | jq '[.report_date, .repository_stats.pull_count, .CompileLog.row_count, .CompileLog.unique_users] | @csv' -r >> $REPORT_FILE
+if [ "x$UP_TO" = "x" ] ; then
+  echo "$UsageReport" | jq -r '[.report_date, .repository_stats.pull_count, .CompileLog.row_count, .CompileLog.unique_users] | @csv' >> $REPORT_FILE
+else
+  echo "$UsageReport" | jq -r '["'"${UP_TO}"'", "n/a", "n/a", .CompileLog.unique_users] | @csv' > $REPORT_FILE
+fi
 echo "Wrote report to $REPORT_FILE"
--- scripts/reach-usage-report.sh
+++ scripts/reach-usage-report.sh
@@ -6,14 +6,20 @@
 # https://docs.docker.com/registry/spec/api/
 
 HERE="$(dirname "$(realpath "${0}")")";
+UP_TO="$1"
 
 # shellcheck source=../VERSION
 . "${HERE}/../VERSION"
 
 echo '{'
 
+if [ "x$UP_TO" = "x" ] ; then
+  DATE="$(TZ=Z date +%FT%TZ)"
+else
+  DATE="$UP_TO"
+fi
 echo '  "report_date":'
-echo "\"$(TZ=Z date +%FT%TZ)\""
+echo "\"$DATE\""
 
 echo ', "repository_stats":'
 curl -s https://hub.docker.com/v2/repositories/reachsh/reach/ | \
@@ -39,8 +45,14 @@ aws dynamodb scan \
     --table-name CompileLog \
     --consistent-read \
     --select SPECIFIC_ATTRIBUTES \
-    --projection-expression userId
+    --projection-expression userId,startTime
 )
-echo "$CompileLog" | jq '{row_count: .Count, unique_users: (.Items | map(.userId.S) | unique | length)}'
+
+if [ "x$UP_TO" = "x" ] ; then
+  echo "$CompileLog" | jq '{row_count: .Count, unique_users: (.Items | map(.userId.S) | unique | length)}'
+else
+  UP_TO_T="${UP_TO}T23:59:59.999999999Z"
+  echo "$CompileLog" | jq '{row_count: .Count, unique_users: (.Items | map(select(.startTime.S <= "'"$UP_TO_T"'")) | map(.userId.S) | unique | length)}'
+fi
 
 echo '}'
