be379a19e6bbf08ab5be96dc421e2e821576bae6
Dan Burton <danburton.email@gmail.com>
2020-09-18 19:41:36 -0400

    js: port ask.mjs to .ts

--- js/ask.ts
+++ js/ask.ts
@@ -1,24 +1,27 @@
 import * as readline from 'readline';
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-const ask_ = async (q) => {
+
+const ask_ = async (q: string): Promise<string> => {
   return new Promise((resolve) => {
     rl.question(q + '\n', (ans) => {
       resolve(ans);
     });
   });
 };
+
 // The validator arg should:
 // * return anything but `undefined` on success.
 // * throw an Error on failure
 // The validator's output will be returned.
-export const ask = async (question, validator) => {
+export const ask = async <T>(question: string, validator?: ((s: string) => T)): Promise<T> => {
   // Not sure how to require T=string if validator is undefined.
   // This is not type safe.
   // @ts-ignore
-  const validator_ = validator || ((x) => x);
+  const validator_: ((s: string) => T) = validator || ((x: string) => x);
   let result = undefined;
   do {
     try {
@@ -31,11 +34,13 @@ export const ask = async (question, validator) => {
   } while (result === undefined);
   return result;
 };
-export const done = () => {
+
+export const done = (): void => {
   rl.close();
 };
+
 // The answer arg be 'y' (true) or 'n' (false)
-export const yesno = (answer) => {
+export const yesno = (answer: string): boolean => {
   if (answer === 'y') {
     return true;
   } else if (answer === 'n') {
