e25c918f4b71da5d8220d5512f3cbe58ab39c868
Dan Burton <danburton.email@gmail.com>
2020-09-18 19:40:00 -0400

    js: misc trivialities

--- js/sbin/fix_imports.sh
+++ js/sbin/fix_imports.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
 
+# Rewrite `from './blah'` to `from './blah.mjs'`
+# Usage: sbin/fix_imports.sh
+
 for FILE in *.mjs ; do
   for IMJS in *.mjs ; do
-    I="${IMJS%.*}"
+    I="${IMJS%.*}" # strip the .mjs suffix
     sed -i.bak "s#from './$I'#from './$I.mjs'#" "$FILE"
     rm "$FILE.bak"
   done
