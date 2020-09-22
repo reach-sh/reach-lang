ec045b39a626b9a412de4710c114926e08faf532
Dan Burton <danburton.email@gmail.com>
2020-09-22 14:44:59 -0400

    examples/overview: finish converting to parseCurrency

--- examples/overview/index.mjs
+++ examples/overview/index.mjs
@@ -2,15 +2,15 @@ import * as stdlib from '@reach-sh/stdlib/ETH.mjs';
 import * as backend from './build/index.main.mjs';
 
 (async () => {
-  const accAlice = await stdlib.newTestAccount(toNetworkCurrency(5));
-  const accBob = await stdlib.newTestAccount(toNetworkCurrency(10));
+  const accAlice = await stdlib.newTestAccount(stdlib.parseCurrency(5));
+  const accBob = await stdlib.newTestAccount(stdlib.parseCurrency(10));
 
   const ctcAlice = await accAlice.deploy(backend);
   const ctcBob = await accBob.attach(backend, ctcAlice);
 
   await Promise.all([
     backend.Alice(stdlib, ctcAlice, {
-      request: toNetworkCurrency(5),
+      request: stdlib.parseCurrency(5),
       info: 'If you wear these, you can see beyond evil illusions.'
     }),
     backend.Bob(stdlib, ctcBob, {
