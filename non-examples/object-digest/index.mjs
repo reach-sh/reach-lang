84ec4e05d873df54934540d902c59da975eba250
Dan Burton <danburton.email@gmail.com>
2020-10-27 13:54:53 -0400

    non-examples/object-digest

---
+++ non-examples/object-digest/index.mjs
@@ -0,0 +1,32 @@
+import {loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+(async () => {
+  const stdlib = await loadStdlib();
+  const startingBalance = stdlib.parseCurrency(100);
+
+  const alice = await stdlib.newTestAccount(startingBalance);
+  const bob = await stdlib.newTestAccount(startingBalance);
+
+  const ctcAlice = alice.deploy(backend);
+  const ctcBob = bob.attach(backend, ctcAlice.getInfo());
+
+  await Promise.all([
+    backend.Alice(stdlib, ctcAlice, {
+      ...stdlib.hasRandom,
+      getObj: () => {
+        console.log('Alice getObj');
+        return {y: 'hello', x: 3}
+      },
+    }),
+    backend.Bob(stdlib, ctcBob, {
+      ...stdlib.hasRandom,
+      showObj: (obj, objHash, salt) => {
+        console.log('Bob showObj');
+        console.log({obj, objHash, salt});
+      }
+    }),
+  ]);
+
+  console.log('Hello, Alice and Bob!');
+})();
---
+++ non-examples/object-digest/index.rsh
@@ -0,0 +1,27 @@
+'reach 0.1';
+
+const Obj = Object({x: UInt, y: Bytes});
+
+export const main = Reach.App(
+  {}, [
+    ['Alice', {
+      ...hasRandom,
+      getObj: Fun([], Obj),
+    }],
+    ['Bob', {
+      showObj: Fun([Obj, Digest, UInt], Null),
+    }],
+  ], (Alice, Bob) => {
+    Alice.only(() => {
+      const obj = declassify(interact.getObj());
+      const commitment = declassify(makeCommitment(interact, obj));
+    });
+    Alice.publish(obj, commitment);
+    checkCommitment(...commitment, obj);
+    commit();
+
+    Bob.only(() => {
+      interact.showObj(obj, ...commitment);
+    });
+  }
+);
