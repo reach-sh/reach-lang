38f565b8a4fc664a0d69ccbe285427fd2a594db0
Dan Burton <danburton.email@gmail.com>
2020-10-28 18:05:15 -0400

    react-examples: reactified tut-7

---
+++ react-examples/tut-7/index.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import AppViews from './views/AppViews';
+import DeployerViews from './views/DeployerViews';
+import AttacherViews from './views/AttacherViews';
+import {renderDOM, renderView} from './views/render';
+import './index.css';
+import * as backend from './build/index.main.mjs';
+import * as reach from '@reach-sh/stdlib/ETH';
+
+const handToInt = {'ROCK': 0, 'PAPER': 1, 'SCISSORS': 2};
+const intToOutcome = ['Bob wins!', 'Draw!', 'Alice wins!'];
+const {standardUnit} = reach;
+const defaults = {defaultFundAmt: '10', defaultWager: '3', standardUnit};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {view: 'ConnectAccount', ...defaults};
+  }
+  async componentDidMount() {
+    const acc = await reach.getDefaultAccount();
+    const balAtomic = await reach.balanceOf(acc);
+    const bal = reach.formatCurrency(balAtomic, 4);
+    this.setState({acc, bal});
+    try {
+      const faucet = await reach.getFaucet();
+      this.setState({view: 'FundAccount', faucet});
+    } catch (e) {
+      this.setState({view: 'DeployerOrAttacher'});
+    }
+  }
+  async fundAccount(fundAmount) {
+    const {acc, faucet} = this.state;
+    const fundAmountAtomic = reach.parseCurrency(fundAmount);
+    await reach.transfer(faucet, acc, fundAmountAtomic);
+    this.setState({view: 'DeployerOrAttacher'});
+  }
+  async skipFundAccount() {
+    this.setState({view: 'DeployerOrAttacher'});
+  }
+  selectAttacher() {
+    this.setState({view: 'Wrapper', ContentView: Attacher});
+  }
+  selectDeployer() {
+    this.setState({view: 'Wrapper', ContentView: Deployer});
+  }
+  render() {
+    return renderView(this, AppViews);
+  }
+}
+
+class Player extends React.Component {
+  playHand(hand) {
+    this.state.resolveHandP(hand);
+  }
+  random() { return reach.hasRandom.random(); }
+  async getHand() { // Fun([], UInt)
+    console.log('XXX: getHand');
+    const hand = await new Promise(resolveHandP => {
+      this.setState({view: 'GetHand', playable: true, resolveHandP});
+    });
+    this.setState({view: 'WaitingForResults', hand});
+    return handToInt[hand];
+  }
+  seeOutcome(outcomeInt) { // Fun([UInt], Null)
+    console.log('XXX: seeOutcome');
+    const outcome = intToOutcome[outcomeInt];
+    console.log(outcome); // XXX
+    this.setState({view: 'Done', outcome});
+  }
+  informTimeout() { // Fun([], Null)
+    console.log('XXX: informTimeout');
+    this.setState({view: 'Timeout'});
+  }
+}
+
+class Deployer extends Player {
+  constructor(props) {
+    super(props);
+    this.state = {view: 'SetWager'};
+  }
+  setWager(wager) {
+    this.setState({view: 'Deploy', wager});
+  }
+  async deploy() {
+    const ctc = this.props.acc.deploy(backend);
+    this.setState({view: 'Deploying', ctc});
+    this.wager = reach.parseCurrency(this.state.wager); // UInt
+    backend.Alice(reach, ctc, this);
+    const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);
+    this.setState({view: 'WaitingForAttacher', ctcInfoStr});
+  }
+  render() {
+    return renderView(this, DeployerViews);
+  }
+}
+
+class Attacher extends Player {
+  constructor(props) {
+    super(props);
+    this.state = {view: 'Attach'};
+  }
+  attach(ctcInfoStr) {
+    const ctcInfo = JSON.parse(ctcInfoStr);
+    const {acc} = this.props;
+    const ctc = acc.attach(backend, ctcInfo);
+    this.setState({view: 'Attaching'});
+    backend.Bob(reach, ctc, this);
+  }
+  termsAccepted() {
+    this.state.resolveAcceptedP();
+    this.setState({view: 'WaitingForTurn'});
+  }
+  async acceptWager(wagerAtomic) { // Fun([UInt], Null)
+    console.log('XXX acceptWager');
+    const wager = reach.formatCurrency(wagerAtomic, 4);
+    console.log({wager});
+
+    return await new Promise(resolveAcceptedP => {
+      this.setState({view: 'AcceptTerms', wager, resolveAcceptedP});
+    });
+  }
+  // shows: Fun([], Null)
+  shows() { console.log('XXX shows'); }
+  render() {
+    return renderView(this, AttacherViews);
+  }
+}
+
+renderDOM(<App />);
---
+++ react-examples/tut-7/views/PlayerViews.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const exports = {};
+
+// Player views must be extended.
+// It does not have its own Wrapper view.
+
+exports.Done = class extends React.Component {
+  render() {
+    const {outcome} = this.props;
+    return (
+      <div>
+        Thank you for playing. The outcome of this game was:
+        <br />{outcome || 'Unknown'}
+      </div>
+    );
+  }
+}
+
+exports.WaitingForTurn = class extends React.Component {
+  render() {
+    return (
+      <div>
+        Waiting for the other player...
+        <br />Think about which move you want to play.
+      </div>
+    );
+  }
+}
+
+exports.GetHand = class extends React.Component {
+  render() {
+    const {parent, playable, hand} = this.props;
+    return (
+      <div>
+        {hand ? 'It was a draw! Pick again.' : ''}
+        <br />
+        {!playable ? 'Please wait...' : ''}
+        <br />
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand('ROCK')}
+        >Rock</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand('PAPER')}
+        >Paper</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand('SCISSORS')}
+        >Scissors</button>
+      </div>
+    );
+  }
+}
+
+exports.WaitingForResults = class extends React.Component {
+  render() {
+    return (
+      <div>
+        Waiting for results...
+      </div>
+    );
+  }
+}
+
+export default exports;
