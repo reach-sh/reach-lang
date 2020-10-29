eb8640196ec64d95c750fcdf97fd88371b116051
Dan Burton <danburton.email@gmail.com>
2020-10-28 20:04:51 -0400

    react-examples: rename to tut-8

--- react-examples/diff-all.sh
+++ react-examples/diff-all.sh
@@ -11,6 +11,10 @@ EXAMPLES=$(find . -mindepth 1 -maxdepth 1 -type d | sort)
 for e in $EXAMPLES ; do
   FILE="$e/index.rsh"
   echo "======== $FILE ========"
-  diff "../examples/$FILE" "$FILE" || :
+  OTHER="../examples/$FILE"
+  if [ "$e" = "./tut-8" ] ; then
+    OTHER="../examples/tut-7/index.rsh"
+  fi
+  diff "$OTHER" "$FILE" || :
 done
 
