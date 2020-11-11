3184f39b6da6d245303bcce42cb7a82db9e16c9c
Dan Burton <danburton.email@gmail.com>
2020-11-11 16:30:14 -0500

    tut-7/check-offchain.sh: update

--- examples/tut-7/check-offchain.sh
+++ examples/tut-7/check-offchain.sh
@@ -2,10 +2,10 @@
 WHO="$1"
 export REACH_CONNECTOR_MODE=ETH-live
 export REACH_DEBUG=1
-export ETH_NODE_URI=https://node.offchainlabs.com:8547
+export ETH_NODE_URI=https://kovan2.arbitrum.io/rpc
 
 DC=docker-compose.offchain.yml
-sed -e '31s/ &default-app//' -e '24s/:/: \&default-app/' docker-compose.yml > "${DC}"
+sed -e '32s/ &default-app//' -e '25s/:/: \&default-app/' docker-compose.yml > "${DC}"
 
 make build || exit 1
 
