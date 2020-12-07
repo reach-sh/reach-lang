0ef1680cfbdbd50b683460941f7d9cc1d09df68b
Chris Nevers <christophernevers96@gmail.com>
2020-12-07 13:34:09 -0500

    Revert debug changes

--- examples/tut-1/index.mjs
+++ examples/tut-1/index.mjs
@@ -8,30 +8,17 @@ import * as backend from './build/index.main.mjs';
   const accAlice = await stdlib.newTestAccount(startingBalance);
   const accBob = await stdlib.newTestAccount(startingBalance);
 
-  console.log(`accAlice is using stdlib: ${accAlice.stdlibT}`);
-  console.log(`accBob is using stdlib: ${accBob.stdlibT}`);
-
   const ctcAlice = accAlice.deploy(backend);
   const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
-
-  console.log(`ctcAlice is using stdlib: ${ctcAlice.stdlibT}`);
-  console.log(`ctcBob is using stdlib: ${ctcBob.stdlibT}`);
-
 
   await Promise.all([
     backend.Alice(
       ctcAlice,
-      {
-        x: 5
-      },
+      {},
     ),
     backend.Bob(
       ctcBob,
-      {
-        x: 10
-      },
+      {},
     ),
   ]);
-
-  console.log('Done');
 })(); // <-- Don't forget these!
--- examples/tut-1/index.rsh
+++ examples/tut-1/index.rsh
@@ -3,19 +3,6 @@
 export const main =
   Reach.App(
     {},
-    [['Alice', { x: UInt }], ['Bob', { x: UInt }]],
+    [['Alice', {}], ['Bob', {}]],
     (A, B) => {
-      A.only(() => {
-        const ax = declassify(interact.x);
-      });
-      A.publish(ax);
-      commit();
-
-      B.only(() => {
-        const bx = declassify(interact.x);
-        const bi = 45 + bx;
-      });
-      B.publish(bi);
-      commit();
-
       exit(); });
