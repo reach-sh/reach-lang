4f71f9946485f28408067ee092e4a04ffab4b527
Jay McCarthy <jay.mccarthy@gmail.com>
2021-02-01 13:42:36 -0500

    subsume type

--- hs/src/Reach/Eval.hs
+++ hs/src/Reach/Eval.hs
@@ -1,5 +1,6 @@
 module Reach.Eval (EvalError, compileBundle) where
 
+import Control.Applicative
 import Control.Arrow (second)
 import Control.Monad
 import Data.Bits
@@ -28,7 +29,6 @@ import Reach.JSUtil
 import Reach.Parser
 import Reach.Counter
 import Reach.Texty (pretty)
-import Reach.Type
 import Reach.Util
 import Reach.Version
 import Safe (atMay)
@@ -125,6 +125,13 @@ data EvalError
   | Err_Fork_ConsensusBadArrow JSExpression
   | Err_Fork_CaseAppearsTwice SLPart SrcLoc SrcLoc
   | Err_ParallelReduceIncomplete String
+  | Err_Type_None SLVal
+  | Err_Type_NotDT SLType
+  | Err_Type_NotApplicable SLType
+  | Err_TypeMeets_None
+  | Err_TypeMeets_Mismatch SrcLoc (SrcLoc, SLType) (SrcLoc, SLType)
+  | Err_Type_TooFewArguments [SLType]
+  | Err_Type_TooManyArguments [SLVal]
   deriving (Eq, Generic)
 
 --- FIXME I think most of these things should be in Pretty
@@ -440,9 +447,22 @@ instance Show EvalError where
       "fork cases must be unique: " <> show who <> " was defined previously at " <> show at0
     Err_ParallelReduceIncomplete lab ->
       "parallel reduce incomplete: " <> lab
+    Err_Type_None val ->
+      "TypeError: Value cannot exist at runtime: " <> show (pretty val)
+    Err_Type_NotDT t ->
+      "TypeError: Value of this type cannot exist at runtime: " <> show (pretty t)
+    Err_Type_NotApplicable ty ->
+      "TypeError: NotApplicable. Cannot apply this like a function: " <> show ty
+    Err_TypeMeets_None ->
+      "TypeError: TypeMeets_None. Cannot find the meet of []"
+    Err_TypeMeets_Mismatch _at t1 t2 ->
+      "TypeError: TypeMeets_Mismatch. These types are mismatched: " <> (show t1 <> " vs " <> show t2)
+    Err_Type_TooFewArguments ts ->
+      "TypeError: TooFewArguments. Expected: " <> show ts
+    Err_Type_TooManyArguments vs ->
+      "TypeError: TooManyArguments. Surplus: " <> show (length vs)
     where
       displayPrim = drop (length ("SLPrim_" :: String)) . conNameOf
-
 
 --- Utilities
 zipEq :: (Show e, ErrorMessageForJson e, ErrorSuggestions e) => Maybe SLCtxt -> SrcLoc -> (Int -> Int -> e) -> [a] -> [b] -> [(a, b)]
@@ -645,26 +665,203 @@ data SLCtxt = SLCtxt
 instance Show SLCtxt where
   show _ = "<context>"
 
-mcfs :: SLCtxt -> Maybe [SLCtxtFrame]
-mcfs = Just . ctxt_stack
+type MCFS = Maybe [SLCtxtFrame]
 
-tint :: SLCtxt -> SLState -> (Maybe [SLCtxtFrame], M.Map SLPart DLVar)
-tint ctxt st = (mcfs ctxt, st_pdvs st)
+type PDVS = M.Map SLPart DLVar
+
+type TINT = (MCFS, PDVS)
+
+typeEqual :: SrcLoc -> (SrcLoc, SLType) -> (SrcLoc, SLType) -> Either EvalError SLType
+typeEqual top_at x@(_, xt) y@(_, yt) =
+  case xt == yt of
+    True -> Right xt
+    False -> Left $ Err_TypeMeets_Mismatch top_at x y
+
+typeMeet :: HasCallStack => MCFS -> SrcLoc -> (SrcLoc, SLType) -> (SrcLoc, SLType) -> SLType
+typeMeet _ _ (_, ST_Bytes xz) (_, ST_Bytes yz) =
+  ST_Bytes (max xz yz)
+typeMeet mcfs top_at x@(_, xt) y =
+  --- FIXME Find meet of objects
+  case typeEqual top_at x y of
+    Right _ -> xt
+    Left err -> expect_throw mcfs top_at err
+
+typeMeets :: HasCallStack => MCFS -> SrcLoc -> [(SrcLoc, SLType)] -> SLType
+typeMeets mcfs top_at l =
+  case l of
+    [] ->
+      expect_throw mcfs top_at $ Err_TypeMeets_None
+    [(_, xt)] -> xt
+    [x, y] -> typeMeet mcfs top_at x y
+    x : more -> typeMeet mcfs top_at x $ (top_at, typeMeets mcfs top_at more)
+
+type TypeEnv = M.Map SLVar (IORef (Maybe SLType))
+
+typeSubst :: SrcLoc -> TypeEnv -> SLType -> IO SLType
+typeSubst at env ty =
+  case ty of
+    ST_Fun doms rng -> do
+      doms' <- mapM iter doms
+      rng' <- typeSubst at env rng
+      return $ ST_Fun doms' rng'
+    ST_Array t sz -> do
+      t' <- iter t
+      return $ ST_Array t' sz
+    ST_Tuple ts -> do
+      ts' <- mapM iter ts
+      return $ ST_Tuple ts'
+    ST_Object oenv -> do
+      oenv' <- mapM iter oenv
+      return $ ST_Object oenv'
+    ST_Var var ->
+      case M.lookup var env of
+        Nothing ->
+          impossible $ "typeSubst: unbound type variable"
+        Just var_ref -> do
+          mvar <- readIORef var_ref
+          case mvar of
+            Nothing ->
+              impossible $ "typeSubst: uninstantiated type variable"
+            Just var_ty ->
+              iter var_ty
+    ST_Forall _ _ ->
+      impossible $ "typeSubst: forall in output"
+    _ -> return ty
+  where
+    iter = typeSubst at env
+
+typeCheck_help :: HasCallStack => MCFS -> SrcLoc -> TypeEnv -> SLType -> SLVal -> SLType -> a -> IO a
+typeCheck_help mcfs at env ty val val_ty res =
+  case (val_ty, ty) of
+    (ST_Var _, _) ->
+      impossible $ "typeCheck: value has type var: " ++ show val
+    (_, ST_Var var) ->
+      case M.lookup var env of
+        Nothing ->
+          impossible $ "typeCheck: unbound type variable"
+        Just var_ref -> do
+          mvar_ty <- readIORef var_ref
+          case mvar_ty of
+            Nothing -> do
+              writeIORef var_ref (Just val_ty)
+              return res
+            Just var_ty ->
+              typeCheck_help mcfs at env var_ty val val_ty res
+    (_, _) ->
+      typeMeet mcfs at (at, val_ty) (at, ty) `seq` return res
+
+slToDL :: HasCallStack => PDVS -> SrcLoc -> SLVal -> Maybe DLArgExpr
+slToDL pdvs _at v =
+  case v of
+    SLV_Null _ _ -> return $ DLAE_Arg $ DLA_Literal $ DLL_Null
+    SLV_Bool _ b -> return $ DLAE_Arg $ DLA_Literal $ DLL_Bool b
+    SLV_Int at i -> return $ DLAE_Arg $ DLA_Literal $ DLL_Int at i
+    SLV_Bytes _ bs -> return $ DLAE_Arg $ DLA_Literal $ DLL_Bytes bs
+    SLV_Array at' t vs -> do
+      ds <- mapM (slToDL pdvs at') vs
+      dt <- st2dt t
+      return $ DLAE_Array dt ds
+    SLV_Tuple at' vs -> do
+      ds <- mapM (slToDL pdvs at') vs
+      return $ DLAE_Tuple $ ds
+    SLV_Object at' _ fenv -> do
+      denv <- mapM ((slToDL pdvs at') . sss_val) fenv
+      return $ DLAE_Obj denv
+    SLV_Clo _ _ _ _ _ -> Nothing
+    SLV_Data at' t vn sv -> do
+      dt <- traverse st2dt t
+      DLAE_Data dt vn <$> slToDL pdvs at' sv
+    SLV_DLC c -> return $ DLAE_Arg $ DLA_Constant c
+    SLV_DLVar dv -> return $ DLAE_Arg $ DLA_Var dv
+    SLV_Type _ -> Nothing
+    SLV_Connector _ -> Nothing
+    SLV_Participant _ who _ mdv ->
+      case (M.lookup who pdvs) <|> mdv of
+        Just dv -> return $ DLAE_Arg $ DLA_Var dv
+        Nothing -> Nothing
+    SLV_RaceParticipant {} -> Nothing
+    SLV_Prim _ -> Nothing
+    SLV_Form _ -> Nothing
+    SLV_Kwd _ -> Nothing
+
+typeOfM :: HasCallStack => PDVS -> SrcLoc -> SLVal -> Maybe (DLType, DLArgExpr)
+typeOfM pdvs at v = do
+  dae <- slToDL pdvs at v
+  return $ (argExprTypeOf dae, dae)
+
+typeOf :: HasCallStack => TINT -> SrcLoc -> SLVal -> (DLType, DLArgExpr)
+typeOf (mcfs, pdvs) at v =
+  case typeOfM pdvs at v of
+    Just x -> x
+    Nothing -> expect_throw mcfs at $ Err_Type_None v
+
+typeCheck :: HasCallStack => TINT -> SrcLoc -> TypeEnv -> SLType -> SLVal -> IO DLArgExpr
+typeCheck tint@(mcfs, _) at env ty val = typeCheck_help mcfs at env ty val vty res
+  where
+    (val_ty, res) = typeOf tint at val
+    vty = dt2st val_ty
+
+typeChecks :: HasCallStack => TINT -> SrcLoc -> TypeEnv -> [SLType] -> [SLVal] -> IO [DLArgExpr]
+typeChecks tint@(mcfs, _) at env ts vs =
+  case (ts, vs) of
+    ([], []) ->
+      return []
+    ((t : ts'), (v : vs')) -> do
+      d <- typeCheck tint at env t v
+      ds' <- typeChecks tint at env ts' vs'
+      return $ d : ds'
+    ((_ : _), _) ->
+      expect_throw mcfs at $ Err_Type_TooFewArguments ts
+    (_, (_ : _)) ->
+      expect_throw mcfs at $ Err_Type_TooManyArguments vs
+
+checkAndConvert_i :: HasCallStack => TINT -> SrcLoc -> TypeEnv -> SLType -> [SLVal] -> IO (SLType, [DLArgExpr])
+checkAndConvert_i tint@(mcfs, _) at env t args =
+  case t of
+    ST_Fun dom rng -> do
+      dargs <- typeChecks tint at env dom args
+      return (rng, dargs)
+    ST_Forall var ft -> do
+      var_ref <- newIORef Nothing
+      let env' = M.insert var var_ref env
+      (vrng, dargs) <- checkAndConvert_i tint at env' ft args
+      rng <- typeSubst at env' vrng
+      return (rng, dargs)
+    _ -> expect_throw mcfs at $ Err_Type_NotApplicable t
+
+checkAndConvert :: HasCallStack => TINT -> SrcLoc -> SLType -> [SLVal] -> IO (DLType, [DLArgExpr])
+checkAndConvert tint@(mcfs, _) at t args = do
+  (st, exprs) <- checkAndConvert_i tint at mempty t args
+  case st2dt st of
+    Just dt -> pure (dt, exprs)
+    Nothing -> expect_throw mcfs at $ Err_Type_NotDT st
+
+checkType :: HasCallStack => TINT -> SrcLoc -> SLType -> SLVal -> DLArgExpr
+checkType tint@(mcfs, _) at et v =
+  typeMeet mcfs at (at, et) (at, dt2st t) `seq` da
+  where
+    (t, da) = typeOf tint at v
+
+gmcfs :: SLCtxt -> Maybe [SLCtxtFrame]
+gmcfs = Just . ctxt_stack
+
+gtint :: SLCtxt -> SLState -> (Maybe [SLCtxtFrame], M.Map SLPart DLVar)
+gtint ctxt st = (gmcfs ctxt, st_pdvs st)
 
 is_class :: SLCtxt -> SLPart -> Bool
 is_class ctxt who = S.member who $ ctxt_classes ctxt
 
 expect_throw_ctx :: HasCallStack => (Show a, ErrorMessageForJson a, ErrorSuggestions a) => SLCtxt -> SrcLoc -> a -> b
-expect_throw_ctx ctxt = expect_throw (mcfs ctxt)
+expect_throw_ctx ctxt = expect_throw (gmcfs ctxt)
 
 typeOf_ctxt :: HasCallStack => SLCtxt -> SLState -> SrcLoc -> SLVal -> (DLType, DLArgExpr)
-typeOf_ctxt ctxt st = typeOf (tint ctxt st)
+typeOf_ctxt ctxt st = typeOf (gtint ctxt st)
 
 checkType_ctxt :: SLCtxt -> SLState -> SrcLoc -> SLType -> SLVal -> DLArgExpr
-checkType_ctxt ctxt st = checkType (tint ctxt st)
+checkType_ctxt ctxt st = checkType (gtint ctxt st)
 
 typeMeet_ctxt :: HasCallStack => SLCtxt -> SrcLoc -> (SrcLoc, SLType) -> (SrcLoc, SLType) -> SLType
-typeMeet_ctxt ctxt = typeMeet (mcfs ctxt)
+typeMeet_ctxt ctxt = typeMeet (gmcfs ctxt)
 
 -- precondition: the SLTypes coming in are known to be DLTypes
 typeMeet_ctxt_dt :: HasCallStack => SLCtxt -> SrcLoc -> (SrcLoc, SLType) -> (SrcLoc, SLType) -> DLType
@@ -673,7 +870,7 @@ typeMeet_ctxt_dt ctxt at t1 t2 = case st2dt $ typeMeet_ctxt ctxt at t1 t2 of
   Nothing -> impossible "The type meet of two DLTypes must also be a DLType"
 
 typeMeets_ctxt :: HasCallStack => SLCtxt -> SrcLoc -> [(SrcLoc, SLType)] -> SLType
-typeMeets_ctxt ctxt = typeMeets (mcfs ctxt)
+typeMeets_ctxt ctxt = typeMeets (gmcfs ctxt)
 
 -- precondition: the SLTypes coming in are known to be DLTypes
 typeMeets_ctxt_dt :: HasCallStack => SLCtxt -> SrcLoc -> [(SrcLoc, SLType)] -> DLType
@@ -682,7 +879,7 @@ typeMeets_ctxt_dt ctxt at tys = case st2dt $ typeMeets_ctxt ctxt at tys of
   Nothing -> impossible "The type meet of DLTypes must also be a DLType"
 
 checkAndConvert_ctxt :: HasCallStack => SLCtxt -> SLState -> SrcLoc -> SLType -> [SLVal] -> IO (DLType, [DLArgExpr])
-checkAndConvert_ctxt ctxt st = checkAndConvert (tint ctxt st)
+checkAndConvert_ctxt ctxt st = checkAndConvert (gtint ctxt st)
 
 ctxt_alloc :: SLCtxt -> IO Int
 ctxt_alloc ctxt =
