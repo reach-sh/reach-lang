c2670023cb64618728461cfd0a99872555c60dda
Matt Audesse <matt@mattaudesse.com>
2021-02-11 12:01:46 -0500

    rpc-client/index.mjs: rearrange closures @jeapostrophe -style
    See:
    https://github.com/reach-sh/reach-lang/pull/54#discussion_r574632062

--- js/rpc-client/index.mjs
+++ js/rpc-client/index.mjs
@@ -1,55 +1,7 @@
 import waitPort from 'wait-port';
 import bent     from 'bent';
 
-const mkRpcReady = (host, port) => async () => {
-  await waitPort({ host, port });
-};
-
-const mkRpc = call => async (m, ...args) => {
-  const lab = `RPC ${m} ${JSON.stringify(args)}`
-  console.log(`${lab}`);
-  const ans = await call(m, args);
-  console.log(`${lab} ==> ${JSON.stringify(ans)}`);
-  return ans;
-};
-
-const mkRpcCallbacks = rpc => async (m, arg, cbacks) => {
-  const vals = {};
-  const meths = {};
-  for (const k in cbacks) {
-    const v = cbacks[k];
-    if ( v instanceof Function ) {
-      meths[k] = true;
-    } else {
-      vals[k] = v;
-    }
-  }
-  return new Promise((resolve, reject) => (async () => {
-    let p = rpc(m, arg, vals, meths);
-    while (true) {
-      try {
-        const r = await p;
-        switch ( r.t ) {
-          case 'Done': {
-            return resolve(r.ans);
-          }
-          case 'Kont': {
-            const { kid, m, args } = r;
-            const ans = await cbacks[m](...args);
-            p = rpc(`/kont`, kid, ans);
-            break;
-          }
-          default:
-            throw new Error(`Illegal callback return: ${JSON.stringify(r)}`);
-        }
-      } catch (e) {
-        return reject(e);
-      }
-    }
-  })());
-};
-
-export const makeRpc = opts => {
+export const mkRPC = opts => {
   const defaults = {
     host:   'REACH_RPC_SERVER',
     port:   'REACH_RPC_PORT',
@@ -74,11 +26,53 @@ export const makeRpc = opts => {
     'X-API-Key': o.apiKey,
   });
 
-  const rpc = mkRpc(call);
-
-  return {
-    rpc,
-    rpcReady:     mkRpcReady(o.host, parseInt(o.port, 10)),
-    rpcCallbacks: mkRpcCallbacks(rpc),
+  const rpcReady = async () => {
+    await waitPort({ host: o.host, port: parseInt(o.port, 10) });
   };
+
+  const rpc = async (m, ...args) => {
+    const lab = `RPC ${m} ${JSON.stringify(args)}`
+    console.log(`${lab}`);
+    const ans = await call(m, args);
+    console.log(`${lab} ==> ${JSON.stringify(ans)}`);
+    return ans;
+  };
+
+  const rpcCallbacks = async (m, arg, cbacks) => {
+    const vals = {};
+    const meths = {};
+    for (const k in cbacks) {
+      const v = cbacks[k];
+      if ( v instanceof Function ) {
+        meths[k] = true;
+      } else {
+        vals[k] = v;
+      }
+    }
+    return new Promise((resolve, reject) => (async () => {
+      let p = rpc(m, arg, vals, meths);
+      while (true) {
+        try {
+          const r = await p;
+          switch ( r.t ) {
+            case 'Done': {
+              return resolve(r.ans);
+            }
+            case 'Kont': {
+              const { kid, m, args } = r;
+              const ans = await cbacks[m](...args);
+              p = rpc(`/kont`, kid, ans);
+              break;
+            }
+            default:
+              throw new Error(`Illegal callback return: ${JSON.stringify(r)}`);
+          }
+        } catch (e) {
+          return reject(e);
+        }
+      }
+    })());
+  };
+
+  return { rpc, rpcReady, rpcCallbacks };
 };
