58c6526daa08bc8327b54f60abe312bad9883c9c
Dan Burton <danburton.email@gmail.com>
2021-02-22 12:17:29 -0500

    tut-6-rpc: use reach script from repo

--- examples/tut-6-rpc/sbin/server-up.sh
+++ examples/tut-6-rpc/sbin/server-up.sh
@@ -3,12 +3,15 @@
 # (Run from tut-6-rpc)
 set -e
 
+# from examples/tut-6-rpc
+ROOT="$(realpath ../..)"
+
 REACH_RPC_KEY=$(cat REACH_RPC_KEY.txt)
 export REACH_RPC_KEY
 
 echo
 echo 'starting server...'
-(cd server && reach server &)
+(cd server && "${ROOT}/reach" server &)
 sleep 10
 echo 'started...'
 echo
