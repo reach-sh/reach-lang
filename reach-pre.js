a57b1272776b00edaa2765a3de134d63fccfcc8c
reachdevbot <devbot@reach.sh>
2021-03-04 18:55:28 +0000

    [ci skip] docs for 88f3588a9e3b2b71bf30099564a94101e9b89d78

--- reach-pre.js
+++ reach-pre.js
@@ -1,3 +1,12 @@
+function reach_DoSearch(event, field) {
+  var val = field.value;
+  if ( event && event.key === 'Enter' ) {
+    window.location = "https://www.google.com/search?q=site:docs.reach.sh+" + encodeURIComponent(val);
+    return false;
+  }
+  return true;
+}
+
 function reach_init() {
   var clipboard = new ClipboardJS('.btn');
 
