918f275d1f2f0cee2f552f61456953cc0c7fb8ec
Dan Burton <danburton.email@gmail.com>
2021-03-15 16:59:18 -0400

    js: a fragile comparison of package.json files

---
+++ js/stdlib/sbin/check.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+# Usage: sbin/check.sh
+# (call from js/stdlib/)
+
+# This is a very fragile and slightly incorrect way of checking
+# that this package.json hasn't drifted too far away
+# from js-deps.
+
+# Basically, when js-deps updates its dependencies,
+# they need to get updated here, too.
+
+N_DIFF_LINES="$(diff ../js-deps/package.json ./package.json | wc -l)"
+if [ $N_DIFF_LINES -eq 36 ]; then
+	echo 'The diff line count looks good'
+else
+	echo 'The diff line count looks off'
+	set -ex
+	diff ../js-deps/package.json ./package.json | wc -l
+	diff ../js-deps/package.json ./package.json
+fi
