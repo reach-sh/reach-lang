9ab824bb10137f03c931563035b8a89dd9d028d7
Dan Burton <danburton.email@gmail.com>
2021-03-15 16:59:44 -0400

    js: proper exit codes for make test

---
+++ js/stdlib/sbin/test.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+# usage: sbin/test.sh
+# (call from js/stdlib/ folder)
+
+# Note: We're creating an `stdlib-test` directory from scratch here in order
+#   to leverage the `reach` script and abstract over boilerplate such as
+#   `docker-compose.yml` config.
+
+set -x
+
+mkdir test/stdlib-test
+cp    test/index.* test/stdlib-test
+cp -r test/lib     test/stdlib-test/lib
+cd test/stdlib-test
+
+# js/stdlib/test/stdlib-test/
+REACH=../../../../reach
+$REACH compile
+REACH_CONNECTOR_MODE=ETH $REACH run
+RESULT=$?
+$REACH down
+
+exit $RESULT
