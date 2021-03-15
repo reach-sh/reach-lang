697a27e51d25cd04af10cb99cc58d511da951abd
Chris Nevers <christophernevers96@gmail.com>
2021-03-15 16:56:26 -0400

    Add example test: varying number of transfers

---
+++ examples/variable_transfers/index.mjs
@@ -0,0 +1,47 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+export const run = async (n) => {
+  const stdlib = await loadStdlib();
+  const startingBalance = stdlib.parseCurrency(100);
+
+  const fmt = (x) => stdlib.formatCurrency(x, 4);
+  const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+
+  const accAlice = await stdlib.newTestAccount(startingBalance);
+  const accBob = await stdlib.newTestAccount(startingBalance);
+
+  const beforeAlice = await getBalance(accAlice);
+  const beforeBob = await getBalance(accBob);
+
+  const ctcAlice = accAlice.deploy(backend);
+  const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
+
+  await Promise.all([
+    backend.A(ctcAlice, {
+      n,
+      payment: stdlib.parseCurrency(75)
+    }),
+    backend.B(ctcBob, {
+    }),
+  ]);
+
+  const afterAlice = await getBalance(accAlice);
+  const afterBob = await getBalance(accBob);
+
+  const info = {
+    0: 'Alice wins',
+    1: 'Bob wins',
+    2: 'Draw'
+  }
+
+  console.log(`Run results: ${info[n]}`);
+  console.log(`Alice from ${beforeAlice} to ${afterAlice}`);
+  console.log(`Bob from ${beforeBob} to ${afterBob}`);
+};
+
+(async () => {
+  await run(0);
+  await run(1);
+  await run(2);
+})();
---
+++ examples/variable_transfers/index.rsh
@@ -0,0 +1,30 @@
+'reach 0.1';
+
+export const main =
+  Reach.App(
+    {},
+    [
+      Participant('A', { n: UInt, payment: UInt }),
+      Participant('B', {})
+    ],
+    (A, B) => {
+      A.only(() => {
+        const payment = declassify(interact.payment);
+        const n = declassify(interact.n);
+      })
+      A.publish(n, payment).pay(payment);
+      commit();
+
+      B.publish().pay(payment);
+
+      if (n == 0) {
+        transfer(payment * 2).to(A);
+      } else if (n == 1) {
+        transfer(payment * 2).to(B);
+      } else {
+        transfer(payment).to(A);
+        transfer(payment).to(B);
+      }
+      commit();
+      exit();
+    });
