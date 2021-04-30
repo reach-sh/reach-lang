efc48160d9e58e82b095b372340cea1b6607cd78
Jay McCarthy <jay.mccarthy@gmail.com>
2021-04-30 15:00:51 -0400

    update art

--- examples/overview/build/index.main.sol
+++ examples/overview/build/index.main.sol
@@ -6,6 +6,8 @@ interface IERC20 {
     function allowance(address owner, address spender) external view returns (uint256);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function balanceOf(address owner) external view returns (uint256);
 }
 
 contract Stdlib {
@@ -68,39 +70,51 @@ contract Stdlib {
   function checkPayAmt(address sender, address payable token, uint256 amt) internal returns (bool) {
     return tokenTransferFrom(token, sender, address(this), amt);
   }
+
+  function tokenApprove(address payable token, address spender, uint256 amt) internal returns (bool res) {
+    (bool ok, bytes memory ret) = token.call{value: uint256(0)}(abi.encodeWithSelector(IERC20.approve.selector, spender, amt));
+    checkFunReturn(ok, ret, 'token.approve');
+    res = abi.decode(ret, (bool));
+  }
+
+  function tokenBalanceOf(address payable token, address owner) internal returns (uint256 res) {
+    (bool ok, bytes memory ret) = token.call{value: uint256(0) }(abi.encodeWithSelector(IERC20.balanceOf.selector, owner));
+    checkFunReturn(ok, ret, 'token.balanceOf');
+    res = abi.decode(ret, (uint256));
+  }
 }
 
 struct T0 {
   uint256 v20;
-   }
+  }
 struct T1 {
   address payable v24;
   uint256 v25;
   uint256 v28;
-   }
+  }
 struct T2 {
   uint256 v25;
-   }
+  }
 struct T3 {
   T0 svs;
   T2 msg;
-   }
+  }
 struct T4 {
   address payable v24;
   uint256 v25;
   uint256 v35;
-   }
+  }
 struct T6 {
   T1 svs;
   bool msg;
-   }
+  }
 struct T7 {
   uint8[128] v40;
-   }
+  }
 struct T8 {
   T4 svs;
   T7 msg;
-   }
+  }
 
 
 contract ReachContract is Stdlib {
@@ -109,7 +123,7 @@ contract ReachContract is Stdlib {
   event e0();
   struct _F0 {
     uint256 v20;
-     }
+    }
   constructor() payable {
     emit e0();
     _F0 memory _f;
@@ -120,7 +134,7 @@ contract ReachContract is Stdlib {
     nsvs.v20 = _f.v20;
     current_state = uint256(keccak256(abi.encode(uint256(0), nsvs)));
     
-     }
+    }
   
   
   
@@ -131,10 +145,8 @@ contract ReachContract is Stdlib {
     current_state = 0x0;
     
     
-    require(true && true);
-    require(true);
+    
     require(msg.value == uint256(0));
-    require(true);
     emit e1(_a);
     T1 memory nsvs;
     nsvs.v24 = payable(msg.sender);
@@ -142,7 +154,7 @@ contract ReachContract is Stdlib {
     nsvs.v28 = uint256(block.number);
     current_state = uint256(keccak256(abi.encode(uint256(1), nsvs)));
     
-     }
+    }
   
   event e2(T6 _a);
   
@@ -151,10 +163,8 @@ contract ReachContract is Stdlib {
     current_state = 0x0;
     
     
-    require(true && true);
-    require(true);
+    
     require(msg.value == _a.svs.v25);
-    require(true);
     emit e2(_a);
     T4 memory nsvs;
     nsvs.v24 = _a.svs.v24;
@@ -162,7 +172,7 @@ contract ReachContract is Stdlib {
     nsvs.v35 = uint256(block.number);
     current_state = uint256(keccak256(abi.encode(uint256(2), nsvs)));
     
-     }
+    }
   
   event e3(T8 _a);
   
@@ -171,8 +181,7 @@ contract ReachContract is Stdlib {
     current_state = 0x0;
     
     
-    require(true && true);
-    require(true);
+    
     require(msg.value == uint256(0));
     require((_a.svs.v24 == payable(msg.sender)));
     _a.svs.v24.transfer(_a.svs.v25);
@@ -180,8 +189,8 @@ contract ReachContract is Stdlib {
     current_state = 0x0;
     selfdestruct(payable(msg.sender));
     
-     }
+    }
   
   
   receive () external payable {}
-   }
+  }
