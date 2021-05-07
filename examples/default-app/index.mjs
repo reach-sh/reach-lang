7ef6de513c11caa6d0328a08238bf204f40e34a6
Chris Nevers <christophernevers96@gmail.com>
2021-05-07 17:20:46 -0400

    Make frontend purty and update docs regarding library output name

--- examples/default-app/index.mjs
+++ examples/default-app/index.mjs
@@ -1,22 +1,13 @@
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.default.mjs';
 
-const equals = (a, b) => {
-  if (a === b) return true;
-  if (a instanceof Date && b instanceof Date)
-    return a.getTime() === b.getTime();
-  if (!a || !b || (typeof a !== 'object' && typeof b !== 'object'))
-    return a === b;
-  if (a.prototype !== b.prototype) return false;
-  let keys = Object.keys(a);
-  if (keys.length !== Object.keys(b).length) return false;
-  return keys.every(k => equals(a[k], b[k]));
-};
+const assertEq = (l, r) =>
+  console.assert(JSON.stringify(l) === JSON.stringify(r));
 
 (async () => {
   const stdlib = await loadStdlib();
-  const exports = backend.getExports(stdlib);
+  const { winnerIs, ROCK, PAPER, SCISSORS, B_WINS, A_WINS } = backend.getExports(stdlib);
 
-  console.assert(equals(exports.winnerIs(exports.ROCK, exports.PAPER), exports.B_WINS));
-  console.assert(equals(exports.winnerIs(exports.SCISSORS, exports.PAPER), exports.A_WINS));
+  assertEq(winnerIs(ROCK, PAPER), B_WINS);
+  assertEq(winnerIs(SCISSORS, PAPER), A_WINS);
 })();
