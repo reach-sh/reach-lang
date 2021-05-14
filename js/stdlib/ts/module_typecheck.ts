4f02bbed8571640d469e1654ce1bf9c7dfd1777f
Dan Burton <danburton.email@gmail.com>
2021-05-14 16:04:40 -0400

    js/ETH: makeEthLike

---
+++ js/stdlib/ts/module_typecheck.ts
@@ -0,0 +1,15 @@
+// This file only exists to check that modules satisfy an interface
+
+import * as ETH from './ETH';
+import * as ETH_compiled from './ETH_compiled';
+// import * as CFX from './CFX';
+
+import { EthLike, EthLikeCompiled } from './ETH_like_interfaces';
+
+const _ETH_compiled: EthLikeCompiled = ETH_compiled;
+void(_ETH_compiled);
+
+const _ETH: EthLike = ETH;
+void(_ETH);
+
+// const _CFX: EthLike = CFX;
