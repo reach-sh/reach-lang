20cd759f64f2030132a285ff2c81401c978e53b1
Dan Burton <danburton.email@gmail.com>
2021-05-14 19:21:02 -0400

    js/ETH: less boilerplate, more exports

--- js/stdlib/ts/ETH.ts
+++ js/stdlib/ts/ETH.ts
@@ -1,86 +1,11 @@
 import { makeEthLike } from './ETH_like'
 import * as ethImpl from './ETH_impl';
 
-// TODO: export some types from ./shared?
+export * from './ETH_compiled';
 
 const ethLike = makeEthLike(ethImpl);
 
 export const {
-  // start ...shared,
-  getViewsHelper,
-  deferContract,
-  truthyEnv,
-  envDefault,
-  setDEBUG,
-  getDEBUG,
-  debug,
-  assert,
-  isBigNumber,
-  checkedBigNumberify,
-  protect,
-  isHex,
-  hexToString,
-  stringToHex,
-  makeDigest,
-  hexToBigNumber,
-  uintToBytes,
-  bigNumberToHex,
-  bytesEq,
-  digestEq,
-  makeRandom,
-  eq,
-  makeArith,
-  ge,
-  gt,
-  le,
-  lt,
-  argsSlice,
-  argsSplit,
-  Array_set,
-  Array_zip,
-  mapRef,
-  objectMap,
-  mkAddressEq,
-  parseFixedPoint,
-  parseInt,
-  // end ...shared,
-
-  // start ...arith,
-  add,
-  sub,
-  mod,
-  mul,
-  div,
-  // end ...arith,
-
-  // start ...typeDefs
-  T_Null,
-  T_Bool,
-  T_UInt,
-  T_Bytes,
-  T_Address,
-  T_Digest,
-  T_Token,
-  T_Object,
-  T_Data,
-  T_Array,
-  T_Tuple,
-  T_Struct,
-  // end ...typeDefs
-
-  // start EthLikeBackendStdlib (+ above)
-  addressEq,
-  tokenEq,
-  digest,
-  UInt_max,
-  // end EthLikeBackendStdlib
-
-  // start EthLikeCompiled (+ above)
-  stdlib,
-  typeDefs,
-  // end EthLikeCompiled
-
-  // start EthLike (+ above)
   getProvider,
   setProvider,
   randomUInt,
@@ -110,6 +35,4 @@ export const {
   formatCurrency,
   formatAddress,
   reachStdlib,
-  // end EthLike
-
 } = ethLike;
