3b860a6a4f51da2a8e57792e279f4f4c91808b1e
Jay McCarthy <jay.mccarthy@gmail.com>
2021-06-01 10:55:00 -0400

    rwrw

---
+++ examples/map-rwrw/index.mjs
@@ -0,0 +1,31 @@
+import {loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+(async () => {
+  const stdlib = await loadStdlib();
+  if ( stdlib.connector === 'ALGO' ) {
+    console.log(`XXX Unsupported`);
+    process.exit(0);
+  }
+  const assertEq = (expected, actual) => {
+    const exps = JSON.stringify(expected);
+    const acts = JSON.stringify(actual);
+    console.log('assertEq', {expected, actual}, {exps, acts});
+    stdlib.assert(exps === acts) };
+  const startingBalance = stdlib.parseCurrency(10);
+  const [ accAlice, accBob ] = await Promise.all([
+    stdlib.newTestAccount(startingBalance),
+    stdlib.newTestAccount(startingBalance),
+  ]);
+  const ctcAlice = accAlice.deploy(backend);
+  const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
+
+  const common = (Who) => ({
+    get: (() => Who.length),
+    check: assertEq,
+  });
+  await Promise.all([
+    backend.Alice(ctcAlice, common('Alice')),
+    backend.Bob(ctcBob, common('Bob')),
+  ]);
+})();
---
+++ examples/map-rwrw/index.rsh
@@ -0,0 +1,34 @@
+'reach 0.1';
+
+const MUInt = Maybe(UInt);
+export const main = Reach.App(() => {
+  const common = {
+    get: Fun([], UInt),
+    check: Fun([MUInt, MUInt], Null),
+  };
+  const A = Participant('Alice', common);
+  const B = Participant('Bob', common);
+  deploy();
+
+  A.only(() => {
+    const a = declassify(interact.get()); });
+  A.publish(a);
+  const m = new Map(UInt);
+  assert(m[A] == MUInt.None());
+
+  const read = () => maybe(m[A], 0, (x) => x);
+
+  m[A] = a;
+  require(read() == a);
+
+  m[A] = a + 1;
+  require(read() == a + 1);
+  
+  m[A] = a + 2;
+  require(read() == a + 2);
+
+  commit();
+  A.interact.check(m[A], MUInt.Some(a+1));
+
+  exit();
+});
