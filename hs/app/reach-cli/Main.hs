3b38d192739cf41cb13a0521a1a23111a5203a84
Matt Audesse <matt@mattaudesse.com>
2021-06-04 13:56:09 -0400

    reach'/#1271: WIP consolidate `Subcommand` definitions

--- hs/app/reach-cli/Main.hs
+++ hs/app/reach-cli/Main.hs
@@ -5,6 +5,7 @@
 
 module Main (main) where
 
+import Control.Monad (join)
 import Control.Monad.Shell
 import Options.Applicative
 import Options.Applicative.Help.Pretty
@@ -18,6 +19,9 @@ default (T.Text)
 -- TODO usage help currently prints `reach-cli` instead of `reach`, which may
 -- be confusing to end-users who shouldn't have to care that their CLI session
 -- is being forwarded to this binary... Consider renaming
+
+type Subcommand = ParserInfo (Script ())
+
 
 -- TODO
 reachVersion :: T.Text
@@ -35,30 +39,6 @@ reachImages =
   ]
 
 
-data Cli
-  = Clean FilePath T.Text
-  | Compile
-  | Devnet
-  | DockerReset
-  | Down
-  | Hashes
-  | Help
-  | Init
-  | NumericVersion
-  | React
-  | ReactDown
-  | RpcRun
-  | RpcServer
-  | RpcServerDown
-  | Run
-  | Scaffold
-  | Unscaffold
-  | Update
-  | Upgrade
-  | Version
-  | Whoami
-
-
 --------------------------------------------------------------------------------
 echo :: CmdParams p => p
 echo = cmd "echo"
@@ -66,6 +46,10 @@ echo = cmd "echo"
 
 docker :: CmdParams p => p
 docker = cmd "docker"
+
+
+rm :: CmdParams p => p
+rm = cmd "rm"
 
 
 stdErrDevNull :: Script () -> Script ()
@@ -83,9 +67,8 @@ regardless' f = regardless $ toStderr f
 
 
 --------------------------------------------------------------------------------
-cmdClean :: Mod CommandFields Cli
-cmdClean = command "clean" . info opts $ fullDesc <> desc <> fdoc
- where
+clean :: Subcommand
+clean = info f $ fullDesc <> desc <> fdoc where
   desc = progDesc "Delete 'build/$MODULE.$IDENT.mjs'"
   fdoc = footerDoc . Just
      $  text "MODULE is \"index\" by default"
@@ -94,222 +77,162 @@ cmdClean = command "clean" . info opts $ fullDesc <> desc <> fdoc
    <$$> text "If:"
    <$$> text " * MODULE is a directory then `cd $MODULE && rm -f \"build/index.$IDENT.mjs\";"
    <$$> text " * MODULE is <something-else> then `rm -f \"build/$MODULE.$IDENT.mjs\""
-  opts = Clean
+
+  go m i = do
+    let f' m' = rm "-f" $ "build/" <> T.pack m' <> "." <> i <> ".mjs"
+
+    case m of
+      "index" -> f' m
+      _       -> ifCmd (test $ TDirExists m)
+        (cmd "cd" m -||- cmd "exit" "1" *> f' "index")
+        (f' m)
+
+  f = go
     <$> strArgument (metavar "MODULE" <> value "index" <> showDefault)
     <*> strArgument (metavar "IDENT"  <> value "main"  <> showDefault)
 
 
--- | > rm -f "build/$m.$i.mjs
-clean :: FilePath -> T.Text -> Script ()
-clean m i = do
-  let f m' = cmd "rm" "-f" $ "build/" <> T.pack m' <> "." <> i <> ".mjs"
-  if m == "index"
-    then f m
-    else ifCmd (test $ TDirExists m)
-      (cmd "cd" m -||- cmd "exit" "1" *> f "index")
-      (f m)
+--------------------------------------------------------------------------------
+compile :: Subcommand
+compile = info f d where
+  d = progDesc "Compile an app"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdCompile :: Mod CommandFields Cli
-cmdCompile = command "compile" $ info (pure Compile) desc where
-  desc = progDesc "Compile an app"
-
-
-compile :: Script ()
-compile = undefined
+init' :: Subcommand
+init' = info f d where
+  d = progDesc "Set up source files for a simple app"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdInit :: Mod CommandFields Cli
-cmdInit = command "init" $ info (pure Init) desc where
-  desc = progDesc "Set up source files for a simple app"
-
-
-init' :: Script ()
-init' = undefined
+run' :: Subcommand
+run' = info f d where
+  d = progDesc "Run a simple app"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdRun :: Mod CommandFields Cli
-cmdRun = command "run" $ info (pure Run) desc where
-  desc = progDesc "Run a simple app"
-
-
-run' :: Script ()
-run' = undefined
+down :: Subcommand
+down = info f d where
+  d = progDesc "Halt any Dockerized devnets for this app"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdDown :: Mod CommandFields Cli
-cmdDown = command "down" $ info (pure Down) desc where
-  desc = progDesc "Halt any dockerized devnets for this app"
-
-
-down :: Script ()
-down = undefined
+scaffold :: Subcommand
+scaffold = info f d where
+  d = progDesc "Set up Docker scaffolding for a simple app"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdScaffold :: Mod CommandFields Cli
-cmdScaffold = command "scaffold" $ info (pure Scaffold) desc where
-  desc = progDesc "Set up Docker scaffolding for a simple app"
-
-
-scaffold :: Script ()
-scaffold = undefined
+react :: Subcommand
+react = info f d where
+  d = progDesc "Run a simple React app"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdReact :: Mod CommandFields Cli
-cmdReact = command "react" $ info (pure React) desc where
-  desc = progDesc "Run a simple React app"
-
-
-react :: Script ()
-react = undefined
+rpcServer :: Subcommand
+rpcServer = info f d where
+  d = progDesc "Run a simple Reach RPC server"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdRpcServer :: Mod CommandFields Cli
-cmdRpcServer = command "rpc-server" $ info (pure RpcServer) desc where
-  desc = progDesc "Run a simple Reach RPC server"
-
-
-rpcServer :: Script ()
-rpcServer = undefined
+rpcRun :: Subcommand
+rpcRun = info f d where
+  d = progDesc "Run an RPC server + frontend with development configuration"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdRpcRun :: Mod CommandFields Cli
-cmdRpcRun = command "rpc-run" $ info (pure RpcRun) desc where
-  desc = progDesc "Run an RPC server + frontend with development configuration"
-
-
-rpcRun :: Script ()
-rpcRun = undefined
+devnet :: Subcommand
+devnet = info f d where
+  d = progDesc "Run only the devnet"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdDevnet :: Mod CommandFields Cli
-cmdDevnet = command "devnet" $ info (pure Devnet) desc where
-  desc = progDesc "Run only the devnet"
-
-
-devnet :: Script ()
-devnet = undefined
+upgrade :: Subcommand
+upgrade = info f d where
+  d = progDesc "Upgrade Reach"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdUpgrade :: Mod CommandFields Cli
-cmdUpgrade = command "upgrade" $ info (pure Upgrade) desc where
-  desc = progDesc "Upgrade Reach"
-
-
-upgrade :: Script ()
-upgrade = undefined
+update :: Subcommand
+update = info f d where
+  d = progDesc "Update Reach Docker images"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdUpdate :: Mod CommandFields Cli
-cmdUpdate = command "update" $ info (pure Update) desc where
-  desc = progDesc "Update Reach Docker images"
-
-
-update :: Script ()
-update = undefined
-
-
---------------------------------------------------------------------------------
-cmdDockerReset :: Mod CommandFields Cli
-cmdDockerReset = command "docker-reset" $ info (pure DockerReset) desc where
-  desc = progDesc "Docker kill and rm all images"
-
-
-dockerReset :: Script ()
-dockerReset = do
-  echo "Docker kill all the things..."
-  regardless' $ docker "kill" (Output $ docker "ps" "-q" )
-  echo "Docker rm   all the things..."
-  regardless' $ docker "rm"   (Output $ docker "ps" "-qa")
-  echo "...done"
-
---------------------------------------------------------------------------------
-cmdVersion :: Mod CommandFields Cli
-cmdVersion = command "version" $ info (pure Version) desc where
-  desc = progDesc "Display version"
-
-
-version :: Script ()
-version = echo (quote $ "reach " <> reachVersion)
+dockerReset :: Subcommand
+dockerReset = info f d where
+  d = progDesc "Docker kill and rm all images"
+  f = pure $ do
+    echo "Docker kill all the things..."
+    regardless' $ docker "kill" (Output $ docker "ps" "-q" )
+    echo "Docker rm   all the things..."
+    regardless' $ docker "rm"   (Output $ docker "ps" "-qa")
+    echo "...done"
 
 
 --------------------------------------------------------------------------------
-cmdHelp :: Mod CommandFields Cli
-cmdHelp = command "help" $ info (pure Help) desc where
-  desc = progDesc "Show usage"
-
-
-help' :: Script ()
-help' = undefined
+version :: Subcommand
+version = info f d where
+  d = progDesc "Display version"
+  f = pure $ echo (quote $ "reach " <> reachVersion)
 
 
 --------------------------------------------------------------------------------
-cmdHashes :: Mod CommandFields Cli
-cmdHashes = command "hashes" $ info (pure Hashes) desc where
-  desc = progDesc "Display git hashes used to build each Docker image"
-
-
-hashes :: Script ()
-hashes = flip mapM_ reachImages $ \i -> do
-  let t = "reachsh/" <> i <> ":" <> reachVersion
-      s = docker "run" "--entrypoint" "/bin/sh" t "-c" (quote "echo $REACH_GIT_HASH")
-  echo (i <> ":") (Output s)
+help' :: Subcommand
+help' = info f d where
+  d = progDesc "Show usage"
+  f = undefined
 
 
 --------------------------------------------------------------------------------
-cmdWhoami :: Mod CommandFields Cli
-cmdWhoami = command "whoami" $ info (pure Whoami) fullDesc
-
-
-whoami :: Script ()
-whoami = stdErrDevNull $ docker "info" "--format" "{{.ID}}"
-
-
---------------------------------------------------------------------------------
-cmdNumericVersion :: Mod CommandFields Cli
-cmdNumericVersion = command "numeric-version" $ info (pure NumericVersion) fullDesc
-
-
-numericVersion :: Script ()
-numericVersion = echo reachVersion
+hashes :: Subcommand
+hashes = info f d where
+  d = progDesc "Display git hashes used to build each Docker image"
+  f = pure $ flip mapM_ reachImages $ \i -> do
+    let t = "reachsh/" <> i <> ":" <> reachVersion
+    let s = docker "run" "--entrypoint" "/bin/sh" t "-c" (quote "echo $REACH_GIT_HASH")
+    echo (i <> ":") (Output s)
 
 
 --------------------------------------------------------------------------------
-cmdReactDown :: Mod CommandFields Cli
-cmdReactDown = command "react-down" $ info (pure ReactDown) fullDesc
-
-
-reactDown :: Script ()
-reactDown = undefined
+whoami :: Subcommand
+whoami = info f fullDesc where
+  f = pure . stdErrDevNull $ docker "info" "--format" "{{.ID}}"
 
 
 --------------------------------------------------------------------------------
-cmdRpcServerDown :: Mod CommandFields Cli
-cmdRpcServerDown = command "rpc-server-down" $ info (pure RpcServerDown) fullDesc
-
-
-rpcServerDown :: Script ()
-rpcServerDown = undefined
+numericVersion :: Subcommand
+numericVersion = info f fullDesc where
+  f = pure $ echo reachVersion
 
 
 --------------------------------------------------------------------------------
-cmdUnscaffold :: Mod CommandFields Cli
-cmdUnscaffold = command "unscaffold" $ info (pure Unscaffold) fullDesc
+reactDown :: Subcommand
+reactDown = info f fullDesc where
+  f = undefined
 
 
-unscaffold :: Script ()
-unscaffold = undefined
+--------------------------------------------------------------------------------
+rpcServerDown :: Subcommand
+rpcServerDown = info f fullDesc where
+  f = undefined
+
+
+--------------------------------------------------------------------------------
+unscaffold :: Subcommand
+unscaffold = info f fullDesc where
+  f = undefined
 
 
 --------------------------------------------------------------------------------
@@ -318,61 +241,38 @@ header' :: String
 header' = "https://reach.sh"
 
 
-cmds :: ParserInfo Cli
-cmds = info (hsubparser cs <|> hsubparser hs <**> helper) im where
-  im = header header' <> fullDesc
-  cs = cmdCompile
-    <> cmdClean
-    <> cmdInit
-    <> cmdRun
-    <> cmdDown
-    <> cmdScaffold
-    <> cmdReact
-    <> cmdRpcServer
-    <> cmdRpcRun
-    <> cmdDevnet
-    <> cmdUpgrade
-    <> cmdUpdate
-    <> cmdDockerReset
-    <> cmdVersion
-    <> cmdHashes
-    <> cmdHelp
+main :: IO ()
+main = join . fmap sh $ customExecParser (prefs showHelpOnError) cmds where
+  s |?| p = command s p
+
+  cs = "compile"      |?| compile
+    <> "clean"        |?| clean
+    <> "init"         |?| init'
+    <> "run"          |?| run'
+    <> "down"         |?| down
+    <> "scaffold"     |?| scaffold
+    <> "react"        |?| react
+    <> "rpc-server"   |?| rpcServer
+    <> "rpc-run"      |?| rpcRun
+    <> "devnet"       |?| devnet
+    <> "upgrade"      |?| upgrade
+    <> "update"       |?| update
+    <> "docker-reset" |?| dockerReset
+    <> "version"      |?| version
+    <> "hashes"       |?| hashes
+    <> "help"         |?| help'
+
   hs = internal
     <> commandGroup "hidden subcommands"
-    <> cmdNumericVersion
-    <> cmdReactDown
-    <> cmdRpcServerDown
-    <> cmdUnscaffold
-    <> cmdWhoami
+    <> "numeric-version" |?| numericVersion
+    <> "react-down"      |?| reactDown
+    <> "rpc-server-down" |?| rpcServerDown
+    <> "unscaffold"      |?| unscaffold
+    <> "whoami"          |?| whoami
 
+  im   = header header' <> fullDesc
+  cmds = info (hsubparser cs <|> hsubparser hs <**> helper) im where
 
-main :: IO ()
-main = customExecParser (prefs showHelpOnError) cmds >>= \case
-  Clean m i      -> sh $ clean m i
-  Compile        -> sh compile
-  Devnet         -> sh devnet
-  DockerReset    -> sh dockerReset
-  Down           -> sh down
-  Hashes         -> sh hashes
-  Help           -> sh help'
-  Init           -> sh init'
-  React          -> sh react
-  RpcRun         -> sh rpcRun
-  RpcServer      -> sh rpcServer
-  Run            -> sh run'
-  Scaffold       -> sh scaffold
-  Update         -> sh update
-  Upgrade        -> sh upgrade
-  Version        -> sh version
-
-  -- Hidden
-  NumericVersion -> sh numericVersion
-  ReactDown      -> sh reactDown
-  RpcServerDown  -> sh rpcServerDown
-  Unscaffold     -> sh unscaffold
-  Whoami         -> sh whoami
-
- where
   sh f = T.putStrLn . script $ do
     stopOnFailure True
     f
