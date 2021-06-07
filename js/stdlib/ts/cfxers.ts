c77579f336540c291a14599e780974e5ba137e4b
Dan Burton <danburton.email@gmail.com>
2021-06-07 14:34:05 -0400

    js: use .call in a weird way because js is insane

--- js/stdlib/ts/cfxers.ts
+++ js/stdlib/ts/cfxers.ts
@@ -190,7 +190,11 @@ export class ContractFactory {
     const argsConformed = conform(args, iface.deploy.inputs);
 
     // XXX gasLimit, is this handled correctly by txnOverrides?
-    const receiptP = contract.constructor(...argsConformed)
+
+    // Note: this usage of `.call` here is because javascript is insane.
+    // XXX 2021-06-07 Dan: This works for the cjs compilation target, but does it work for the other targets?
+    // @ts-ignore
+    const receiptP = contract.constructor.call(...argsConformed)
       .sendTransaction(txn)
       .executed();
 
