64ce0ae190c0584c9df3ecb33f03ccc149bab999
Matt Audesse <matt@mattaudesse.com>
2021-06-09 20:16:32 -0400

    reach'/#1271: WIP Dockerize, create `init` embeds

--- hs/src/Reach/EmbeddedFiles.hs
+++ hs/src/Reach/EmbeddedFiles.hs
@@ -1,7 +1,8 @@
-module Reach.EmbeddedFiles (runtime_smt2, runtime_bt_smt2, stdlib_sol, stdlib_rsh) where
+module Reach.EmbeddedFiles (runtime_smt2, runtime_bt_smt2, stdlib_sol, stdlib_rsh, mkEmbed) where
 
 import Data.ByteString (ByteString)
 import Data.FileEmbed
+import Language.Haskell.TH.Syntax
 
 runtime_smt2 :: ByteString
 runtime_smt2 = $(makeRelativeToProject "./smt2/runtime.smt2" >>= embedFile)
@@ -14,3 +15,8 @@ stdlib_sol = $(makeRelativeToProject "./sol/stdlib.sol" >>= embedFile)
 
 stdlib_rsh :: ByteString
 stdlib_rsh = $(makeRelativeToProject "./rsh/stdlib.rsh" >>= embedFile)
+
+-- Exported as a shorthand convenience for app/reach/Main.hs to work around
+-- GHC's "stage restriction" rules
+mkEmbed :: FilePath -> Q Exp
+mkEmbed f = makeRelativeToProject ("./app/reach/embed/" <> f) >>= embedStringFile
