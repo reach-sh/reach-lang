6bbaa8a859c3c8f224260bbf14db45532360bc45
Matt Audesse <matt@mattaudesse.com>
2021-06-11 16:57:27 -0400

    reach'/#1271: WIP implement `init` templates via `ReaderT Env IO`

--- hs/src/Reach/EmbeddedFiles.hs
+++ hs/src/Reach/EmbeddedFiles.hs
@@ -1,8 +1,7 @@
-module Reach.EmbeddedFiles (runtime_smt2, runtime_bt_smt2, stdlib_sol, stdlib_rsh, mkEmbed) where
+module Reach.EmbeddedFiles (runtime_smt2, runtime_bt_smt2, stdlib_sol, stdlib_rsh) where
 
 import Data.ByteString (ByteString)
 import Data.FileEmbed
-import Language.Haskell.TH.Syntax
 
 runtime_smt2 :: ByteString
 runtime_smt2 = $(makeRelativeToProject "./smt2/runtime.smt2" >>= embedFile)
@@ -15,8 +14,3 @@ stdlib_sol = $(makeRelativeToProject "./sol/stdlib.sol" >>= embedFile)
 
 stdlib_rsh :: ByteString
 stdlib_rsh = $(makeRelativeToProject "./rsh/stdlib.rsh" >>= embedFile)
-
--- Exported as a shorthand convenience for app/reach/Main.hs to work around
--- GHC's "stage restriction" rules
-mkEmbed :: FilePath -> Q Exp
-mkEmbed f = makeRelativeToProject ("./app/reach/embed/" <> f) >>= embedStringFile
