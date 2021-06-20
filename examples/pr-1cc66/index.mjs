26767179a0cc8ded1c1661b35ebd3a882052e59d
Dan Burton <danburton.email@gmail.com>
2021-06-20 10:35:40 -0400

    examples/pr-1cc66: a little more money for Alice

--- examples/pr-1cc66/index.mjs
+++ examples/pr-1cc66/index.mjs
@@ -2,7 +2,7 @@ import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 (async () => {
   const stdlib = await loadStdlib();
-  const startingBalance = stdlib.parseCurrency(1);
+  const startingBalance = stdlib.parseCurrency(10);
   const Alice = await stdlib.newTestAccount(startingBalance);
   const ctcAlice = Alice.deploy(backend);
   await Promise.all([
