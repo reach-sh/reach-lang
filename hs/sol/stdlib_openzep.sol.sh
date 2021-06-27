f95d47653c671e6453b1ffaa85dbefb11e4f1335
Jay McCarthy <jay.mccarthy@gmail.com>
2021-06-26 23:05:07 -0400

    sh-lint

--- hs/sol/stdlib_openzep.sol.sh
+++ hs/sol/stdlib_openzep.sol.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 TARGET="$1"
 
-sed '1,4d' "${ERC20}/IERC20.sol" > "$TARGET"
-sed '1,6d' "${ERC20}/extensions/IERC20Metadata.sol" >> "$TARGET"
-sed '1,4d' "${ERC20}/../../utils/Context.sol" >> "$TARGET"
-sed '1,8d; 158,196d; 215d; 237d; 245,267d; 288,303d' "${ERC20}/ERC20.sol" >> "$TARGET"
-
+{
+  sed '1,4d' "${ERC20}/IERC20.sol";
+  sed '1,6d' "${ERC20}/extensions/IERC20Metadata.sol";
+  sed '1,4d' "${ERC20}/../../utils/Context.sol";
+  sed '1,8d; 158,196d; 215d; 237d; 245,267d; 288,303d' "${ERC20}/ERC20.sol"
+} > "$TARGET"
