666effcb4ce4c9151a04cc82d955bcbb10200225
Dan Burton <danburton.email@gmail.com>
2021-07-12 16:53:00 -0400

    js: expose more types on interfaces

--- js/stdlib/ts/interfaces.ts
+++ js/stdlib/ts/interfaces.ts
@@ -2,43 +2,45 @@ import type { num } from './shared_backend'; // =>
 import type { BigNumber } from 'ethers'; // =>
 
 // TODO better typing on these
+type Ty = any
+
 export interface TypeDefs {
-  T_Null: any
-  T_Bool: any
-  T_UInt: any
-  T_Bytes: any
-  T_Address: any
-  T_Digest: any
-  T_Token: any
-  T_Object: any
-  T_Data: any
-  T_Array: any
-  T_Tuple: any
-  T_Struct: any
+  T_Null: Ty
+  T_Bool: Ty
+  T_UInt: Ty
+  T_Bytes: Ty
+  T_Address: Ty
+  T_Digest: Ty
+  T_Token: Ty
+  T_Object: (tyMap: {[key: string]: Ty}) => Ty
+  T_Data: (tyMap: {[key: string]: Ty}) => Ty
+  T_Array: (ty: Ty, size: number) => Ty
+  T_Tuple: (tys: Ty[]) => Ty
+  T_Struct: (nameTyPairs: [string, Ty][]) => Ty
 };
 
 export interface Stdlib_Backend_Shared_User {
-  protect: any,
-  assert: any,
-  Array_set: any,
-  eq: any,
-  ge: any,
-  gt: any,
-  le: any,
-  lt: any,
-  bytesEq: any,
-  digestEq: any,
+  protect: (ty: Ty, v: unknown, m?: string) => unknown
+  assert: (b: boolean, message: string) => void
+  Array_set: <A>(arr: A[], idx: number, val: A) => A[]
+  eq: (n1: num, n2: num) => boolean
+  ge: (n1: num, n2: num) => boolean
+  gt: (n1: num, n2: num) => boolean
+  le: (n1: num, n2: num) => boolean
+  lt: (n1: num, n2: num) => boolean
+  bytesEq: (s1: string, s2: string) => boolean
+  digestEq: (d1: string, d2: string) => boolean
 }
 
 export interface Stdlib_Backend_Shared extends Stdlib_Backend_Shared_User {
-  checkedBigNumberify: any,
-  protect: any,
-  Array_zip: any,
-  mapRef: any,
-  simMapRef: any,
-  simMapSet: any,
-  simMapDupe: any,
-  bytesConcat: any,
+  checkedBigNumberify: (at: string, max: BigNumber, n: any) => BigNumber
+  protect: (t: any, v: unknown, ai?: string) => unknown
+  Array_zip: <A, B>(a1: A[], a2: B[]) => [A, B][]
+  mapRef: <A>(m: {[key: string]: A}, f: string) => ['Some', A]|['None', null]
+  simMapRef: (sim_r: unknown, mapi: number, f: string) => ['Some', unknown]|['None', null]
+  simMapSet: (sim_r: unknown, mapi: number, f: string, v: unknown) => unknown
+  simMapDupe: (sim_r: unknown, mapi: number, mapo: unknown) => void,
+  bytesConcat: (b1: string, b2: string) => string
 };
 
 export interface Arith {
@@ -64,64 +66,79 @@ export interface Stdlib_Backend<Ty> extends Stdlib_Backend_Base<Ty> {
 export interface Stdlib_Impl_Shared {
 };
 
+// TODO: per-stddlib Provider type
+// TODO: per-stdlib env types
+// TODO: per-stdlib providerName union of string literals type
+type Provider = any
+type ProviderEnv = any // Record<string, string>
+type ProviderName = any
+
 // TODO: types
 export interface ProviderLib {
-  getProvider: any
-  setProvider: any
-  setProviderByEnv: any
-  setProviderByName: any
-  providerEnvByName: any
+  getProvider: () => Provider|Promise<Provider>
+  setProvider: (p: Provider|Promise<Provider>) => void
+  setProviderByEnv: (env: ProviderEnv) => void
+  setProviderByName: (providerName: ProviderName) => void
+  providerEnvByName: (providerName: ProviderName) => ProviderEnv
 };
+
+type FixedPoint = { sign: boolean, i: { i: BigNumber, scale: BigNumber } }
 
 // XXX
 // The shared code
 export interface Stdlib_User_Shared {
-  isBigNumber: any
-  hexToBigNumber: any
-  bigNumberToHex: any
-  uintToBytes: any
-  parseFixedPoint: any
-  parseInt: any
+  isBigNumber: (bn: unknown) => bn is BigNumber
+  hexToBigNumber: (hex: string) => BigNumber
+  bigNumberToHex: (bn: BigNumber) => string
+  uintToBytes: (bn: BigNumber) => string
+  parseFixedPoint: (fp: FixedPoint) => number
+  parseInt: (n: {sign: boolean, i: num}) => number
 };
 
 // XXX
 // The thing as composed by each connector
 export interface Stdlib_User_Base extends Stdlib_Backend_Shared_User, Stdlib_User_Shared, Arith, TypeDefs {
-  isHex: any
-  bigNumberify: any
-  stringToHex: any
-  addressEq: any
-  digest: any
+  isHex: (x: unknown) => boolean
+  bigNumberify: (n: num|string) => BigNumber
+  stringToHex: (s: string) => string
+  addressEq: (addr1: string, addr2: string) => boolean
+  digest: (t: any, a: unknown) => string
 };
 
-// TODO: types
+// TODO: types for
+type NetworkAccount = any
+type Acc = any
+type Token = any // ETH/CFX: string, ALGO: num
+type CtcInfo = any
+type Backend = any
+
 // The real thing
 export interface Stdlib_User<Ty> extends Stdlib_User_Base, ProviderLib {
   connector: string
-  randomUInt: any
-  hasRandom: any
-  hasConsoleLogger: any
-  balanceOf: any
-  transfer: any
-  connectAccount: any
-  newAccountFromSecret: any
-  newAccountFromMnemonic: any
-  getDefaultAccount: any
-  createAccount: any
-  getFaucet: any
-  fundFromFaucet: any
-  newTestAccount: any
-  getNetworkTime: any
-  wait: any
-  waitUntilTime: any
-  verifyContract: any
+  randomUInt: () => BigNumber
+  hasRandom: { random: () => BigNumber }
+  hasConsoleLogger: { log: (...a: any) => void }
+  balanceOf: (acc: Acc, token?: Token) => Promise<BigNumber>
+  transfer: (from: Acc, to: Acc, val?: BigNumber, token?: Token) => Promise<unknown>
+  connectAccount: (networkAccount: NetworkAccount) => Promise<Acc>
+  newAccountFromSecret: (secret: string) => Promise<Acc>
+  newAccountFromMnemonic: (phrase: string) => Promise<Acc>
+  getDefaultAccount: () => Promise<Acc>
+  createAccount: () => Promise<Acc>
+  getFaucet: () => Promise<Acc> // XXX
+  fundFromFaucet: (acc: Acc, balance: BigNumber) => Promise<void>
+  newTestAccount: (balance: BigNumber) => Promise<Acc>
+  getNetworkTime: () => Promise<BigNumber>
+  wait: (timeDelta: BigNumber) => Promise<BigNumber>
+  waitUntilTime: (time: BigNumber) => Promise<BigNumber>
+  verifyContract: (ctcInfo: CtcInfo, backend: Backend) => Promise<true>
   /** @description the display name of the standard unit of currency for the network */
   standardUnit: string
   /** @description the display name of the atomic (smallest) unit of currency for the network */
   atomicUnit: string
-  parseCurrency: any
-  minimumBalance: any
-  formatCurrency: any
-  formatAddress: any
+  parseCurrency: (amtDesc: any) => BigNumber
+  minimumBalance: BigNumber
+  formatCurrency: (amt: BigNumber, decimals: number) => string
+  formatAddress: (addr: string) => string
   reachStdlib: Stdlib_Backend<Ty>
 }
