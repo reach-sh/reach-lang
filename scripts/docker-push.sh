c711686949f4c18514d0c3e879ca6ba5badf2eb1
Dan Burton <danburton.email@gmail.com>
2021-07-14 10:54:18 -0400

    docker-push.sh: push tags for git hash and date

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -5,7 +5,8 @@ HERE=$(dirname "$0")
 
 IMAGE="$1"
 LATEST_TAG="${IMAGE}:latest"
-
+REACH_GIT_HASH="$("${HERE}/git-hash.sh")"
+DATE="$(date '+%Y-%m-%d')"
 
 tagpush() {
     docker tag "${LATEST_TAG}" "${IMAGE}:$1"
@@ -18,6 +19,10 @@ tagpush "latest"
 tagpush "${MAJOR}.${MINOR}.${PATCH}"
 tagpush "${MAJOR}.${MINOR}"
 tagpush "${MAJOR}"
+tagpush "${DATE}"
+if [ ${#REACH_GIT_HASH} = 8 ]; then
+  tagpush "${REACH_GIT_HASH}"
+fi
 
 if [ "${MAJOR}.${MINOR}.${PATCH}" = "${STABLE}" ] ; then
     tagpush stable
