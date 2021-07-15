53cfce23b57e5cc85d16a02f83284c3ef3142c2c
Dan Burton <danburton.email@gmail.com>
2021-07-15 19:20:11 -0400

    examples/faucet-balance: assert after logging the bal

--- examples/faucet-balance/index.mjs
+++ examples/faucet-balance/index.mjs
@@ -6,10 +6,9 @@ const stdlib = loadStdlib(process.env);
   const faucet = await stdlib.getFaucet();
   const balBn = await stdlib.balanceOf(faucet);
 
-  stdlib.assert(stdlib.gt(balBn, 0), `Faucet has funds`);
-
   const addr = stdlib.formatAddress(faucet);
   const bal = stdlib.formatCurrency(balBn);
-
   console.log(`${addr} has ${bal} ${stdlib.standardUnit}`);
+
+  stdlib.assert(stdlib.gt(balBn, 0), `Faucet has funds`);
 })();
