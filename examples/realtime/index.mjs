970256e787363bd45aecfd496ceabd8952cc037f
Jay McCarthy <jay.mccarthy@gmail.com>
2021-07-30 16:07:09 -0400

    up to realtime

--- examples/realtime/index.mjs
+++ examples/realtime/index.mjs
@@ -6,6 +6,7 @@ import * as backend from './build/index.main.mjs';
   const startingBalance = stdlib.parseCurrency(10);
   const [ accAlice ] =
     await stdlib.newTestAccounts(1, startingBalance);
+  console.log(accAlice);
   const ctcAlice = accAlice.deploy(backend);
 
   const interact = {
