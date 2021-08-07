a43c1c05c3df9fd4ee71f719151e931e8efb82b5
Jay McCarthy <jay.mccarthy@gmail.com>
2021-08-07 05:20:17 -0400

    artifacts

--- .circleci/example.sh
+++ .circleci/example.sh
@@ -3,15 +3,7 @@ CONN="$1"
 SIZE="$2"
 RANK="$3"
 
-banner () {
-  echo
-  echo "############"
-  echo "$*"
-  echo "############"
-  echo
-}
-
-banner Running w/ "${CONN}"
+echo Running w/ "${CONN}"
 
 export REACH_CONNECTOR_MODE="${CONN}"
 export REACH_DEBUG=1
@@ -21,19 +13,26 @@ case "${CONN}" in
   ETH) TIMEOUT=$((5 * 60)) ;;
 esac
 
+CIRCLE_ARTIFACTS_URL=$(curl -X GET "https://circleci.com/api/v2/project/github/reach-sh/reach-lang/${CIRCLE_BUILD_NUM}/artifacts" \
+  -H "Accept: application/json" \
+  -u "${CIRCLE_API_TOKEN}:")
+
 cd ../examples || exit 1
 go() {
   WHICH="$1"
-  banner "${WHICH}" - clean
-  ./one.sh clean "${WHICH}"
+  echo "${WHICH}"
+  THIS="${CONN}.${WHICH}"
+  THIS_URL="${CIRCLE_ARTIFACTS_URL}/${THIS}"
+  THIS_ART="/tmp/artifacts/${THIS}"
+  touch "${THIS_ART}"
+  ./one.sh clean "${WHICH}" >>"${THIS_ART}"
   STATUS="fail"
-  banner "${WHICH}" - build
-  if ./one.sh build "${WHICH}" ; then
+  if ./one.sh build "${WHICH}" >>"${THIS_ART}" ; then
     # We are using foreground to get around the lack of TTY allocation that
     # inhibits docker-compose run. I am worried that this will be ineffective
     # at stopping the containers
     # ^ XXX it actually doesn't enforce things properly for tut-7-rpc
-    timeout --foreground "${TIMEOUT}" ./one.sh run "${WHICH}"
+    timeout --foreground "${TIMEOUT}" ./one.sh run "${WHICH}" >>"${THIS_ART}"
     EXIT=$?
     if [ $EXIT -eq 124 ] ; then
       echo Timeout
@@ -41,12 +40,9 @@ go() {
     elif [ $EXIT -eq 0 ] ; then
       STATUS="pass"
     fi
-    banner "${WHICH}" - down
-    ./one.sh down "${WHICH}"
+    ./one.sh down "${WHICH}" >>"${THIS_ART}"
   fi
-  # XXX output results in the JUnit test format so we can go to
-  # ${EXAMPLE_URL}/tests/ETH on a failure?
-  echo "[ \"${STATUS}\", \"${CIRCLE_BUILD_URL}\" ]" > /tmp/workspace/record/"${CONN}.${WHICH}"
+  echo "[ \"${STATUS}\", \"${THIS_URL}\" ]" >/tmp/workspace/record/"${THIS}"
 }
 for WHICH in $(find . -maxdepth 1 -type d | sed 'sX./XX' | sort | tail -n +2 | awk "NR % ${SIZE} == ${RANK}") ; do
   go "${WHICH}" &
