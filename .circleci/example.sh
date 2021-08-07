387cea14e98cdeb4fd7cd54e0bce5d0573a09d61
Jay McCarthy <jay.mccarthy@gmail.com>
2021-08-07 05:45:44 -0400

    fixes

--- .circleci/example.sh
+++ .circleci/example.sh
@@ -13,26 +13,21 @@ case "${CONN}" in
   ETH) TIMEOUT=$((5 * 60)) ;;
 esac
 
-CIRCLE_ARTIFACTS_URL=$(curl -X GET "https://circleci.com/api/v2/project/github/reach-sh/reach-lang/${CIRCLE_BUILD_NUM}/artifacts" \
-  -H "Accept: application/json" \
-  -u "${CIRCLE_API_TOKEN}:")
-
 cd ../examples || exit 1
 go() {
   WHICH="$1"
   echo "${WHICH}"
   THIS="${CONN}.${WHICH}"
-  THIS_URL="${CIRCLE_ARTIFACTS_URL}/${THIS}"
   THIS_ART="/tmp/artifacts/${THIS}"
   touch "${THIS_ART}"
   ./one.sh clean "${WHICH}" >>"${THIS_ART}"
   STATUS="fail"
-  if ./one.sh build "${WHICH}" >>"${THIS_ART}" ; then
+  if ./one.sh build "${WHICH}" >>"${THIS_ART}" 2>&1 ; then
     # We are using foreground to get around the lack of TTY allocation that
     # inhibits docker-compose run. I am worried that this will be ineffective
     # at stopping the containers
     # ^ XXX it actually doesn't enforce things properly for tut-7-rpc
-    timeout --foreground "${TIMEOUT}" ./one.sh run "${WHICH}" >>"${THIS_ART}"
+    timeout --foreground "${TIMEOUT}" ./one.sh run "${WHICH}" >>"${THIS_ART}" 2>&1
     EXIT=$?
     if [ $EXIT -eq 124 ] ; then
       echo Timeout
@@ -40,9 +35,8 @@ go() {
     elif [ $EXIT -eq 0 ] ; then
       STATUS="pass"
     fi
-    ./one.sh down "${WHICH}" >>"${THIS_ART}"
   fi
-  echo "[ \"${STATUS}\", \"${THIS_URL}\" ]" >/tmp/workspace/record/"${THIS}"
+  echo "[ \"${STATUS}\", \"${CONN}.${RANK}\" ]" >/tmp/workspace/record/"${THIS}"
 }
 for WHICH in $(find . -maxdepth 1 -type d | sed 'sX./XX' | sort | tail -n +2 | awk "NR % ${SIZE} == ${RANK}") ; do
   go "${WHICH}" &
