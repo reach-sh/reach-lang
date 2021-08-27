1ac67baf06eaca3544ef1f47f6be680136a67753
brohamgoham <mohammed@MoHamGoHam.localdomain>
2021-08-27 11:57:12 -0400

    newTestAcc using ganacheSigner as faucet

--- examples/ganache/index.mjs
+++ examples/ganache/index.mjs
@@ -5,31 +5,14 @@ import ganache from 'ganache-core';
 const stdlib = loadStdlib();
 
 (async () => {
-  // OPTIONAL GANACHE CONFIG THAT CAN BE PASSED into ganache.provier()
-  const options = {
-    total_accounts: 1000,
-    gasPrice: '21000',
-    gasLimit: '5000000',
-    hardfork: "petersburg",
-    default_balance_ether: 7777777,
-    port: 7545,
-    ws: true,
-    vmErrorsonRPCResponse: true,
-    debug: true,
-    allowUnlimitedContractSize: true
-  };
-  //pass ganacheProvider into setProvider to run a reach program in a ganache env
-  const ganacheProvider = new ethers.providers.Web3Provider(ganache.provider(options));
+  const ganacheProvider = new ethers.providers.Web3Provider(ganache.provider());
   await stdlib.setProvider(ganacheProvider);
-  const accAlice = await stdlib.connectAccount(ganacheProvider.getSigner(999));
 
-  //Alice connects to ganacheAcc #999 with default balance of 7777777ETH
-  const balance = await stdlib.balanceOf(accAlice);
-  const addr = stdlib.formatAddress(accAlice);
-  const bal = stdlib.formatCurrency(balance);
-  console.log(ganacheProvider);
-  console.log([`${addr} has ${bal} ${stdlib.standardUnit}`]);
+  const faucet = ganacheProvider.getSigner();
+  await stdlib.setFaucet(stdlib.connectAccount(faucet));
 
+  const startingBalance = stdlib.parseCurrency(10);
+  const accAlice = await stdlib.newTestAccount(startingBalance)
 
   const ctcAlice = accAlice.deploy(backend);
   await Promise.all([
@@ -37,5 +20,4 @@ const stdlib = loadStdlib();
       ...stdlib.hasRandom,
     }),
   ]);
-  console.log('Alice has the correct balance of ganache default 7777777 ETH;')
 })();
