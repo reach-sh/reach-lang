a30cdd1251a2e03aab889dac143297f84eb85839
Matt Audesse <matt@mattaudesse.com>
2021-08-28 11:10:10 -0400

    reach'/#1271: eliminate docker-compose from tut-7-rpc

--- examples/tut-7-rpc/sbin/run-with.sh
+++ examples/tut-7-rpc/sbin/run-with.sh
@@ -4,21 +4,31 @@
 set -e
 
 CLIENT="$1"
-CONTAINER="tut-7-rpc_client-${CLIENT}-only_1"
+
+REACH_RPC_PORT=3000
+REACH_RPC_TLS_REJECT_UNVERIFIED=0
 REACH_RPC_KEY=$(cat REACH_RPC_KEY.txt)
-export REACH_RPC_KEY
+
+export REACH_RPC_PORT REACH_RPC_TLS_REJECT_UNVERIFIED REACH_RPC_KEY
 
 if [ "$CLIENT" = "" ]; then
   echo 'Error: An argument to this script is required'
   exit 1
 fi
 
-if docker container inspect "$CONTAINER" >/dev/null 2>&1; then
-  docker rm -f "$CONTAINER"
-  echo
-fi
-
 echo "Running client ${CLIENT}..."
-docker-compose -f "docker-compose.yml" up --remove-orphans --force-recreate "client-${CLIENT}-only"
+REACH_RPC_SERVER="127.0.0.1" ../../reach rpc-server-await
+
+cd "client-$CLIENT"
+docker run --rm \
+  -e         "REACH_RPC_SERVER=host.docker.internal" \
+  -e         "REACH_RPC_PORT" \
+  -e         "REACH_RPC_TLS_REJECT_UNVERIFIED" \
+  -e         "REACH_RPC_KEY" \
+  --add-host "host.docker.internal:172.17.0.1" \
+  --name     "reach-app-rpc-client-$CLIENT" \
+  --network  reach-devnet \
+  "reachsh/reach-app-tut-7-rpc-client-$CLIENT:latest"
+
 echo "Client ${CLIENT} completed."
 echo
