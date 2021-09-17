cc6963a14bf38f03e94a6ee88a201d5765c14371
Vinicius Mazzi <vinicius.mazzi@gmail.com>
2021-09-17 09:20:56 -0300

    Remove script

--- scripts/get-ipaddress.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#echo $(/usr/sbin/ip a ls $(/usr/sbin/ip r | grep default | awk '{print $NF}') | grep inet\ | awk '{print $2}' | awk -F \/ '{print $1}')
-echo /usr/sbin/ifconfig $(route  | grep default | awk '{print $NF}') | grep inet\ | awk '{print $2}' | awk -F \/ '{print $1}'
