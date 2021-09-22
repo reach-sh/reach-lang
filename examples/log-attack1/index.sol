cb1304801e1b38cac3bae271741f26e49f4ba75d
Jay McCarthy <jay.mccarthy@gmail.com>
2021-09-22 13:41:21 -0400

    evts

--- examples/log-attack1/index.sol
+++ examples/log-attack1/index.sol
@@ -1,26 +1,29 @@
 pragma abicoder v2;
 pragma solidity ^0.8.0;
 
-struct T2 {
-  address payable v52;
-  uint256 v53;
+struct T0 {
+  address payable v49;
   }
-struct T3 {
-  bool svs;
-  T2 msg;
+struct T4 {
+  address payable v59;
+  uint256 v60;
+  }
+struct T5 {
+  T0 svs;
+  T4 msg;
   }
 contract LogAttack1 {
   
-  event e1(T3 _a);
+  event e1(T5 _a);
 
   constructor () payable {
     }
 
   function m1(address payable acct, uint256 amt) external payable {
-    T3 memory a;
-    a.svs = false;
-    a.msg.v52 = acct;
-    a.msg.v53 = amt + 1;
-     emit e1(a);
+    T5 memory a;
+    a.svs.v49 = acct;
+    a.msg.v59 = acct;
+    a.msg.v60 = amt + 1;
+    emit e1(a);
   }
 }
