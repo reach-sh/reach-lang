711eeda4b3e1bf4f8bcb33073845bba7d32ff290
Chris Nevers <christophernevers96@gmail.com>
2021-09-27 16:16:28 -0400

    Add script to add external test example

---
+++ scripts/mk-ext-test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Creates a directory in `examples` to test Reach code external to this project.
+#
+# Usage:
+#   ./mk-ext-test.sh <dir-name> <git-url>
+
+NAME="$1"
+REPO="$2"
+
+cd ../examples  || exit 1
+mkdir $NAME     || exit 2
+cd $NAME        || exit 3
+
+touch Makefile
+
+cat >>Makefile <<END
+all: build
+
+REACH = ../../reach
+
+.PHONY: clean
+clean:
+	rm -f index.rsh index.mjs
+	rm -rf ${NAME}
+
+checkout:
+	if test -d ${NAME}; then echo "Repo already cloned"; else git clone ${REPO} ${NAME}; fi
+
+.PHONY: build
+build: checkout
+	cd ${NAME} && make build
+
+.PHONY: run
+run:
+	echo "external code will not be ran"
+END
