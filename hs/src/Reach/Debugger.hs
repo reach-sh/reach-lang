c2ac85dfa8c1b16b131839e3693d0739232618ee
Chike Abuah <abuahchu@gmail.com>
2021-10-20 20:44:35 -0400

    interp typeclass, add store to monad, progress on expr interpreter

--- hs/src/Reach/Debugger.hs
+++ hs/src/Reach/Debugger.hs
@@ -3,12 +3,14 @@
 module Reach.Debugger where
 
 import Control.Monad.Reader
-import Data.IORef
+-- import Data.IORef
 import qualified Data.Map.Strict as M
 import Reach.AST.Base
 import Reach.AST.DLBase
 import Reach.AST.LL
 -- import Data.Either
+import Data.Bits
+import Control.Exception
 
 type ConsensusEnv = M.Map DLVar DLVal
 type Store = ConsensusEnv
@@ -68,8 +70,12 @@ data DLVal
 
 type App = ReaderT Env IO
 
+-- data Env = Env
+--   { e_state :: IORef Session
+--   }
+
 data Env = Env
-  { e_state :: IORef Session
+  { e_store :: Store
   }
 
 -- Identify program states
@@ -84,122 +90,171 @@ type Params = String -- JSON.Value
 
 -- ## INTERPRETER ## --
 
-interpPrimOp :: [DLArg] -> PrimOp -> App DLVal
-interpPrimOp dlargs = \case
-  (ADD ) -> undefined
-  (SUB ) -> undefined
-  (MUL ) -> undefined
-  (DIV ) -> undefined
-  (MOD ) -> undefined
-  (PLT ) -> undefined
-  (PLE ) -> undefined
-  (PEQ ) -> undefined
-  (PGE ) -> undefined
-  (PGT ) -> undefined
-  (IF_THEN_ELSE ) -> undefined
-  (DIGEST_EQ ) -> undefined
-  (ADDRESS_EQ ) -> undefined
-  (TOKEN_EQ ) -> undefined
-  (SELF_ADDRESS ) -> undefined
-  (LSH ) -> undefined
-  (RSH ) -> undefined
-  (BAND ) -> undefined
-  (BIOR ) -> undefined
-  (BXOR ) -> undefined
-  (BYTES_CONCAT ) -> undefined
+class Interp a where
+  interp :: a -> App DLVal
 
-interpExpr :: Store -> DLExpr -> App DLVal
-interpExpr st = \case
-  (DLE_Arg _at dlarg) -> case dlarg of
-    (DLA_Var dlvar) -> return $ st M.! dlvar
-    (DLA_Constant _dlconst) -> return $ V_UInt $ toInteger (maxBound :: Int)
-    (DLA_Literal dllit) -> case dllit of
-      (DLL_Null) -> return $ V_Null
-      (DLL_Bool bool) -> return $ V_Bool bool
-      (DLL_Int _at int) -> return $ V_UInt int
-      (DLL_Bytes bytes) -> return $ V_Bytes $ show bytes
+data InterpreterException = InterpreterException
+  deriving Show
+
+instance Exception InterpreterException
+
+type PrimExpr = (PrimOp, [DLVal])
+
+instance Interp PrimExpr where
+  interp = \case
+    (ADD, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ (+) lhs rhs
+    (SUB, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ (-) lhs rhs
+    (MUL, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ (*) lhs rhs
+    (DIV, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ div lhs rhs
+    (MOD, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ mod lhs rhs
+    (PLT, [V_UInt lhs,V_UInt rhs]) -> return $ V_Bool $ (<) lhs rhs
+    (PLE, [V_UInt lhs,V_UInt rhs]) -> return $ V_Bool $ (<=) lhs rhs
+    (PEQ, [V_UInt lhs,V_UInt rhs]) -> return $ V_Bool $ (==) lhs rhs
+    (PGE, [V_UInt lhs,V_UInt rhs]) -> return $ V_Bool $ (>=) lhs rhs
+    (PGT, [V_UInt lhs,V_UInt rhs]) -> return $ V_Bool $ (>) lhs rhs
+    (IF_THEN_ELSE, [V_Bool cond, cond_val, alt]) -> do
+      return $ if cond then cond_val else alt
+    (DIGEST_EQ,  [V_Digest lhs,V_Digest rhs]) -> return $ V_Bool $ (==) lhs rhs
+    (ADDRESS_EQ,  [V_Address lhs,V_Address rhs]) -> return $ V_Bool $ (==) lhs rhs
+    (TOKEN_EQ, [V_Bytes lhs,V_Bytes rhs]) -> return $ V_Bool $ (==) lhs rhs
+    -- TODO QUESTION
+    (SELF_ADDRESS, _) -> undefined
+    (LSH, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ shiftL lhs (fromIntegral rhs)
+    (RSH, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ shiftR lhs (fromIntegral rhs)
+    (BAND, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ (.&.) lhs rhs
+    (BIOR, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ (.|.) lhs rhs
+    (BXOR, [V_UInt lhs,V_UInt rhs]) -> return $ V_UInt $ xor lhs rhs
+    (BYTES_CONCAT, [V_Bytes lhs,V_Bytes rhs]) -> return $ V_Bytes $ (++) lhs rhs
+
+instance Interp DLArg where
+  interp = \case
+    DLA_Var dlvar -> do
+      stt <- asks e_store
+      return $ stt M.! dlvar
+    DLA_Constant _dlconst -> return $ V_UInt $ toInteger (maxBound :: Int)
+    DLA_Literal dllit -> interp dllit
     -- TODO: handle interact case
-    (DLA_Interact _slpart _string _dltype) -> undefined
-  (DLE_LArg at dllargearg) -> case dllargearg of
-    (DLLA_Array _dltype dlargs) -> do
-      evd_args <- mapM (\arg -> interpExpr st $ DLE_Arg at arg) dlargs
-      return $ V_Array evd_args
-    (DLLA_Tuple dlargs) -> do
-      evd_args <- mapM (\arg -> interpExpr st $ DLE_Arg at arg) dlargs
-      return $ V_Tuple evd_args
-    (DLLA_Obj map_strs_to_dlargs) -> do
-      evd_args <- mapM (\arg -> interpExpr st $ DLE_Arg at arg) map_strs_to_dlargs
-      return $ V_Object evd_args
-    -- QUESTION: why are there types instead of dlargs?
-    (DLLA_Data _map_slvars_to_dltypes _string _dlarg) -> undefined
-    (DLLA_Struct assoc_slvars_dlargs) -> do
-      evd_args <- mapM (\arg -> interpExpr st $ DLE_Arg at arg) $ M.fromList assoc_slvars_dlargs
-      return $ V_Struct $ M.toList evd_args
-  (DLE_Impossible _at _impossible_error) -> error "ImpossibleError"
-  (DLE_PrimOp _at primop dlargs) -> interpPrimOp dlargs primop
-  (DLE_ArrayRef _at _dlarg1 _dlarg2) -> undefined
-  (DLE_ArraySet _at _dlarg1 _dlarg2 _dlarg3) -> undefined
-  (DLE_ArrayConcat _at _dlarg1 _dlarg2) -> undefined
-  (DLE_ArrayZip _at _dlarg1 _dlarg2) -> undefined
-  (DLE_TupleRef _at _dlarg _integer) -> undefined
-  (DLE_ObjectRef _at _dlarg _string) -> undefined
-  (DLE_Interact _at _slcxtframes _slpart _string _dltype _dlargs) -> undefined
-  (DLE_Digest _at _dlargs) -> undefined
-  (DLE_Claim _at _slcxtframes _claimtype _dlarg _maybe_bytestring) -> undefined
-  (DLE_Transfer _at _dlarg1 _dlarg2 _maybe_dlarg) -> undefined
-  (DLE_TokenInit _at _dlarg) -> undefined
-  (DLE_CheckPay _at _slcxtframes _dlarg _maybe_dlarg) -> undefined
-  (DLE_Wait _at _dltimearg) -> undefined
-  (DLE_PartSet _at _slpart _dlarg) -> undefined
-  (DLE_MapRef _at _dlm_var _dlarg) -> undefined
-  (DLE_MapSet _at _dlm_var _dlarg _maybe_dlarg) -> undefined
-  (DLE_Remote _at _slcxtframes _dlarg _string _dlpayamnt _dlargs _dlwithbill) -> undefined
-  (DLE_TokenNew _at _dltokennew) -> undefined
-  (DLE_TokenBurn _at _dlarg1 _dlarg2) -> undefined
-  (DLE_TokenDestroy _at _dlarg) -> undefined
+    DLA_Interact _slpart _string _dltype -> undefined
 
-interpStmt :: Store -> DLStmt -> App Store
-interpStmt st = \case
-  (DL_Nop _at) -> return st
-  (DL_Let _at _let_var _expr) -> undefined
-  (DL_ArrayMap _at _var1 _arg _var2 _block) -> undefined
-  (DL_ArrayReduce _at _var1 _arg1 _arg2 _var2 _var3 _block) -> undefined
-  (DL_Var _at _var) -> undefined
-  (DL_Set _at _var _arg) -> undefined
-  (DL_LocalDo _at _tail) -> undefined
-  (DL_LocalIf _at _arg _tail1 _tail2) -> undefined
-  (DL_LocalSwitch _at _var _switch_cases) -> undefined
-  (DL_Only _at _either_part _tail) -> undefined
-  (DL_MapReduce _at _int _var1 _dlm_var _arg _var2 _var3 _block) -> undefined
+instance Interp DLLiteral where
+  interp = \case
+    DLL_Null -> return $ V_Null
+    DLL_Bool bool -> return $ V_Bool bool
+    DLL_Int _at int -> return $ V_UInt int
+    DLL_Bytes bytes -> return $ V_Bytes $ show bytes
 
-interpTailStmt :: Store -> DLTail -> App Store
-interpTailStmt st = \case
-  (DT_Return _at) -> return st
-  (DT_Com stmt dltail) -> do
-    st' <- interpStmt st stmt
-    interpTailStmt st' dltail
+instance Interp DLExpr where
+  interp = \case
+    DLE_Arg _at dlarg -> interp dlarg
+    DLE_LArg _at dllargearg -> case dllargearg of
+      DLLA_Array _dltype dlargs -> V_Array <$> mapM interp dlargs
+      DLLA_Tuple dlargs -> V_Tuple <$> mapM interp dlargs
+      DLLA_Obj map_strs_to_dlargs -> V_Object <$> mapM interp map_strs_to_dlargs
+      DLLA_Data _map_slvars_to_dltypes string dlarg -> do
+        evd_arg <- interp $ dlarg
+        return $ V_Data $ M.singleton string evd_arg
+      DLLA_Struct assoc_slvars_dlargs -> do
+        evd_args <- mapM (\arg -> interp arg) $ M.fromList assoc_slvars_dlargs
+        return $ V_Struct $ M.toList evd_args
+    DLE_Impossible _at _impossible_error -> error "ImpossibleError"
+    DLE_PrimOp _at primop dlargs -> do
+      evd_args <- mapM interp dlargs
+      interp $ (primop,evd_args)
+    DLE_ArrayRef _at dlarg1 dlarg2 -> do
+      ev1 <- interp dlarg1
+      ev2 <- interp dlarg2
+      case (ev1,ev2) of
+        (V_Array arr, V_UInt n) -> return $ arr !! (fromIntegral n)
+        _ -> throw InterpreterException
+    DLE_ArraySet _at dlarg1 dlarg2 dlarg3 -> do
+      ev1 <- interp dlarg1
+      ev2 <- interp dlarg2
+      ev3 <- interp dlarg3
+      case (ev1,ev2) of
+        (V_Array arr, V_UInt n) -> do
+          let n' = fromIntegral n
+          return $ V_Array $ take n' arr ++ [ev3] ++ drop (n' + 1) arr
+        _ -> throw InterpreterException
+    DLE_ArrayConcat _at dlarg1 dlarg2 -> do
+      ev1 <- interp dlarg1
+      ev2 <- interp dlarg2
+      case (ev1,ev2) of
+        (V_Array arr1, V_Array arr2) -> return $ V_Array $ arr1 ++ arr2
+        _ -> throw InterpreterException
+    DLE_ArrayZip _at dlarg1 dlarg2 -> do
+      ev1 <- interp dlarg1
+      ev2 <- interp dlarg2
+      case (ev1,ev2) of
+        (V_Array arr1, V_Array arr2) -> do
+          return $ V_Array $ map (\(l,r)-> V_Array $ [l,r]) $ zip arr1 arr2
+        _ -> throw InterpreterException
+    DLE_TupleRef _at dlarg n -> do
+      ev <- interp dlarg
+      case ev of
+        V_Tuple arr -> return $ arr !! (fromIntegral n)
+        _ -> throw InterpreterException
+    DLE_ObjectRef _at dlarg str -> do
+      ev <- interp dlarg
+      case ev of
+        V_Object obj -> return $ obj M.! str
+        _ -> throw InterpreterException
+    DLE_Interact _at _slcxtframes _slpart _string _dltype _dlargs -> undefined
+    DLE_Digest _at dlargs -> V_Digest <$> V_Array <$> mapM interp dlargs
+    DLE_Claim _at _slcxtframes _claimtype _dlarg _maybe_bytestring -> undefined
+    DLE_Transfer _at _dlarg1 _dlarg2 _maybe_dlarg -> undefined
+    DLE_TokenInit _at _dlarg -> undefined
+    DLE_CheckPay _at _slcxtframes _dlarg _maybe_dlarg -> undefined
+    DLE_Wait _at _dltimearg -> undefined
+    DLE_PartSet _at _slpart _dlarg -> undefined
+    DLE_MapRef _at _dlm_var _dlarg -> undefined
+    DLE_MapSet _at _dlm_var _dlarg _maybe_dlarg -> undefined
+    DLE_Remote _at _slcxtframes _dlarg _string _dlpayamnt _dlargs _dlwithbill -> undefined
+    DLE_TokenNew _at _dltokennew -> undefined
+    DLE_TokenBurn _at _dlarg1 _dlarg2 -> undefined
+    DLE_TokenDestroy _at _dlarg -> undefined
+
+instance Interp DLStmt where
+  interp = \case
+    DL_Nop _at -> return V_Null
+    DL_Let _at _let_var _expr -> undefined
+    DL_ArrayMap _at _var1 _arg _var2 _block -> undefined
+    DL_ArrayReduce _at _var1 _arg1 _arg2 _var2 _var3 _block -> undefined
+    DL_Var _at _var -> undefined
+    DL_Set _at _var _arg -> undefined
+    DL_LocalDo _at _tail -> undefined
+    DL_LocalIf _at _arg _tail1 _tail2 -> undefined
+    DL_LocalSwitch _at _var _switch_cases -> undefined
+    DL_Only _at _either_part _tail -> undefined
+    DL_MapReduce _at _int _var1 _dlm_var _arg _var2 _var3 _block -> undefined
+
+interpTailStmt :: DLTail -> App ()
+interpTailStmt = \case
+  DT_Return _at -> return ()
+  DT_Com stmt dltail -> do
+    _ <- interp stmt
+    interpTailStmt dltail
 
 interpCons :: LLConsensus -> App ()
 interpCons = \case
-  (LLC_Com _stmt _cons) -> undefined
-  (LLC_If _at _arg _cons1 _cons2) -> undefined
-  (LLC_Switch _at _var _switch_cases) -> undefined
-  (LLC_FromConsensus _at1 _at2 _step) -> undefined
-  (LLC_While _at _asn _inv _cond _body _k) -> undefined
-  (LLC_Continue _at _asn) -> undefined
-  (LLC_ViewIs _at _part _var _export _cons) -> undefined
+  LLC_Com _stmt _cons -> undefined
+  LLC_If _at _arg _cons1 _cons2 -> undefined
+  LLC_Switch _at _var _switch_cases -> undefined
+  LLC_FromConsensus _at1 _at2 _step -> undefined
+  LLC_While _at _asn _inv _cond _body _k -> undefined
+  LLC_Continue _at _asn -> undefined
+  LLC_ViewIs _at _part _var _export _cons -> undefined
 
-interpStep :: Store -> (LLStep -> LLProg) -> LLStep -> App (Store)
-interpStep st pmeta = \case
-  (LLS_Com stmt step) -> do
-    st' <- interpStmt st stmt
-    interpStep st' pmeta step
-  (LLS_Stop _loc) -> return st
-  (LLS_ToConsensus _at _tc_send _tc_recv _tc_mtime) -> undefined
+interpStep :: (LLStep -> LLProg) -> LLStep -> App ()
+interpStep pmeta = \case
+  LLS_Com stmt step -> do
+    _ <- interp stmt
+    interpStep pmeta step
+  LLS_Stop _loc -> return ()
+  LLS_ToConsensus _at _tc_send _tc_recv _tc_mtime -> undefined
 
 -- evaluate a linear Reach program
-interp :: Store -> LLProg -> App (Store)
-interp st (LLProg at llo ps dli dex dvs step) = interpStep st (LLProg at llo ps dli dex dvs) step
+interp1 :: LLProg -> App ()
+interp1 (LLProg at llo ps dli dex dvs step) = interpStep (LLProg at llo ps dli dex dvs) step
 
 -- interpM :: Store -> LLProg -> App (Store)
 -- interpM st mprog = do
