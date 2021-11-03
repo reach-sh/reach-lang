b4ea0db01743c441865b021d4c189a471dccdb0f
Morgan Wilde <morgan@wilde.work>
2021-11-03 23:00:10 +0200

    Removed deprecated calls to deploy and attach.
    When compiling with Reach `0.1.6` the cli produces deprecation warnings when using `deploy` and `attach`. The suggested replacement is `contract`.

--- examples/tut-2/index.mjs
+++ examples/tut-2/index.mjs
@@ -7,8 +7,8 @@ const stdlib = loadStdlib(process.env);
   const accAlice = await stdlib.newTestAccount(startingBalance);
   const accBob = await stdlib.newTestAccount(startingBalance);
 
-  const ctcAlice = accAlice.deploy(backend);
-  const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
+  const ctcAlice = accAlice.contract(backend);
+  const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
   await Promise.all([
     backend.Alice(ctcAlice, {
