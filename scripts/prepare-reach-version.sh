3b85203d781ebfbbee06d5874f04cc3e9bb8e626
Vinicius Mazzi <vinicius.mazzi@gmail.com>
2021-11-09 09:11:52 -0300

    Test

---
+++ scripts/prepare-reach-version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. ../VERSION.mo
+
+rccount() {
+  rc_exists=$(git tag | grep $VERSION | grep rc > /dev/null ; echo $?)
+  if [ $rc_exists -eq 0 ] ; then
+    rc=$(($(git tag | grep $VERSION | grep rc | sort | tail -1 | awk -F '.' '{ print $NF }')+1)) 
+  else
+    rc=0
+  fi
+  echo $rc
+}
+
+if [ "${RC_RELEASE}" = "TRUE" ]; then
+ rc=$(rccount)
+ export tag="-rc.${rc}"
+ export rc=".${rc}"
+else
+ export tag=""
+ export rc=""
+fi
+
+mo ../VERSION.mo > ../VERSION
