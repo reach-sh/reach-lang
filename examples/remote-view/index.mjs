1ebf43befb18069d603c4d48dad21b0bdf3663f0
Chris Nevers <christophernevers96@gmail.com>
2021-11-10 10:16:01 -0500

    Skip CFX test

--- examples/remote-view/index.mjs
+++ examples/remote-view/index.mjs
@@ -7,7 +7,7 @@ import * as fs from 'fs';
 const stdlib = loadStdlib(process.env);
 
 (async () => {
-  if ( stdlib.connector === 'ALGO' ) { return; }
+  if ( ['ALGO', 'CFX'].includes(stdlib.connector) ) { return; }
   const ethers = stdlib.connector === 'CFX' ? cfxers : real_ethers;
 
   const startingBalance = stdlib.parseCurrency(100);
@@ -17,6 +17,11 @@ const stdlib = loadStdlib(process.env);
 
   const gasLimit = 5000000;
   accAlice.setGasLimit(gasLimit);
+  accBob.setGasLimit(gasLimit);
+  if (stdlib.connector == 'CFX') {
+    accAlice.setStorageLimit(10000);
+    accBob.setStorageLimit(10000);
+  }
   const compiled = JSON.parse(await fs.readFileSync('./build/index.sol.json'));
   const remoteCtc = compiled["contracts"]["index.sol:MyContract"];
   const remoteABI = remoteCtc["abi"];
@@ -37,8 +42,9 @@ const stdlib = loadStdlib(process.env);
       keepGoing: async () => {
         const kg = count < 5;
         const ctcAddr = await ctcAlice.getContractAddress();
-        const x = await contract.callStatic["callView"](ctcAddr);
-        console.log(`Compare: `, x.toNumber(), count);
+        const f = (stdlib.connector == 'CFX') ? contract : contract.callStatic;
+        const x = await f["callView"](ctcAddr);
+        console.log(`Compare: `, x, count);
         console.assert(x == count, "x == count");
         count = count + 1;
         return [kg, count];
