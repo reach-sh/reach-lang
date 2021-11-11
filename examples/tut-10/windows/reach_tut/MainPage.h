ed4ed73717e5d2b27e6274656812fca0837715f1
mobileguru007 <zhezui727@gmail.com>
2021-11-12 03:21:49 +0800

    Added MacOS & Windows Module

---
+++ examples/tut-10/windows/reach_tut/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::reach_tut::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::reach_tut::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
