591f5d165caf970fd2be4a13cc8f105e27e4fbf6
Chike Abuah <abuahchu@gmail.com>
2021-11-16 05:31:59 -0500

    add unblock post request

--- hs/src/Reach/SimServer.hs
+++ hs/src/Reach/SimServer.hs
@@ -9,13 +9,16 @@ import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics
 import qualified Reach.Simulator as S
 
+type StateId = Int
+type ActionId = Int
+
 data State = State
-  { state_id :: Int
+  { state_id :: StateId
   }
   deriving (Show, Generic)
 
 data Action = Action
-  { action_id :: Int
+  { action_id :: ActionId
   , action_type :: S.Action
   }
   deriving (Generic)
@@ -28,13 +31,20 @@ instance FromJSON State
 instance ToJSON Action
 instance FromJSON Action
 
+-- TODO: need a way to load real programs from parser
 p :: Program
 p = -1
+
+initProg :: Program -> ()
+initProg = undefined
+
+unblockProg :: Program -> StateId -> ActionId -> S.DLVal -> ()
+unblockProg = undefined
 
 allStates :: Program -> [State]
 allStates = undefined
 
-actions :: Program -> State -> [Action]
+actions :: Program -> StateId -> [Action]
 actions = undefined
 
 matchesId :: Int -> (a -> Int) -> a -> Bool
@@ -45,25 +55,16 @@ main = scotty 3000 $ do
   get "/states" $ do
     json $ allStates p
 
-  get "/states/:id" $ do
-    i <- param "id"
-    json (filter (matchesId i state_id) $ allStates p)
+  get "/states/:s" $ do
+    s <- param "s"
+    json (filter (matchesId s state_id) $ allStates p)
 
-  -- get "/states/:id/actions" $ do
-  --   i <- param "id"
-  --   json $ actions p i
+  get "/states/:s/actions" $ do
+    s <- param "s"
+    json $ actions p s
 
-  -- get "/states/:id1/actions/:id2" $ do
-  --   i1 <- param "id1"
-  --   i2 <- param "id2"
-  --   json (filter (matchesId i2 action_id) $ actions p i1)
-
-
--- Identify program states
--- type Id = Integer
-
--- All of the states visited
--- type Session = M.Map Id State
-
--- free-form & untyped parameters for the action
--- type Params = String -- JSON.Value
+  post "/states/:s/actions/:a/?data=post_val" $ do
+    s <- param "s"
+    a <- param "a"
+    v <- param "post_val"
+    return $ unblockProg p s a $ S.V_UInt v
