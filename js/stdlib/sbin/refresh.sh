56d781f90c6c1de8dc21442ead2d2a4f2f2e6c0c
Vinicius Mazzi <vinicius.mazzi@gmail.com>
2021-11-16 10:43:35 -0300

    Test

--- js/stdlib/sbin/refresh.sh
+++ js/stdlib/sbin/refresh.sh
@@ -5,27 +5,15 @@ git diff --exit-code || (printf '\n\n\nGit repo is not clean; please commit firs
 
 IMAGE=reachsh/stdlib:latest
 HASH="$(docker run --entrypoint /bin/sh "$IMAGE" -c 'echo $REACH_GIT_HASH')"
-#if [ $(echo "$HASH" | wc -c) -eq 9 ]; then
-#  :
-#else
-#  set +x
-#  echo
-#  echo
-#  echo "Hash length looks wrong: ${HASH}"
-#  echo '(A trailing asterisk means the image was made from a dirty git repo)'
-#  echo
-#  exit 1
-#fi
 
 rm -f ./*.mjs ./*.d.ts
 rm -rf ./dist/
 docker run --entrypoint /bin/sh --volume "$(pwd):/cwd" "$IMAGE" \
   -c 'cp /stdlib/*.mjs /stdlib/package.json /cwd/ && cp -r /stdlib/dist /cwd/dist && cp /stdlib/dist/types/* /cwd/'
-  # -c 'cp /stdlib/*.mjs /stdlib/package.json /stdlib/*.d.ts /cwd/'
 
 
 sudo chown -R $(whoami) ./*
-# rm tester.mjs ETH-test.mjs
+
 rm ./version.mo.d.ts ./version.mo.mjs
 
 git add .
