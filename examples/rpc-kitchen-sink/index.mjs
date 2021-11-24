cd1223cbc2a98ca271c18482bdc17f1daceded58
Matt Audesse <matt@mattaudesse.com>
2021-11-24 18:38:58 -0500

    CORE-4: extend RPC `getExports` tests to include nested arrays

--- examples/rpc-kitchen-sink/index.mjs
+++ examples/rpc-kitchen-sink/index.mjs
@@ -18,7 +18,7 @@ import { mkRPC } from '@reach-sh/rpc-client';
 
   const accAlice = await rpc('/stdlib/newTestAccount', await rpc('/stdlib/parseCurrency', 100));
   const ctcAlice = await rpc('/acc/contract', accAlice);
-  const meta     = "This is a test string";
+  const meta     = 'This is a test string';
 
   /* Views */
   const checkViewFun = async (l, x, y, e) => {
@@ -34,18 +34,37 @@ import { mkRPC } from '@reach-sh/rpc-client';
   };
 
   /* Exports */
-  // TODO: how should we access nested elements in arrays?
   const five = await rpc('/backend/getExports/five');
+  const e1   = { f: [{ g: 'h' }] };
+
   assertEq('backend.getExports > app.timesTwoPlusThree(app.five):',
     await rpc('/stdlib/bigNumberify', 13),
     await rpc('/backend/getExports/timesTwoPlusThree', five));
 
   assertEq('backend.getExports > app.a.b.e:',
-    [ five, await rpc('/stdlib/bigNumberify', 6) ],
+    [ five, e1, await rpc('/stdlib/bigNumberify', 6) ],
     await rpc('/backend/getExports/a/b/e'));
+
+  assertEq('backend.getExports > app.a.b.e[0]:',
+    five,
+    await rpc('/backend/getExports/a/b/e/0'));
+
+  assertEq('backend.getExports > app.a.b.e[1]:',
+    e1,
+    await rpc('/backend/getExports/a/b/e/1'));
+
+  assertEq('backend.getExports > app.a.b.e[1].f[0]:',
+    { g: 'h' },
+    await rpc('/backend/getExports/a/b/e/1/f/0'));
+
+  assertEq('backend.getExports > app.a.b.e[2]:',
+    await rpc('/stdlib/bigNumberify', 6),
+    await rpc('/backend/getExports/a/b/e/2'));
 
   assertEq('backend.getExports > app.a.b.c:',         'd',  await rpc('/backend/getExports/a/b/c'));
   assertEq('backend.getExports > app.a.z.c:',         null, await rpc('/backend/getExports/a/z/c'));
+  assertEq('backend.getExports > app.a.z.e[3]:',      null, await rpc('/backend/getExports/a/z/e/3'));
+  assertEq('backend.getExports > app.a.z.e[3][0]:',   null, await rpc('/backend/getExports/a/z/e/3/0'));
   assertEq('backend.getExports > app.nonExistent:',   null, await rpc('/backend/getExports/nonExistent'));
   assertEq('backend.getExports > app.nonExistent.a:', null, await rpc('/backend/getExports/nonExistent/a'));
 
