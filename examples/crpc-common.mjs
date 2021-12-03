be137481ab06ef176f862bab27e248bca5670eed
Matt Audesse <matt@mattaudesse.com>
2021-12-03 16:31:20 -0500

    CORE-4: improve test case readability

--- examples/crpc-common.mjs
+++ examples/crpc-common.mjs
@@ -1,12 +1,16 @@
+import chalk from 'chalk';
+
 export const mkAssertEq = rpc => async (l, e, a) => {
   const f = x => JSON.stringify(x).replace(/\\u0000/g, '');
   const [ ej, aj ] = [ f(e), f(a) ];
   if (ej === aj) {
-    console.log(`${l} ${ej} === ${aj}`);
+    console.log(chalk.green.bold(`\u2714 ${l}`));
+    console.log('  Expected:', JSON.parse(ej));
+    console.log('    Actual:', JSON.parse(aj));
   } else {
-    console.log(`${l} *** Mismatch! ***`);
-    console.log(`  Expected: ${ej}`);
-    console.log(`    Actual: ${aj}`);
+    console.log(chalk.red.bold(`\u2717 ${l} *** Mismatch! ***`));
+    console.log('  Expected:', JSON.parse(ej));
+    console.log('    Actual:', JSON.parse(aj));
   }
   await rpc('/stdlib/assert', ej === aj);
 };
