05da403320cc4afbe10b880600a30b3bf30b5329
Vinicius Mazzi <vinicius.mazzi@gmail.com>
2021-12-06 10:09:52 -0300

    Test

--- .circleci/image.sh
+++ .circleci/image.sh
@@ -1,30 +1,28 @@
-#!/bin/sh
-#WHICH="$1"
-#ROOT=".."
-#TARGET="build-${WHICH}"
-#
-#case "$WHICH" in
-#  haskell-build-artifacts*|"reach"|"reach-cli")
-#    DIR="hs"
-#    ;;
-#  "js-deps"|"runner"|"react-runner"|"rpc-server"|stdlib*)
-#    DIR="js"
-#    ;;
-#  devnet-algo*)
-#    DIR="scripts/devnet-algo"
-#    ;;
-#  devnet-cfx*)
-#    DIR="scripts/devnet-cfx"
-#    ;;
-#  devnet-eth*)
-#    DIR="scripts/devnet-eth"
-#    ;;
-#  *)
-#    echo "No image: ${WHICH}"
-#    exit 1
-#    ;;
-#esac
-#cd "${ROOT}/${DIR}" || exit 1
-#exec make "${TARGET}"
-#
-echo $LAYERS
+!/bin/sh
+WHICH="$1"
+ROOT=".."
+TARGET="build-${WHICH}"
+
+case "$WHICH" in
+  haskell-build-artifacts*|"reach"|"reach-cli")
+    DIR="hs"
+    ;;
+  "js-deps"|"runner"|"react-runner"|"rpc-server"|stdlib*)
+    DIR="js"
+    ;;
+  devnet-algo*)
+    DIR="scripts/devnet-algo"
+    ;;
+  devnet-cfx*)
+    DIR="scripts/devnet-cfx"
+    ;;
+  devnet-eth*)
+    DIR="scripts/devnet-eth"
+    ;;
+  *)
+    echo "No image: ${WHICH}"
+    exit 1
+    ;;
+esac
+cd "${ROOT}/${DIR}" || exit 1
+exec make "${TARGET}"
