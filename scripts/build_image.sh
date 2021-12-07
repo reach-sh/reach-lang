d3fd7b5482636e4091063ab1eedf869eb76ceb8e
Vinicius Mazzi <vinicius.mazzi@gmail.com>
2021-12-07 06:25:19 -0300

    Test

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,20 +2,23 @@
 IMAGE_NAME=$1
 LAYERS=$2
 DOCKER_FILE=$3
-#DOCKER_PATH=$4
 shift 3
 ARGS=$@
+
+docker pull $IMAGE_NAME:${CIRCLE_BRANCH} || true
+docker pull $IMAGE_NAME:master | true
 
 CACHE_FROM="--cache-from=${IMAGE_NAME}:${CIRCLE_BRANCH} --cache-from=${IMAGE_NAME}:master"
 
 for i in $LAYERS; do
+    docker pull $IMAGE_NAME:${i}-${CIRCLE_BRANCH} || true
+    docker pull $IMAGE_NAME:${i}-master | true
 	CACHE_FROM="--cache-from=${IMAGE_NAME}:${i}-${CIRCLE_BRANCH} --cache-from=${IMAGE_NAME}:${i}-master ${CACHE_FROM}";
 done
 
 build_image () {
     LAYER=$1
     if [ "x${LAYER}" != "x" ]; then
-
         TAG="--tag=${IMAGE_NAME}:${LAYER}"
         TARGET="--target=${LAYER}"
     else
