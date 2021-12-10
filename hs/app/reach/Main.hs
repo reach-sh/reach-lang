fa400fd43cb47da79658845bc33072d588332f12
Chiké Abuah <abuahchu@gmail.com>
2021-12-10 16:41:54 -0500

    backgrounding server is actually necessary (#442)

--- hs/app/reach/Main.hs
+++ hs/app/reach/Main.hs
@@ -846,7 +846,7 @@ compile = command "compile" $ info f d where
     let CompilerOpts {..} = cta_co
     let v = versionBy majMinPat version''
     let ci' = if ci then "true" else ""
-    let ports = if elem "--sim" rawArgs then "-p 3001:3001" else ""
+    let ports = if (elem "--sim" rawArgs || co_sim) then "-p 3001:3001" else ""
     liftIO $ do
       diePathContainsParentDir co_source
       maybe (pure ()) diePathContainsParentDir co_mdirDotReach
