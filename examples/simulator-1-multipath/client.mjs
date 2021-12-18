55d227e78ff5a1fdde4f8e68d641be6b9bf1742d
Chike Abuah <abuahchu@gmail.com>
2021-12-18 10:48:32 -0500

    rewrite multipath tests

--- examples/simulator-1-multipath/client.mjs
+++ examples/simulator-1-multipath/client.mjs
@@ -7,29 +7,52 @@ const main = async () => {
   const params = {
     port: port
   };
-  await waitPort(params)
-  await c.ping()
+  await c.waitForPort(params)
   await c.load()
   await c.init()
+  await c.initFor(0,0)
+  await c.initFor(1,1)
 
   // 1ˢᵗ path
-  await c.respondWithVal(0,0,0,"Number")
-  await c.respondWithVal(1,1,"Alice","String")
-  await c.respondWithVal(2,2,1,"Number")
-  await c.respondWithVal(3,3,"Bob","String")
-  await c.respondWithVal(4,4,0,"Number")
-  await c.respondWithVal(5,5,0,"Number")
+  await c.changeActor(0)
+  await c.respondWithVal(2,2,0,"Number")
+  await c.changeActor(-1)
+  await c.respondWithVal(3,3,0,"Number")
+  await c.changeActor(0)
+  await c.respondWithVal(4,2,-99,"Number")
+  await c.changeActor(1)
+  await c.respondWithVal(5,5,-99,"Number")
+  await c.respondWithVal(6,6,1,"Number")
+  await c.changeActor(-1)
+  await c.respondWithVal(7,7,1,"Number")
+  await c.changeActor(0)
+  await c.respondWithVal(8,8,-99,"Number")
+  await c.changeActor(1)
+  await c.respondWithVal(9,9,-99,"Number")
+  await c.respondWithVal(10,10,-99,"Number")
 
   let r = await c.getStatus()
   assert.equal(r,"Done");
 
   // 2ⁿᵈ path
-  await c.respondWithVal(0,0,1,"Number")
-  await c.respondWithVal(7,7,"Alice","String")
-  await c.respondWithVal(8,8,0,"Number")
-  await c.respondWithVal(9,9,"Bob","String")
-  await c.respondWithVal(10,10,0,"Number")
-  await c.respondWithVal(11,11,0,"Number")
+  await c.changeActor(0)
+  await c.respondWithVal(2,2,1,"Number")
+  await c.changeActor(-1)
+  await c.respondWithVal(12,12,0,"Number")
+  await c.changeActor(0)
+  await c.respondWithVal(13,2,-99,"Number")
+  await c.changeActor(1)
+  await c.respondWithVal(14,14,-99,"Number")
+  await c.respondWithVal(15,15,0,"Number")
+  await c.changeActor(-1)
+  await c.respondWithVal(16,16,1,"Number")
+  await c.changeActor(0)
+  await c.respondWithVal(17,17,-99,"Number")
+  await c.changeActor(1)
+  await c.respondWithVal(18,18,-99,"Number")
+  await c.respondWithVal(19,19,-99,"Number")
+  r = await c.getStatus()
+  assert.equal(r,"Done");
 
   r = await c.getStatus()
   assert.equal(r,"Done");
