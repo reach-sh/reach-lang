716bd9ca8019290605f2dc2aaf498e5f263c6e23
Jay McCarthy <jay.mccarthy@gmail.com>
2022-01-01 15:24:32 -0500

    merge all image building together

---
+++ .circleci/build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+KIDS=0
+
+WORKSPACE="/tmp/workspace/docker"
+mkdir -p "${WORKSPACE}"
+DONE="/tmp/done"
+mkdir -p "${DONE}"
+
+imagek () {
+  IMAGE="$1"
+  shift 1
+  DEPS=( "$@" )
+  echo "Scheduling ${IMAGE}"
+
+  for DEP in "${DEPS[@]}" ; do
+    while [ ! -f "${DONE}/${DEP}" ] ; do
+      sleep 1
+    done
+    echo "${IMAGE} sees ${DEP} is done"
+  done
+
+  echo "Building ${IMAGE}"
+  ./image.sh "${IMAGE}" >>"/tmp/artifacts/${IMAGE}" 2>&1
+  touch "${DONE}/${IMAGE}"
+
+  docker save "reachsh/${IMAGE}:latest" | gzip > "${WORKSPACE}/${IMAGE}".tar.gz
+  "${ROOT}/scripts/cache-image.sh" "${IMAGE}"
+}
+
+image () {
+  KIDS=$((KIDS + 1))
+  imagek "$@" &
+}
+
+image "devnet-eth"
+image "devnet-cfx"
+image "devnet-algo"
+image "haskell-build-artifacts" "devnet-algo"
+image "reach" "haskell-build-artifacts"
+image "reach-cli" "haskell-build-artifacts"
+image "js-deps"
+image "stdlib" "reach" "js-deps"
+image "runner" "stdlib"
+image "react-runner" "stdlib" "js-deps"
+image "rpc-server" "runner"
+
+while true; do
+  wait -n || true
+  KIDS=$((KIDS - 1))
+  if [ $KIDS -eq 0 ]; then break; fi
+done
+
+wait
--- .circleci/make.sh
+++ .circleci/make.sh
@@ -5,59 +5,8 @@ TOTAL=$((TOTALP1 - 1))
 PRE=config.pre.yml
 MID=config.mid.yml
 END=config.end.yml
-IEND=config.iend.yml
 cat >"${MID}" </dev/null
 cat >"${END}" </dev/null
-cat >"${IEND}" </dev/null
-
-cat >>"${IEND}" <<END
-    - "build-sink":
-        requires:
-          - "hs-test"
-END
-
-deps () {
-  DEPS="$*"
-  cat >>"${MID}" <<END
-        deps: "$DEPS"
-END
-  if [ "x${DEPS}" != "x" ] ; then
-  cat >>"${MID}" <<END
-        requires:
-END
-  for DEP in "$@"; do
-    cat >>"${MID}" <<END
-          - "build-${DEP}"
-END
-  done
-  fi
-}
-
-image () {
-  EXEC="$1"
-  IMAGE="$2"
-  shift 2
-  NAME="build-${IMAGE}"
-  cat >>"${MID}" <<END
-    - "build-image":
-        name: "${NAME}"
-        image: "${IMAGE}"
-        exec: "${EXEC}"
-END
-  deps "$@"
-  cat >>"${IEND}" <<END
-          - "${NAME}"
-END
-}
-
-image "real" "haskell-build-artifacts" "devnet-algo"
-image "fake" "reach" "haskell-build-artifacts"
-image "fake" "reach-cli" "haskell-build-artifacts"
-image "real" "js-deps"
-image "real" "stdlib" "reach" "js-deps"
-image "fake" "runner" "stdlib"
-image "fake" "react-runner" "stdlib" "js-deps"
-image "fake" "rpc-server" "runner"
 
 cat >>"${END}" <<END
     - "examples-sink":
@@ -68,11 +17,10 @@ for CONN in ETH ALGO CFX ; do
   CONNlc=$(echo "${CONN}" | tr '[:upper:]' '[:lower:]')
   IMAGE="devnet-${CONNlc}"
   case "${CONN}" in
-    ALGO) PER=8 EXEC="fake";;
-    CFX) PER=8 EXEC="real";;
-    ETH) PER=16 EXEC="fake";;
+    ALGO) PER=8  ;;
+     CFX) PER=8  ;;
+     ETH) PER=16 ;;
   esac
-  image "${EXEC}" "${IMAGE}"
   SIZE=$(((TOTAL + (PER - 1)) / PER))
   NAME="examples.${CONN}"
   cat >>"${MID}" <<END
@@ -80,11 +28,13 @@ for CONN in ETH ALGO CFX ; do
         name: "${NAME}"
         connector: "${CONN}"
         size: ${SIZE}
+        deps: "${IMAGE}"
+        requires:
+          - "build"
 END
-  deps "reach" "reach-cli" "runner" "rpc-server" "${IMAGE}"
   cat >>"${END}" <<END
           - "${NAME}"
 END
 done
 
-cat "${PRE}" "${MID}" "${END}" "${IEND}" > config.gen.yml
+cat "${PRE}" "${MID}" "${END}" > config.gen.yml
