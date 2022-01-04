8fce85d83a364c5e14b4af134a05d2c06fcb696a
Chike Abuah <abuahchu@gmail.com>
2022-01-04 13:04:43 -0500

    restructure layout and display src code

--- examples/programmer-art/src/index.mjs
+++ examples/programmer-art/src/index.mjs
@@ -1,9 +1,51 @@
 import * as c from '@reach-sh/simulator-client';
 import cytoscape from 'cytoscape';
 import klay from 'cytoscape-klay';
+import Prism from 'prismjs';
 import "../scss/custom.scss";
 
+// NOTE: placeholder
+const rsh = `'reach 0.1';
+
+const Player = {
+  getHand: Fun([], UInt),
+  seeOutcome: Fun([UInt], Null),
+};
+
+export const main = Reach.App(() => {
+  const Alice = Participant('Alice', {
+    ...Player,
+  });
+  const Bob   = Participant('Bob', {
+    ...Player,
+  });
+  deploy();
+
+  Alice.only(() => {
+    const handAlice = declassify(interact.getHand());
+  });
+  Alice.publish(handAlice);
+  commit();
+
+  Bob.only(() => {
+    const handBob = declassify(interact.getHand());
+  });
+  Bob.publish(handBob);
+
+  const outcome = (handAlice + (4 - handBob)) % 3;
+  commit();
+
+  each([Alice, Bob], () => {
+    interact.seeOutcome(outcome);
+  });
+});
+`
+
 cytoscape.use( klay );
+
+const srcHtml = Prism.highlight(rsh, Prism.languages.javascript, 'javascript');
+const prism = document.querySelector("#cx")
+prism.innerHTML = srcHtml
 
 const redraw = async () => {
   let edges = await c.getEdges()
