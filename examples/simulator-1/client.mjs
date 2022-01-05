74cff8730ba35090da8a564955629701c58b9a17
Chiké Abuah <abuahchu@gmail.com>
2022-01-05 18:50:31 -0500

    test state of rps 3 + 5 (#490)
    * test state of rps 3 bets
    
    * test rps 5 trust state

--- examples/simulator-1/client.mjs
+++ examples/simulator-1/client.mjs
@@ -21,6 +21,11 @@ const getVar = async (v,n,aid) => {
   return x.l_locals[aid].l_store.find(el => el[0] == v)
 }
 
+const assertVar = (y,t,v) => {
+  assert.equal(y[1].tag,t);
+  assert.equal(y[1].contents,v);
+}
+
 const main = async () => {
   console.log("Init Testing!")
   await c.waitForPort()
@@ -40,13 +45,11 @@ const main = async () => {
   // Alice observes that her hand is published
   await c.respondWithVal(4,2,-99,0)
   let y = await getVar('handAlice',5,0)
-  assert.equal(y[1].tag,'V_UInt');
-  assert.equal(y[1].contents,0);
+  assertVar(y,'V_UInt',0)
   // Bob observes that Alice's hand is published
   await c.respondWithVal(5,5,-99,1)
   y = await getVar('handAlice',6,1)
-  assert.equal(y[1].tag,'V_UInt');
-  assert.equal(y[1].contents,0);
+  assertVar(y,'V_UInt',0)
   // Bob interactively gets his hand (1)
   await c.respondWithVal(6,6,1)
   // Bob's hand (1) is published
@@ -54,13 +57,11 @@ const main = async () => {
   // Alice observes that Bob's hand is published
   await c.respondWithVal(8,8,-99,0)
   y = await getVar('handBob',9,0)
-  assert.equal(y[1].tag,'V_UInt');
-  assert.equal(y[1].contents,1);
+  assertVar(y,'V_UInt',1)
   // Bob observes that his hand is published
   await c.respondWithVal(9,9,-99,1)
   y = await getVar('handBob',10,1)
-  assert.equal(y[1].tag,'V_UInt');
-  assert.equal(y[1].contents,1);
+  assertVar(y,'V_UInt',1)
   // Bob seeOutcome's
   await c.respondWithVal(10,10,-99)
   // Alice seeOutcome's
