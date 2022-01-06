3a5f8d1d9752b466aba4ee90252e1f28e656165c
Jay McCarthy <jay.mccarthy@gmail.com>
2022-01-06 05:43:23 -0500

    upgrade conflux

--- scripts/devnet-cfx/run.sh
+++ scripts/devnet-cfx/run.sh
@@ -2,6 +2,6 @@
 CFX_CONFIG=/default.toml
 export CFX_CONFIG
 
-./conflux --config "${CFX_CONFIG}" >/dev/null 2>&1 &
+./conflux --config "${CFX_CONFIG}" & #>/dev/null 2>&1 &
 cd /faucet
 exec npm start
