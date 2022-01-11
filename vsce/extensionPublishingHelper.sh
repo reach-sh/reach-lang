9aa55d46fd2f9bf3835889f0f974525eaa63a1f2
Hamir Mahal <hamirmahal@gmail.com>
2022-01-11 08:20:34 -0800

    Core 966 create dockerfile to help automate extension publishing (#502)
    * CORE-966 create `Dockerfile` to help publish ext
    
    * add quotation marks to make CircleCI sh-check pass
    
    * rename `PAT` to `KEY` to improve code readability
    
    * `Add a shebang or a 'shell' directive.` for Circle

---
+++ vsce/extensionPublishingHelper.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# ^-- SC2148: Tips depend on target
+# shell and yours is unknown. Add a
+# shebang or a 'shell' directive.
+
+echo "Building Docker image to publish"
+echo "version $VER of our extension..."
+# Give the new image a tag of "img".
+docker build -t img .
+
+# Give publish, "$VER", -p and "$KEY"
+# all as arguments to vsce, which is the
+# tool we use to publish our extension.
+# We need the "publish" argument to
+# publish our extension at all.
+# We need the "$VER" argument to have
+# our extension's version synchronize
+# with the rest of our versioning.
+# We need the -p and "$KEY" arguments
+# to have the permissions necessary to
+# publish this extension under our
+# organization's name.
+echo "Running Docker image to publish"
+echo "version $VER of our extension..."
+# ^--^ SC2086: Double quote to prevent
+# globbing and word splitting.
+docker run img publish "$VER" -p "$KEY"
