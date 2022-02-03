b698f1fd2c36b310cf8e57e9745ff8366576ad57
Hamir Mahal <hamirmahal@gmail.com>
2022-02-02 20:56:16 -0800

    change namespace so developers do `ask.ask`, etc.

--- js/stdlib/ts/index.ts
+++ js/stdlib/ts/index.ts
@@ -1,4 +1,4 @@
-export {ask, done, yesno} from './ask';
+export * as ask from './ask';
 export {loadStdlib, unsafeAllowMultipleStdlibs} from './loader';
 export {getConnector, getConnectorMode} from './ConnectorMode';
 export * as rpc_server from './rpc_server';
