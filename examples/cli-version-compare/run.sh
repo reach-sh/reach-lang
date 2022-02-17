a71456751becdad32de6112836a336e8e4ba9b8c
Matt Audesse <matt@mattaudesse.com>
2022-02-17 14:39:42 -0500

    CORE-1134: cli-version-compare automatically by subdirectory

--- examples/cli-version-compare/run.sh
+++ examples/cli-version-compare/run.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2000
 set -e
 
-LDIR="$(find . ! -name . -prune -type d -exec sh -c 'echo "$1" | wc -m' shell {} \; | sort -h | tail -n 1)"
+DIRS="$(find . ! -name . -prune -type d | sed 's/^\.\///' | sort -h)"
+LDIR="$(for d in $DIRS; do echo "$d" | wc -m; done | sort -h | tail -n 1)"
 ROOT="$( ([ "$REACH_DOCKER" = 0 ] && pwd) || echo /app/src)"
 
 e () {
-  printf '\e[34;1mRunning %s...\e[0m' "$2"
+  printf '\e[34;1mRunning %s...\e[0m' "$1"
   if [ "$REACH_DOCKER" = 0 ]; then
     echo
   fi
 
   set +e
-  o=$(REACH_VERSION="$( ([ -f "./$2/REACH_VERSION" ] && cat "./$2/REACH_VERSION") || echo)" \
+  o=$(REACH_VERSION="$( ([ -f "./$1/REACH_VERSION" ] && cat "./$1/REACH_VERSION") || echo)" \
     ../../reach version-compare2 \
       --non-interactive \
-      --ils="$ROOT/$2/l.json" \
-      --stub-remote="$ROOT/$2/r.json" \
-      --stub-script="./$2/reach") # skip $ROOT because `diff` is evaluated outside of Docker
+      --ils="$ROOT/$1/l.json" \
+      --stub-remote="$ROOT/$1/r.json" \
+      --stub-script="./$1/reach") # skip $ROOT because `diff` is evaluated outside of Docker
   c=$?
   set -e
 
-  e="$(cat "./$2/o.txt")"
-  if [ ! $c -eq "$1" ] || [ ! "$o" = "$e" ]; then
+  n="$(cat "./$1/exit")"
+  x="$(cat "./$1/o.txt")"
+  if [ ! $c -eq "$n" ] || [ ! "$o" = "$x" ]; then
     echo
 
-    if [ ! $c -eq "$1" ]; then
-      printf '\e[31;1mExpected %s to exit with %s but got %s.\e[0m\n' "$2" "$1" "$c"
+    if [ ! $c -eq "$n" ]; then
+      printf '\e[31;1mExpected %s to exit with %s but got %s.\e[0m\n' "$1" "$n" "$c"
     fi
 
-    if [ ! "$o" = "$e" ]; then
+    if [ ! "$o" = "$x" ]; then
       printf '\e[31;1mUnexpected output:\e[0m\n'
-      echo "$o" | diff --color "./$2/o.txt" -
+      echo "$o" | diff --color "./$1/o.txt" -
     fi
 
     echo
@@ -38,23 +41,11 @@ e () {
   fi
 
   if [ "$REACH_DOCKER" = 0 ]; then
-    printf '\e[1;32m ✔\e[0m Exited with %s\n' "$1"
+    printf '\e[1;32m ✔\e[0m Exited with %s\n' "$n"
   else
-    printf '\e[1;32m  %*s\e[0m Exited with %s' "$((LDIR - ${#2}))" "✔" "$1"
+    printf '\e[1;32m  %*s\e[0m Exited with %s' "$((LDIR - ${#1} + 2))" "✔" "$n"
   fi
   echo
 }
 
-e  1 unknown-img
-e  1 unknown-tag:0.1.7
-e  1 unknown-tag:dab71275
-e 60 new-images:reach-cli+react-runner+runner
-e 60 new-images:rpc-server#new-tags:reach-cli+devnet-cfx
-e 60 new-tags:reach+react-runner
-e  0 fully-synchronized
-e  0 connector-avail:eth+cfx
-e  0 connector-avail-3rd-party:image-not-present
-e 60 script#new-images:devnet-algo+postgres
-e 60 script#fully-synchronized
-e  0 connector-avail-3rd-party:image-present-tag-not
-e  0 connector-avail-3rd-party:image-present-sha-not
+for d in $DIRS; do e "$d"; done
