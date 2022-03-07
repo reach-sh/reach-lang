d4ca6dcde6e214b9ac4a844b4bee24a94464639f
Chike Abuah <abuahchu@gmail.com>
2022-03-07 08:47:28 -0500

    outline pandemic pass

--- hs/src/Reach/Eval.hs
+++ hs/src/Reach/Eval.hs
@@ -80,8 +80,121 @@ compileDApp shared_lifts exports (SLV_Prim (SLPrim_App_Delay at top_s (top_env, 
   let sps_ies = ar_pie
   let sps_apis = ar_isAPI
   let sps = SLParts {..}
-  return $ DLProg at final_dlo' sps dli exports ar_views ar_apis ar_events final
+  final' <- pandemic' final
+  return $ DLProg at final_dlo' sps dli exports ar_views ar_apis ar_events final'
 compileDApp _ _ _ = impossible "compileDApp called without a Reach.App"
+
+pandemic' :: DLStmts -> App DLStmts
+pandemic' = mapM pandemic
+
+pandemicLV :: DLLetVar -> App DLLetVar
+pandemicLV = undefined
+
+pandemicE :: DLExpr -> App DLExpr
+pandemicE = undefined
+
+pandemicArg :: DLArg -> App DLArg
+pandemicArg = undefined
+
+pandemicVar :: DLVar -> App DLVar
+pandemicVar = undefined
+
+pandemicBl :: DLSBlock -> App DLSBlock
+pandemicBl = undefined
+
+pandemicSW :: SwitchCases DLStmts -> App (SwitchCases DLStmts)
+pandemicSW = undefined
+
+pandemicMT :: Maybe (DLTimeArg, DLStmts) -> App (Maybe (DLTimeArg, DLStmts))
+pandemicMT = undefined
+
+pandemicAgn :: DLAssignment -> App DLAssignment
+pandemicAgn = undefined
+
+pandemicSend :: M.Map SLPart DLSend -> ReaderT Env IO (M.Map SLPart DLSend)
+pandemicSend = undefined
+
+pandemicRecv :: DLRecv DLStmts -> App (DLRecv DLStmts)
+pandemicRecv = undefined
+
+pandemic :: DLSStmt -> App DLSStmt
+pandemic = \case
+  DLS_Let at letv expr -> do
+    r1 <- pandemicLV letv
+    r2 <- pandemicE expr
+    return $ DLS_Let at r1 r2
+  DLS_ArrayMap at v1 a1 v2 v3 bl -> do
+    r1 <- pandemicVar v1
+    r2 <- pandemicArg a1
+    r3 <- pandemicVar v2
+    r4 <- pandemicVar v3
+    r5 <- pandemicBl bl
+    return $ DLS_ArrayMap at r1 r2 r3 r4 r5
+  DLS_ArrayReduce at v1 a1 a2 v2 v3 v4 bl -> do
+    r1 <- pandemicVar v1
+    r2 <- pandemicArg a1
+    r3 <- pandemicArg a2
+    r4 <- pandemicVar v2
+    r5 <- pandemicVar v3
+    r6 <- pandemicVar v4
+    r7 <- pandemicBl bl
+    return $ DLS_ArrayReduce at r1 r2 r3 r4 r5 r6 r7
+  DLS_If at arg ann sts1 sts2 -> do
+    r1 <- pandemicArg arg
+    r2 <- pandemic' sts1
+    r3 <- pandemic' sts2
+    return $ DLS_If at r1 ann r2 r3
+  DLS_Switch at v sa sw -> do
+    r1 <- pandemicVar v
+    DLS_Switch at r1 sa <$> pandemicSW sw
+  DLS_Return at i arg -> do
+    DLS_Return at i <$> pandemicArg arg
+  DLS_Prompt at v ann sts -> do
+    DLS_Prompt at v ann <$> pandemic' sts
+  DLS_Stop at -> return $ DLS_Stop at
+  DLS_Unreachable at ctx s -> return $ DLS_Unreachable at ctx s
+  DLS_Only at sl sts -> DLS_Only at sl <$> pandemic' sts
+  DLS_ToConsensus at send recv mtime -> do
+    r1 <- pandemicSend send
+    r2 <- pandemicRecv recv
+    r3 <- pandemicMT mtime
+    return $ DLS_ToConsensus at r1 r2 r3
+  DLS_FromConsensus at cxt sts -> DLS_FromConsensus at cxt <$> pandemic' sts
+  DLS_While at agn bl1 bl2 sts -> do
+    r1 <- pandemicAgn agn
+    r2 <- pandemicBl bl1
+    r3 <- pandemicBl bl2
+    r4 <- pandemic' sts
+    return $ DLS_While at r1 r2 r3 r4
+  DLS_Continue at agn -> DLS_Continue at <$> pandemicAgn agn
+  DLS_FluidSet at flv arg -> DLS_FluidSet at flv <$> pandemicArg arg
+  DLS_FluidRef at v flv -> do
+    r1 <- pandemicVar v
+    return $ DLS_FluidRef at r1 flv
+  DLS_MapReduce at i v1 dlmv arg v2 v3 bl -> do
+    r1 <- pandemicVar v1
+    r2 <- pandemicArg arg
+    r3 <- pandemicVar v2
+    r4 <- pandemicVar v3
+    r5 <- pandemicBl bl
+    return $ DLS_MapReduce at i r1 dlmv r2 r3 r4 r5
+  DLS_Throw at arg b -> do
+    r1 <- pandemicArg arg
+    return $ DLS_Throw at r1 b
+  DLS_Try at sts1 v sts2 -> do
+    r1 <- pandemic' sts1
+    r2 <- pandemicVar v
+    r3 <- pandemic' sts2
+    return $ DLS_Try at r1 r2 r3
+  DLS_ViewIs at sl1 sl2 expo -> return $ DLS_ViewIs at sl1 sl2 expo
+  DLS_TokenMetaGet tm at v a i -> do
+    r1 <- pandemicVar v
+    r2 <- pandemicArg a
+    return $ DLS_TokenMetaGet tm at r1 r2 i
+  DLS_TokenMetaSet tm at a1 a2 i b -> do
+    r1 <- pandemicArg a1
+    r2 <- pandemicArg a2
+    return $ DLS_TokenMetaSet tm at r1 r2 i b
 
 mmapMaybeM :: Monad m => (a -> m (Maybe b)) -> M.Map k a -> m (M.Map k b)
 mmapMaybeM f m = M.mapMaybe id <$> mapM f m
@@ -129,6 +242,7 @@ makeEnv cns = do
   e_lifts <- newIORef mempty
   e_vars_tracked <- newIORef mempty
   e_vars_used <- newIORef mempty
+  e_infections <- newIORef mempty
   -- XXX revise
   e_exn <- newIORef $ ExnEnv False Nothing Nothing SLM_Module
   e_mape <- makeMapEnv
