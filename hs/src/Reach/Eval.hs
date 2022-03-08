d149652b70f0f39ed1674ba555a92d550dfa05a6
Chike Abuah <abuahchu@gmail.com>
2022-03-07 22:12:40 -0500

    done writing pass

--- hs/src/Reach/Eval.hs
+++ hs/src/Reach/Eval.hs
@@ -87,46 +87,74 @@ compileDApp _ _ _ = impossible "compileDApp called without a Reach.App"
 class Pandemic a where
   pan :: a -> App a
 
+instance Pandemic a => Pandemic [a] where
+  pan = mapM pan
+
+instance Pandemic a => Pandemic (Maybe a) where
+  pan = mapM pan
+
+instance (Pandemic b) => Pandemic (Either a b) where
+  pan = mapM pan
+
 instance Pandemic DLStmts where
   pan = mapM pan
 
 instance Pandemic DLExpr where
   pan = \case
-    DLE_Arg at arg -> return $ DLE_Arg at arg
-    DLE_LArg at larg  -> return $ DLE_LArg at larg
+    DLE_Arg at arg -> DLE_Arg at <$> pan arg
+    DLE_LArg at larg  -> DLE_LArg at <$> pan larg
     DLE_Impossible at i err -> return $ DLE_Impossible at i err
-    DLE_VerifyMuldiv at cxt ct argl err -> return $ DLE_VerifyMuldiv at cxt ct argl err
-    DLE_PrimOp at primop argl -> return $ DLE_PrimOp at primop argl
-    DLE_ArrayRef at arg1 arg2 -> return $ DLE_ArrayRef at arg1 arg2
-    DLE_ArraySet at arg1 arg2 arg3 -> return $ DLE_ArraySet at arg1 arg2 arg3
-    DLE_ArrayConcat at arg1 arg2 -> return $ DLE_ArrayConcat at arg1 arg2
-    DLE_ArrayZip at arg1 arg2 -> return $ DLE_ArrayZip at arg1 arg2
-    DLE_TupleRef at arg1 i -> return $ DLE_TupleRef at arg1 i
-    DLE_ObjectRef at arg s -> return $ DLE_ObjectRef at arg s
-    DLE_Interact at cxt slp s ty argl -> return $ DLE_Interact at cxt slp s ty argl
-    DLE_Digest at argl -> return $ DLE_Digest at argl
-    DLE_Claim at cxt ct arg mbbs -> return $ DLE_Claim at cxt ct arg mbbs
-    DLE_Transfer at arg1 arg2 marg -> return $ DLE_Transfer at arg1 arg2 marg
-    DLE_TokenInit at arg -> return $ DLE_TokenInit at arg
-    DLE_CheckPay at cxt arg marg -> return $ DLE_CheckPay at cxt arg marg
-    DLE_Wait at targ -> return $ DLE_Wait at targ
-    DLE_PartSet at slp arg -> return $ DLE_PartSet at slp arg
-    DLE_MapRef at dlm arg -> return $ DLE_MapRef at dlm arg
-    DLE_MapSet at dlm arg marg -> return $ DLE_MapSet at dlm arg marg
-    DLE_Remote at cxt arg ty s amt argl bill -> return $ DLE_Remote at cxt arg ty s amt argl bill
-    DLE_TokenNew at tknew -> return $ DLE_TokenNew at tknew
-    DLE_TokenBurn at arg1 arg2 -> return $ DLE_TokenBurn at arg1 arg2
-    DLE_TokenDestroy at arg -> return $ DLE_TokenDestroy at arg
-    DLE_TimeOrder at margs_vars -> return $ DLE_TimeOrder at margs_vars
+    DLE_VerifyMuldiv at cxt ct argl err -> do
+      r <- pan argl
+      return $ DLE_VerifyMuldiv at cxt ct r err
+    DLE_PrimOp at primop argl -> DLE_PrimOp at primop <$> pan argl
+    DLE_ArrayRef at arg1 arg2 -> DLE_ArrayRef at <$> pan arg1 <*> pan arg2
+    DLE_ArraySet at arg1 arg2 arg3 -> DLE_ArraySet at <$> pan arg1 <*> pan arg2 <*> pan arg3
+    DLE_ArrayConcat at arg1 arg2 -> DLE_ArrayConcat at <$> pan arg1 <*> pan arg2
+    DLE_ArrayZip at arg1 arg2 -> DLE_ArrayZip at <$> pan arg1 <*> pan arg2
+    DLE_TupleRef at arg1 i -> do
+      r <- pan arg1
+      return $ DLE_TupleRef at r i
+    DLE_ObjectRef at arg s -> do
+      r <- pan arg
+      return $ DLE_ObjectRef at r s
+    DLE_Interact at cxt slp s ty argl -> do
+      r <- pan argl
+      return $ DLE_Interact at cxt slp s ty r
+    DLE_Digest at argl -> DLE_Digest at <$> pan argl
+    DLE_Claim at cxt ct arg mbbs -> do
+      r <- pan arg
+      return $ DLE_Claim at cxt ct r mbbs
+    DLE_Transfer at arg1 arg2 marg -> DLE_Transfer at <$> pan arg1 <*> pan arg2 <*> pan marg
+    DLE_TokenInit at arg -> DLE_TokenInit at <$> pan arg
+    DLE_CheckPay at cxt arg marg -> DLE_CheckPay at cxt <$> pan arg <*> pan marg
+    DLE_Wait at targ -> DLE_Wait at <$> pan targ
+    DLE_PartSet at slp arg -> DLE_PartSet at slp <$> pan arg
+    DLE_MapRef at dlm arg -> DLE_MapRef at dlm <$> pan arg
+    DLE_MapSet at dlm arg marg -> DLE_MapSet at dlm <$> pan arg <*> pan marg
+    DLE_Remote at cxt arg ty s amt argl bill -> do
+      DLE_Remote at cxt arg ty s <$> pan amt <*> pan argl <*> pan bill
+    DLE_TokenNew at tknew -> DLE_TokenNew at <$> pan tknew
+    DLE_TokenBurn at arg1 arg2 -> DLE_TokenBurn at <$> pan arg1 <*> pan arg2
+    DLE_TokenDestroy at arg -> DLE_TokenDestroy at <$> pan arg
+    DLE_TimeOrder at margs_vars -> DLE_TimeOrder at <$> pan margs_vars
     DLE_GetContract at -> return $ DLE_GetContract at
     DLE_GetAddress at -> return $ DLE_GetAddress at
-    DLE_EmitLog at lk vars -> return $ DLE_EmitLog at lk vars
+    DLE_EmitLog at lk vars -> DLE_EmitLog at lk <$> pan vars
     DLE_setApiDetails at who dom mc info -> return $ DLE_setApiDetails at who dom mc info
-    DLE_GetUntrackedFunds at marg arg -> return $ DLE_GetUntrackedFunds at marg arg
-    DLE_FromSome at arg1 arg2 -> return $ DLE_FromSome at arg1 arg2
+    DLE_GetUntrackedFunds at marg arg -> DLE_GetUntrackedFunds at <$> pan marg <*> pan arg
+    DLE_FromSome at arg1 arg2 -> DLE_FromSome at <$> pan arg1 <*> pan arg2
 
 instance Pandemic DLVar where
-  pan = undefined
+  pan (DLVar at m_locvar t i) = do
+    case m_locvar of
+      Nothing -> do
+        infections <- asks e_infections
+        r <- liftIO $ readIORef infections
+        case M.lookup (fromIntegral i) r of
+          Nothing -> return $ DLVar at m_locvar t i
+          Just s -> return $ DLVar at (Just (at,s)) t i
+      Just _ -> return $ DLVar at m_locvar t i
 
 instance Pandemic DLLetVar where
   pan = \case
@@ -134,31 +162,79 @@ instance Pandemic DLLetVar where
     DLV_Let vc v -> return $ DLV_Let vc v
 
 instance Pandemic DLSBlock where
-  pan = undefined
+  pan (DLSBlock at cxt sts arg) = DLSBlock at cxt <$> pan sts <*> pan arg
+
+instance Pandemic DLWithBill where
+  pan (DLWithBill b nb) = DLWithBill <$> pan b <*> pan nb
+
+instance Pandemic DLPayAmt where
+  pan (DLPayAmt net ks) = do
+    let
+      f (a,b) = do
+        r1 <- pan a
+        r2 <- pan b
+        return (r1,r2)
+    DLPayAmt <$> pan net <*> mapM f ks
+
+instance Pandemic DLTokenNew where
+  pan (DLTokenNew r s t u v w) = do
+    DLTokenNew <$> pan r <*> pan s <*> pan t <*> pan u <*> pan v <*> pan w
 
 instance Pandemic DLArg where
-  pan = undefined
+  pan = \case
+    DLA_Var v -> DLA_Var <$> pan v
+    DLA_Constant c -> return $ DLA_Constant c
+    DLA_Literal l -> return $ DLA_Literal l
+    DLA_Interact sl s t -> return $ DLA_Interact sl s t
 
-instance Pandemic (SwitchCases DLStmts) where
-  pan = undefined
+instance Pandemic b => Pandemic (a, b) where
+  pan (s,a) = do
+    r <- pan a
+    return (s,r)
 
-instance Pandemic (Maybe (DLTimeArg, DLStmts)) where
-  pan = undefined
+instance Pandemic DLLargeArg where
+  pan = \case
+    DLLA_Array t argl -> DLLA_Array t <$> mapM pan argl
+    DLLA_Tuple argl -> DLLA_Tuple <$> mapM pan argl
+    DLLA_Obj strs_args -> DLLA_Obj <$> mapM pan strs_args
+    DLLA_Data m s arg -> DLLA_Data m s <$> pan arg
+    DLLA_Struct vars_args -> DLLA_Struct <$> mapM pan vars_args
+    DLLA_Bytes s -> return $ DLLA_Bytes s
+
+instance Pandemic DLSend where
+  pan (DLSend b r s t) =
+    DLSend b <$> pan r <*> pan s <*> pan t
+
+instance Pandemic (DLVar, Bool, DLStmts) where
+  pan (v,b,sts) = do
+    r1 <- pan v
+    r3 <- pan sts
+    return (r1,b,r3)
 
 instance Pandemic DLAssignment where
-  pan = undefined
+  pan (DLAssignment mvargs) = do
+    let
+      f (a,b) = do
+        r1 <- pan a
+        r2 <- pan b
+        return (r1,r2)
+    r <- mapM f $ M.toList mvargs
+    return $ DLAssignment $ M.fromList r
 
-instance Pandemic (M.Map SLPart DLSend) where
-  pan = undefined
+instance Pandemic b => Pandemic (M.Map a b) where
+  pan = mapM pan
 
-instance Pandemic (DLRecv DLStmts) where
-  pan = undefined
+instance Pandemic a => Pandemic (DLRecv a) where
+  pan (DLRecv r s t u v w) =
+    DLRecv <$> pan r <*> pan s <*> pan t <*> pan u <*> pan v <*> pan w
 
 instance Pandemic DLSStmt where
   pan = \case
     DLS_Let at v e -> DLS_Let at <$> pan v <*> pan e
-    DLS_ArrayMap at v1 a1 v2 v3 bl -> DLS_ArrayMap at <$> pan v1 <*> pan a1 <*> pan v2 <*> pan v3 <*> pan bl
-    DLS_ArrayReduce at v1 a1 a2 v2 v3 v4 bl -> DLS_ArrayReduce at <$> pan v1 <*> pan a1 <*> pan a2 <*> pan v2 <*> pan v3 <*> pan v4 <*> pan bl
+    DLS_ArrayMap at v1 a1 v2 v3 bl -> do
+      DLS_ArrayMap at <$> pan v1 <*> pan a1 <*> pan v2 <*> pan v3 <*> pan bl
+    DLS_ArrayReduce at v1 a1 a2 v2 v3 v4 bl -> do
+      DLS_ArrayReduce at <$> pan v1 <*> pan a1 <*> pan a2 <*> pan v2 <*> pan v3 <*> pan v4 <*> pan bl
     DLS_If at arg ann sts1 sts2 -> do
       r1 <- pan arg
       r2 <- pan sts1
@@ -174,18 +250,11 @@ instance Pandemic DLSStmt where
     DLS_Stop at -> return $ DLS_Stop at
     DLS_Unreachable at ctx s -> return $ DLS_Unreachable at ctx s
     DLS_Only at sl sts -> DLS_Only at sl <$> pan sts
-    DLS_ToConsensus at send recv mtime -> do
-      r1 <- pan send
-      r2 <- pan recv
-      r3 <- pan mtime
-      return $ DLS_ToConsensus at r1 r2 r3
+    DLS_ToConsensus at s r m -> do
+      DLS_ToConsensus at <$> pan s <*> pan r <*> pan m
     DLS_FromConsensus at cxt sts -> DLS_FromConsensus at cxt <$> pan sts
     DLS_While at agn bl1 bl2 sts -> do
-      r1 <- pan agn
-      r2 <- pan bl1
-      r3 <- pan bl2
-      r4 <- pan sts
-      return $ DLS_While at r1 r2 r3 r4
+      DLS_While at <$> pan agn <*> pan bl1 <*> pan bl2 <*> pan sts
     DLS_Continue at agn -> DLS_Continue at <$> pan agn
     DLS_FluidSet at flv arg -> DLS_FluidSet at flv <$> pan arg
     DLS_FluidRef at v flv -> do
@@ -202,10 +271,7 @@ instance Pandemic DLSStmt where
       r1 <- pan arg
       return $ DLS_Throw at r1 b
     DLS_Try at sts1 v sts2 -> do
-      r1 <- pan sts1
-      r2 <- pan v
-      r3 <- pan sts2
-      return $ DLS_Try at r1 r2 r3
+      DLS_Try at <$> pan sts1 <*> pan v <*> pan sts2
     DLS_ViewIs at sl1 sl2 expo -> return $ DLS_ViewIs at sl1 sl2 expo
     DLS_TokenMetaGet tm at v a i -> do
       r1 <- pan v
