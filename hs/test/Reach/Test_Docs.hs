42d7f0fa952d2721c3f90da891b1dda72d6f76b5
William G Hatch <william@hatch.uno>
2022-03-16 22:05:14 +0000

    Add test that base_env and stdlib identifiers are documented

--- hs/test/Reach/Test_Docs.hs
+++ hs/test/Reach/Test_Docs.hs
@@ -1,7 +1,10 @@
-module Reach.Test_Docs (spec_errorCodes) where
+module Reach.Test_Docs (spec_errorCodes, spec_baseAndStdlibIdentifiers) where
 
+import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Data.Aeson as A
+import qualified Data.Map as M
 import Generic.Data
 import Reach.APICut (APICutError (..))
 import Reach.AST.Base (ImpossibleError (..), errPrefix)
@@ -14,10 +17,24 @@ import Reach.PackageImport (PkgError (..))
 import Reach.Parser (ParserError (..))
 import Reach.Util
 import Reach.Warning (Warning (W_ExternalObject))
+import System.Directory
+import System.Process
 import Test.Hspec
 
-docName :: String
-docName = "../docs/src/rsh/errors/index.md"
+rshDocDir :: String
+rshDocDir = "../docs/src/rsh/"
+
+rshDoc :: String -> String
+rshDoc path = rshDocDir <> path <> "/index.md"
+
+errDoc :: String
+errDoc = rshDoc "errors"
+
+allRshDocs :: IO [String]
+allRshDocs = do
+  files <- listDirectory rshDocDir
+  dirs <- filterM (\fn -> doesDirectoryExist $ rshDocDir <> fn) files
+  return $ map rshDoc dirs
 
 -- For each type of Error:
 --  Get the error code prefix (use random constructor)
@@ -42,15 +59,36 @@ getCodes pre num =
   where
     go x = "## {#" <> T.pack x <> "}"
 
-missingDocs :: IO [T.Text]
-missingDocs = do
-  actual <- TIO.readFile docName
-  let expected = concatMap (uncurry getCodes) allErrorCodes
+missingDocs :: IO [String] -> IO [T.Text] -> IO [T.Text]
+missingDocs docNames expected = do
+  docNames' <- docNames
+  actuals <- mapM TIO.readFile docNames'
+  let actual = T.unlines actuals
   let present = flip T.isInfixOf actual
-  return $ filter (not . present) expected
+  expected' <- expected
+  return $ filter (not . present) expected'
+
+allErrorStrings :: IO [T.Text]
+allErrorStrings = return $ concatMap (uncurry getCodes) allErrorCodes
 
 spec_errorCodes :: Spec
 spec_errorCodes =
   describe "error codes" $
     it "have docs" $
-      missingDocs >>= flip shouldBe []
+      missingDocs (return [errDoc]) allErrorStrings >>= flip shouldBe []
+
+spec_baseAndStdlibIdentifiers :: Spec
+spec_baseAndStdlibIdentifiers =
+  describe "base and stdlib identifiers" $
+    it "have docs" $
+      missingDocs allRshDocs baseAndStdlibIdentifiers
+      >>= flip shouldBe []
+
+baseAndStdlibIdentifiers :: IO [T.Text]
+baseAndStdlibIdentifiers = do
+  json <- readProcess "reachc" ["--print-keyword-info"] ""
+  let obj = A.decode (lbpack json) :: Maybe (M.Map T.Text A.Value)
+  case obj of
+    Nothing -> return ["Error getting base and stdlib identifiers for test" :: T.Text]
+    Just o -> return $ map (\x -> "ref(\"rsh\", \"" <> x <> "\")")
+      $ filter (not . T.isInfixOf "_") $ M.keys o
