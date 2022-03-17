f119e4cf44815e1c37c9266df030d2a31e9b98c3
Chike Abuah <abuahchu@gmail.com>
2022-03-17 12:41:26 -0400

    color code grapg, bump ui version

--- examples/programmer-art/src/index.mjs
+++ examples/programmer-art/src/index.mjs
@@ -916,8 +916,10 @@ const redraw = async () => {
   let edges = await c.getEdges()
   let states = await c.getStates()
   let elements = []
+  const singleColors = ['#0f4539','#2f3b22','#052d0a','#001f24']
   for (const [s, dets] of Object.entries(states)) {
     let displayLabel = dets[1].tag.slice(2)
+    let actorStateID = dets[0]
     if (displayLabel !== 'None') {
       displayLabel = displayLabel + '?'
     }
@@ -925,7 +927,8 @@ const redraw = async () => {
       {
         data:
           { id: s,
-            label: displayLabel
+            label: displayLabel,
+            color: singleColors[parseInt(actorStateID)+1]
           }
       }
     )
@@ -933,7 +936,9 @@ const redraw = async () => {
   for (const [index, value] of edges.entries()) {
     const from = value[0]
     const to = value[1]
-    elements.push({data: { id: `edge-${index}`, source: from, target: to } })
+    elements.push({data:
+      { id: `edge-${index}`, source: from, target: to }
+    })
   }
   let cy = cytoscape({
     container: document.getElementById('cy'),
@@ -956,7 +961,7 @@ const redraw = async () => {
           'text-valign': 'center',
           'text-halign': 'center',
           'border-width': 1.5,
-          'border-color': '#555',
+          'border-color': 'data(color)',
         }
       },
       {
