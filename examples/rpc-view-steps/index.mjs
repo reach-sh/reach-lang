a7d9764c191ff7151385c5990e5c1fba356c7fe7
erer1243 <oliver@reach.sh>
2022-03-18 11:40:56 -0400

    Updated rpc examples

--- examples/rpc-view-steps/index.mjs
+++ examples/rpc-view-steps/index.mjs
@@ -1,38 +1,38 @@
 import { mkRPC }      from '@reach-sh/rpc-client';
 import { mkAssertEq } from './common.mjs';
 
-  const { rpc, rpcCallbacks } = await mkRPC();
+const { rpc, rpcCallbacks } = await mkRPC();
 
-  const startBal = await rpc('/stdlib/parseCurrency', 200);
-  const accAlice = await rpc('/stdlib/newTestAccount', startBal);
-  const accBob   = await rpc('/stdlib/newTestAccount', startBal);
-  const ctcs     = [];
+const startBal = await rpc('/stdlib/parseCurrency', 200);
+const accAlice = await rpc('/stdlib/newTestAccount', startBal);
+const accBob   = await rpc('/stdlib/newTestAccount', startBal);
+const ctcs     = [];
 
-  const run = async (p, whosBob) => {
-    const ctcAlice = await rpc('/acc/contract', accAlice);
-    ctcs.push(ctcAlice);
+const run = async (p, whosBob) => {
+  const ctcAlice = await rpc('/acc/contract', accAlice);
+  ctcs.push(ctcAlice);
 
-    const checkView = async e => mkAssertEq(rpc)(`Run w/ ${p} checkView:`, e, [
-        await rpc('/ctc/v/Main/last', ctcAlice),
-        await rpc('/ctc/v/Main/i',    ctcAlice),
-      ]);
-
-    await Promise.all([
-      rpcCallbacks('/ctc/p/Alice', ctcAlice, { checkView }),
-
-      rpc('/ctc/getInfo', ctcAlice)
-        .then(async i => {
-          const ctcBob = await rpc('/acc/contract', whosBob, i);
-          ctcs.push(ctcBob);
-          await rpcCallbacks('/ctc/p/Bob', ctcBob, {});
-        }),
+  const checkView = async e => mkAssertEq(rpc)(`Run w/ ${p} checkView:`, e, [
+      await rpc('/ctc/v/Main/last', ctcAlice),
+      await rpc('/ctc/v/Main/i',    ctcAlice),
     ]);
-  };
-
-  await run('Bob',   accBob);
-  await run('Alice', accAlice);
 
   await Promise.all([
-    rpc('/forget/ctc', ...ctcs),
-    rpc('/forget/acc', accAlice, accBob),
+    rpcCallbacks('/ctc/p/Alice', ctcAlice, { checkView }),
+
+    rpc('/ctc/getInfo', ctcAlice)
+      .then(async i => {
+        const ctcBob = await rpc('/acc/contract', whosBob, i);
+        ctcs.push(ctcBob);
+        await rpcCallbacks('/ctc/p/Bob', ctcBob, { checkView });
+      }),
   ]);
+};
+
+await run('Bob',   accBob);
+await run('Alice', accAlice);
+
+await Promise.all([
+  rpc('/forget/ctc', ...ctcs),
+  rpc('/forget/acc', accAlice, accBob),
+]);
