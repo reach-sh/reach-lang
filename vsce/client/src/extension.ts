805fbf7a12a6fba3574417599ff6ff00decf5a56
Hamir Mahal <hamirmahal@gmail.com>
2022-03-20 10:05:41 -0500

    remove extra, unnecessary `;`

--- vsce/client/src/extension.ts
+++ vsce/client/src/extension.ts
@@ -67,7 +67,7 @@ export function activate(context: ExtensionContext) {
 	terminal = window.createTerminal(terminalOptions);
 	const reachExecutablePath = workspace.getConfiguration().get('reachide.executableLocation') as string;
 	const wf = workspace.workspaceFolders[0].uri.path || '.';
-	const reachPath = path.join(wf, reachExecutablePath);;
+	const reachPath = path.join(wf, reachExecutablePath);
 	registerCommands(context, reachPath);
 
 
