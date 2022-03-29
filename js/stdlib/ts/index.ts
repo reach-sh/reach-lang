b0750e994cc2d484bb5fe77d3d3ef20e22a0df47
Dan Burton <danburton.email@gmail.com>
2022-03-28 19:01:15 -0700

    Revert "potentially fix `import WalletConnect` issue"
    This reverts commit e89833023936acfd293ab41f5430c8ec43b6e5fb.

--- js/stdlib/ts/index.ts
+++ js/stdlib/ts/index.ts
@@ -1,4 +1,3 @@
-export * as ALGO_WalletConnect from './ALGO_WalletConnect';
 export * as ask from './ask';
 export {loadStdlib, unsafeAllowMultipleStdlibs} from './loader';
 export {getConnector, getConnectorMode} from './ConnectorMode';
