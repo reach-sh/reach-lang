154928570e6a6eb9c263965c563898b447922904
John Hopkins <johnthopkins@gmail.com>
2022-03-30 14:56:02 -0500

    Simulator_ui Initial

--- examples/simulator_ui/src/App.tsx
+++ examples/simulator_ui/src/App.tsx
@@ -1,7 +1,6 @@
 import * as c from "@reach-sh/simulator-client";
 import { useEffect, useState } from "react";
-// import cytoscape from 'cytoscape';
-// import klay from 'cytoscape-klay';
+import { redraw } from './reach/Visualizer';
 import { createGlobalStyle } from "styled-components";
 import getObjectView from "./reach/ObjectView";
 import LeftPanel from "./componenents/organisms/LeftPanel";
@@ -10,6 +9,7 @@ import Navbar from "./componenents/molecules/Navbar";
 import CodePanel from "./componenents/organisms/CodePanel";
 import "./App.css";
 import styled from "styled-components";
+import VisualizerPanel from "./componenents/organisms/Visualizer";
 
 export const GlobalStyles = createGlobalStyle`
   html {
@@ -55,16 +55,22 @@ async function initApp(mountProgram: Function) {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-const AppContainer = styled.div`
-  display: flex;
-  flex-direction: row;
+  width: 100vw;
+  overflow-x: hidden;
 `;
 
 const InfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: 30% 30% 40%;
+  grid-template-rows: 33% 33% 33%;
+`;
+
+const Info = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  grid-column-start: 2;
+  width: 70vw;
+  height: 62.5vh;
 `;
 
 function App() {
@@ -72,11 +78,25 @@ function App() {
   const [nodeId, setNodeId] = useState(0);
   const [objectViewData, setObjectViewData] = useState({});
   const [code, mountProgram] = useState("");
+  const [graphElements, setGraphElements] = useState([])
   const [jsonLog, updateJsonLog] = useState([
     ["resetServer"],
     ["load"],
     ["init"],
   ]);
+
+   useEffect(() => {
+    const getVisualizerData = async () => {
+      console.log('preeffect')
+      if (Object.entries(graphElements).length < 1 ) {
+        const e = await redraw(c);
+        console.log('in use effect')
+        console.log(e);
+        setGraphElements(e);
+      }
+    };
+    getVisualizerData();
+  });
 
   useEffect(() => {
     const fetchObjectViewData = async () => {
@@ -96,22 +116,23 @@ function App() {
     }
   });
 
-  useEffect(() => {}, []);
 
   return (
     <div className="App">
       <GlobalStyles />
       <Container>
         <Navbar />
-        <AppContainer>
+        <InfoContainer>
           <CodePanel code={code} />
-          <InfoContainer>
+          <Info>
             <LeftPanel
               initParticipant={initParticipant}
               objectViewData={objectViewData}
             />
-          </InfoContainer>
-        </AppContainer>
+            <RightPanel selection={null}/>
+          </Info>
+          <VisualizerPanel elements={graphElements ? graphElements : []}/>
+        </InfoContainer>
       </Container>
     </div>
   );
--- examples/simulator_ui/src/componenents/organisms/LeftPanel.tsx
+++ examples/simulator_ui/src/componenents/organisms/LeftPanel.tsx
@@ -1,153 +1,184 @@
-import styled from 'styled-components'
-import Button from '../atoms/Button'
-import SectionHeaderComponent from '../molecules/SectionHeader'
-import { useState } from 'react'
-import DropdownInput from '../atoms/DropdownInput'
-import ObsPanel from '../molecules/ObsPanel'
+import styled from "styled-components";
+import Button from "../atoms/Button";
+import SectionHeaderComponent from "../molecules/SectionHeader";
+import { useState } from "react";
+import DropdownInput from "../atoms/DropdownInput";
+import ObsPanel from "../molecules/ObsPanel";
 
-const  PanelContainer = styled.div`
-    position: static;
-    width: 420px;
-    height: 562px;
-    border: 1px solid var(--off-white);
-    box-sizing: border-box;
-    background: var(--dark-bg);
-`
+const PanelContainer = styled.div`
+  position: static;
+  width: 30vw;
+  height: 62.5vh;
+  border: 1px solid var(--off-white);
+  box-sizing: border-box;
+  background: var(--dark-bg);
+  grid-column-start: 3;
+`;
 const StyledPlus = styled(Button)`
-    background-color: var(--reach-2);
-    label{
-        font-style: normal;
-        font-weight: normal;
-        font-size: 12px;
-        color: var(--dark-bg);
-    }
-    line-height: 150%;
-    border-radius: 2px;
-    width: 38px;
-    height: 26px;
-    text-align: center;
-
-
-`
-function PlusButton (){
-    return (<StyledPlus label="+" icon={<></>} color={'#101010'} ></StyledPlus>)
+  background-color: var(--reach-2);
+  label {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    color: var(--dark-bg);
+  }
+  line-height: 150%;
+  border-radius: 2px;
+  width: 38px;
+  height: 26px;
+  text-align: center;
+`;
+function PlusButton() {
+  return <StyledPlus label="+" icon={<></>} color={"#101010"}></StyledPlus>;
 }
 
 const SectionHeader = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
 const SectionTitle = styled.p`
-    color: var(--off-white);
-    font-style: normal;
-    font-weight: bold;
-    font-size: 14px;
-    line-height: 16px;
-
-`
+  color: var(--off-white);
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+`;
 
 const ObjectSection = styled.div`
-    margin: 0 7.5%;
-    align-self: center;
-    align-items: left;
-    
-`
+  margin: 0 7.5%;
+  align-self: center;
+  align-items: left;
+`;
 const ObjectContent = styled.div`
-    height: 110px;
-`
+  height: 110px;
+`;
 
 const AccountsSection = styled.div`
-    margin: 0 7.5%;
-`
+  margin: 0 7.5%;
+`;
 const AccountContent = styled.div`
-    max-height: 100px;
-`
+  max-height: 100px;
+`;
 
 const AddItem = styled.div`
-    display: flex;
-    flex-direction: row;
-    p{
-        color: var(--white);
-    };
-    align-items: center;
-    justify-content: space-between;
-`
+  display: flex;
+  flex-direction: row;
+  p {
+    color: var(--white);
+  }
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const InitSection = styled.div`
-    margin: 0 7.5%;
-`
+  margin: 0 7.5%;
+`;
 
 const InitContent = styled.div`
-    height: 184px;
-    position: relative;
-    display: flex;
-    flex-direction: row;
-`
-
-
+  height: 184px;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+`;
 
 const SectionDivider = styled.div`
-    border: 0.5px solid var(--white);
-    height: 0px;
-    width: 85%;
-    margin-left: 7.5%;
-    align-self: center;
-`
+  border: 0.5px solid var(--white);
+  height: 0px;
+  width: 85%;
+  margin-left: 7.5%;
+  align-self: center;
+`;
 const InitParticipantsButton = styled(Button)`
-    margin-left: auto;
-    width: 156px;
-    height: 28px;
-`
-const ParticipantName = styled.p``
+  margin-left: auto;
+  width: 156px;
+  height: 28px;
+`;
+const ParticipantName = styled.p``;
 
-
-
-
-function LeftPanel ({initParticipant, objectViewData}: {initParticipant: Function, objectViewData: any}) {
-    const [openObjectSection, setOpenObjectSection] = useState(true);
-    const [openAccountsSection, setOpenAccountsSection] = useState(true);
-    const [openInitSection, setOpenInitSection] = useState(true);
-    const [dropDownSelection, changeDropDownSelection] = useState<number | null>(null)
-    const participantChange = (participant: number) => {
+function LeftPanel({
+  initParticipant,
+  objectViewData,
+}: {
+  initParticipant: Function;
+  objectViewData: any;
+}) {
+  const [openObjectSection, setOpenObjectSection] = useState(true);
+  const [openAccountsSection, setOpenAccountsSection] = useState(true);
+  const [openInitSection, setOpenInitSection] = useState(true);
+  const [dropDownSelection, changeDropDownSelection] = useState<number | null>(
+    null
+  );
+  const participantChange = (participant: number) => {
     changeDropDownSelection(participant);
   };
-    return (
+  return (
     <PanelContainer>
-        <ObjectSection>
-            <SectionHeaderComponent sectionOpen={openObjectSection} setSectionOpen={setOpenObjectSection} title='Object'/>
-            {openObjectSection && Object.entries(objectViewData).length > 0 ?<ObjectContent>
-                <ObsPanel objectViewData={objectViewData}/>
-            </ObjectContent>:<></>}
-        </ObjectSection>
-        <SectionDivider/>
-        <AccountsSection>
-            <SectionHeaderComponent sectionOpen={openAccountsSection} setSectionOpen={(setOpenAccountsSection)} title={'Accounts/Tokens'} />
-            {openAccountsSection ? <AccountContent>
+      <ObjectSection>
+        <SectionHeaderComponent
+          sectionOpen={openObjectSection}
+          setSectionOpen={setOpenObjectSection}
+          title="Object"
+        />
+        {openObjectSection && Object.entries(objectViewData).length > 0 ? (
+          <ObjectContent>
+            <ObsPanel objectViewData={objectViewData} />
+          </ObjectContent>
+        ) : (
+          <></>
+        )}
+      </ObjectSection>
+      <SectionDivider />
+      <AccountsSection>
+        <SectionHeaderComponent
+          sectionOpen={openAccountsSection}
+          setSectionOpen={setOpenAccountsSection}
+          title={"Accounts/Tokens"}
+        />
+        {openAccountsSection ? (
+          <AccountContent>
             <AddItem>
-            <p>New Account</p>
-            <PlusButton />
+              <p>New Account</p>
+              <PlusButton />
             </AddItem>
             <AddItem>
-                <p>New Token</p>
-                <PlusButton />
+              <p>New Token</p>
+              <PlusButton />
             </AddItem>
-
-        </AccountContent> : <></>}
-        </AccountsSection>
-        <SectionDivider/>
-        <InitSection>
-            <SectionHeaderComponent sectionOpen={openInitSection} setSectionOpen={setOpenInitSection} title='Participant Initialization'/>
-            {openInitSection?<InitContent>
-                <DropdownInput value={dropDownSelection ? dropDownSelection.toString() : ''} onChange={participantChange}/>
-            <InitParticipantsButton label='Init Participant' icon={<></>} onClick={ () =>{console.log(dropDownSelection); return initParticipant(dropDownSelection)}}  />
-            </InitContent>:<></>}
-        </InitSection>
+          </AccountContent>
+        ) : (
+          <></>
+        )}
+      </AccountsSection>
+      <SectionDivider />
+      <InitSection>
+        <SectionHeaderComponent
+          sectionOpen={openInitSection}
+          setSectionOpen={setOpenInitSection}
+          title="Participant Initialization"
+        />
+        {openInitSection ? (
+          <InitContent>
+            <DropdownInput
+              value={dropDownSelection ? dropDownSelection.toString() : ""}
+              onChange={participantChange}
+            />
+            <InitParticipantsButton
+              label="Init Participant"
+              icon={<></>}
+              onClick={() => {
+                console.log(dropDownSelection);
+                return initParticipant(dropDownSelection);
+              }}
+            />
+          </InitContent>
+        ) : (
+          <></>
+        )}
+      </InitSection>
     </PanelContainer>
-    )
+  );
 }
 
-export default styled(LeftPanel)`
-
-`
+export default styled(LeftPanel)``;
