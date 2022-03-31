1ab00b4fd870efbdfee3eef825b5ee575ac6f751
erer1243 <oliver@reach.sh>
2022-03-31 18:24:58 -0400

    wip

---
+++ scripts/devnet-cfx/daily-ping.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+START_TIME_UNIX="$(date +%s)"
+START_TIME_UTC="$(date --utc --iso-8601=seconds)Z"
+
+ping_devnet() {
+  TIME_NOW="$(date +%s)"
+  ELAPSED="$(expr $TIME_NOW - $START_TIME_UNIX).0"
+  REPORT="{
+    \"userId\": \"CFX-devnet\",
+    \"startTime\": \"$START_TIME_UTC\",
+    \"version\": \"0.1.9\",
+    \"elapsed\": \"$ELAPSED\",
+    \"result\": \"devnet 24h ping\",
+    \"connectorMode\": \"CFX-devnet\"
+  }"
+
+  curl \
+    --request POST \
+    --header "Content-Type: application/json; charset=utf-8" \
+    --data "$REPORT" \
+    "https://log.reach.sh/submit"
+}
+
+# 86400 sec = 24 hours
+while sleep 86400; do
+  ping_devnet &
+done
