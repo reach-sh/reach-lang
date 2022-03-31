13ac2bcbb9699d608c0e34b6b111fb61cdc39971
Jay McCarthy <jay.mccarthy@gmail.com>
2022-03-31 15:45:15 -0400

    init

---
+++ examples/uint256/index.mjs
@@ -0,0 +1,21 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+const stdlib = loadStdlib();
+
+const accAlice = await stdlib.newTestAccount(stdlib.parseCurrency(100));
+const accBob = await stdlib.newTestAccount(stdlib.parseCurrency(100));
+
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+
+await Promise.all([
+  ctcAlice.participants.Alice({
+    request: stdlib.parseCurrency(5),
+    info: 'If you wear these, you can see beyond evil illusions.'
+  }),
+  ctcBob.p.Bob({
+    want: (amt) => console.log(`Alice asked Bob for ${stdlib.formatCurrency(amt)}`),
+    got: (secret) => console.log(`Alice's secret is: ${secret}`),
+  }),
+]);
