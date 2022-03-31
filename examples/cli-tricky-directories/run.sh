94770668cee98d84d9a1f9f292414bc3379ba77b
Matt Audesse <matt@mattaudesse.com>
2022-03-31 19:50:02 -0400

    CORE-564: fiddle with output alignment

--- examples/cli-tricky-directories/run.sh
+++ examples/cli-tricky-directories/run.sh
@@ -2,25 +2,29 @@
 # shellcheck disable=SC2000
 set -e
 
+# TODO: portable replacement for `wc -L`
+l () { echo -n "$1" | wc -L 2>/dev/null || echo -n "$1" | wc -m; }
+
 TEMP="$(mkdir -p ./.tmp && cd ./.tmp && pwd)"
 TOUT="$(mkdir -p "$TEMP"/build && cd "$TEMP/build" && pwd)"
 DIRS="$(find . -maxdepth 1 -type d ! \( -name . -o -name .tmp \) -prune | sed 's/^\.\///' | sort -h)"
-LDIR="$(echo "$DIRS" | while IFS=$'\n' read -r d; do echo "$d" | wc -m; done | sort -h | tail -n 1)"
+LDIR="$(echo "$DIRS" | while IFS=$'\n' read -r d; do l "$d"; done | sort -h | tail -n 1)"
 ROOT="$(cd ../.. && pwd)"
 
 echo 0 >"$TEMP/EXIT"
 
 IFS=$'\n'
 for d in $DIRS; do
+  p="$((LDIR - $(l "$d") + 3))"
   printf '\e[34;1mRunning "%s"...\e[0m' "$d"
 
   if [ "$(find "$d" -type f -name SKIP | wc -l)" -gt 0 ]; then
     if [ -n "$CI" ]; then
-      printf '\e[31;1m  %*s\e[0m Failed:\n' "$((LDIR - ${#d} + 2))" "✘"
+      printf '\e[31;1m  %*s\e[0m Failed:\n' "$p" "✘"
       echo 'Skipping is only allowed during development.'
       echo 1 >"$TEMP/EXIT"
     else
-      printf '\e[1;33m  %*s\e[0m Skipped\n' "$((LDIR - ${#d} + 2))" "•"
+      printf '\e[1;33m  %*s\e[0m Skipped\n' "$p" "•"
     fi
     continue
   fi
@@ -33,9 +37,9 @@ for d in $DIRS; do
   set -e
 
   if [ "$r" -eq 0 ]; then
-    printf '\e[1;32m  %*s\e[0m Succeeded\n'            "$((LDIR - ${#d} + 2))" "✔"
+    printf '\e[1;32m  %*s\e[0m Succeeded\n'            "$p" "✔"
   else
-    printf '\e[31;1m  %*s\e[0m Failed with code %s:\n' "$((LDIR - ${#d} + 2))" "✘" "$r"
+    printf '\e[31;1m  %*s\e[0m Failed with code %s:\n' "$p" "✘" "$r"
     printf '%s\n\n' "$(cat "$t")"
     echo 1 >"$TEMP/EXIT"
   fi
