68686a4b1b3ecce11d931914be47633dc570792a
Chris Nevers <christophernevers96@gmail.com>
2022-04-05 13:48:37 -0400

    Update log attack variables

--- examples/log-attack1/index.sol
+++ examples/log-attack1/index.sol
@@ -2,8 +2,8 @@ pragma abicoder v2;
 pragma solidity ^0.8.0;
 
 struct T4 {
-  address payable v68;
-  uint256 v69;
+  address payable v69;
+  uint256 v70;
   }
 struct T5 {
   uint256 time;
@@ -20,8 +20,8 @@ contract LogAttack1 {
   function m1(address payable acct, uint256 amt) external payable {
     T5 memory a;
     a.time = 0;
-    a.msg.v68 = acct;
-    a.msg.v69 = amt + 1;
+    a.msg.v69 = acct;
+    a.msg.v70 = amt + 1;
     emit _reach_e1(a);
   }
 }
