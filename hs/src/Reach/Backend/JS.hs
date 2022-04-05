76fe9739be8ce3de5c4154d9001cc0af841d9a73
Chris Nevers <christophernevers96@gmail.com>
2022-04-05 10:12:27 -0400

    Fix JS generation for Remotes

--- hs/src/Reach/Backend/JS.hs
+++ hs/src/Reach/Backend/JS.hs
@@ -454,8 +454,8 @@ jsExpr = \case
               , ("accs", jsArray accs')
               ]
         net' <- jsCon $ DLL_Int at 0
-        let bill' = jsArray $ map (const net') nnRecv
-        return $ jsArray [ net', bill', res' <> ", undefined" ]
+        let bill' = map (const net') nnRecv
+        return $ jsArray $ net' : bill' <> [res' <> ", undefined" ]
   DLE_TokenNew _ tns -> do
     (ctxt_mode <$> ask) >>= \case
       JM_Backend -> return "undefined /* TokenNew */"
