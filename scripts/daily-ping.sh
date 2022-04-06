f4d9fef9d40571c56a69de9d9db8579e8cfb27a2
erer1243 <oliver@reach.sh>
2022-04-06 11:45:39 -0400

    pr fixes, get necessary info into containers
    Example report generated by script:
    {
      "userId": "Z5UG:RC3H:BP5K:BFHF:FMJ6:T3QN:T5TL:DJID:HG65:W3BI:QY5H:ANUF",
      "startTime": "2022-04-06T15:38:39+00:00Z",
      "version": "0.1.9",
      "elapsed": "0.0",
      "result": "devnet 24h ping",
      "connectorMode": "ALGO-devnet"
    }

---
+++ scripts/daily-ping.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+START_TIME_UNIX="$(date +%s)"
+START_TIME_UTC="$(date -u -Iseconds)Z"
+
+ping_devnet() {
+  # REACH_CONNECTOR_MODE, REACH_VERSION, and REACHC_ID are defined in hs/app/reach/embed/docker/service-devnet-*.yml
+  TIME_NOW="$(date +%s)"
+  ELAPSED="$(( $TIME_NOW - $START_TIME_UNIX )).0"
+  REPORT="{
+    \"userId\": \"$REACHC_ID\",
+    \"startTime\": \"$START_TIME_UTC\",
+    \"version\": \"$REACH_VERSION\",
+    \"elapsed\": \"$ELAPSED\",
+    \"result\": \"devnet 24h ping\",
+    \"connectorMode\": \"$REACH_CONNECTOR_MODE\"
+  }"
+
+  curl \
+    --request POST \
+    --header "Content-Type: application/json; charset=utf-8" \
+    --data "$REPORT" \
+    "https://log.reach.sh/submit"
+}
+
+# 86400 sec = 24 hours
+while sleep 86400; do
+  ping_devnet &
+done
