14e49ccb1f5173902301c54725c6b1248f52727c
Chike Abuah <abuahchu@gmail.com>
2022-04-08 10:51:48 -0400

    update multipath test

--- examples/simulator-rps-2-rps-multipath/client.mjs
+++ examples/simulator-rps-2-rps-multipath/client.mjs
@@ -1,46 +1,19 @@
 import * as c from '@reach-sh/simulator-client';
 import * as  assert from 'assert';
+import { readFile } from 'fs/promises';
+
+const script = JSON.parse(
+  await readFile(
+    new URL('./client.json', import.meta.url)
+  )
+);
 
 const main = async () => {
-  const port = 3001
   console.log("Init Testing!")
-  const params = {
-    port: port
-  };
-  await c.waitForPort(params)
-  await c.resetServer()
-  await c.load()
-  await c.init()
-  await c.initFor(0,0)
-  await c.initFor(1,1)
-
-  // 1ˢᵗ path
-  await c.respondWithVal(2,2,0,0)
-  await c.respondWithVal(3,3,0,-1)
-  await c.respondWithVal(4,2,-99,0)
-  await c.respondWithVal(5,5,-99,1)
-  await c.respondWithVal(6,6,1)
-  await c.respondWithVal(7,7,1,-1)
-  await c.respondWithVal(8,8,-99,0)
-  await c.respondWithVal(9,9,-99,1)
-  await c.respondWithVal(10,10,-99)
-
-  let r = await c.getStatus()
+  await c.waitForPort()
+  await c.interp(script)
+  const r = await c.getStatus()
   assert.equal(r,"Done");
-
-  // 2ⁿᵈ path
-  await c.respondWithVal(2,2,1,0)
-  await c.respondWithVal(12,12,0,-1)
-  await c.respondWithVal(13,2,-99,0)
-  await c.respondWithVal(14,14,-99,1)
-  await c.respondWithVal(15,15,0)
-  await c.respondWithVal(16,16,1,-1)
-  await c.respondWithVal(17,17,-99,0)
-  await c.respondWithVal(18,18,-99,1)
-  await c.respondWithVal(19,19,-99)
-  r = await c.getStatus()
-  assert.equal(r,"Done");
-
   console.log("Testing Complete!")
 }
 
