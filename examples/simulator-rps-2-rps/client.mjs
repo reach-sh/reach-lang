d29ec8e9f4b7682995e26413b172b1670d73914b
Chike Abuah <abuahchu@gmail.com>
2022-04-08 10:43:52 -0400

    update rps-2 simulation

--- examples/simulator-rps-2-rps/client.mjs
+++ examples/simulator-rps-2-rps/client.mjs
@@ -1,72 +1,17 @@
 import * as c from '@reach-sh/simulator-client';
 import * as  assert from 'assert';
-import * as pj from 'prettyjson';
+import { readFile } from 'fs/promises';
 
-const logGlobal = async (n) => {
-  const r = await c.getStateGlobals(n)
-  console.log(`LOGGING GLOBAL STATE ${n}`)
-  console.log(pj.render(r))
-  console.log("-------------------")
-}
-
-const logLocal = async (n) => {
-  const r = await c.getStateLocals(n)
-  console.log(`LOGGING LOCAL STATE ${n}`)
-  console.log(pj.render(r))
-  console.log("-------------------")
-}
-
-const getVar = async (v,n,aid) => {
-  const x = await c.getStateLocals(n)
-  return x.l_locals[aid].l_store.find(el => el[0] == v)
-}
-
-const assertVar = (y,t,v) => {
-  assert.equal(y[1].tag,t);
-  assert.equal(y[1].contents,v);
-}
+const script = JSON.parse(
+  await readFile(
+    new URL('./client.json', import.meta.url)
+  )
+);
 
 const main = async () => {
   console.log("Init Testing!")
   await c.waitForPort()
-  await c.resetServer()
-  await c.ping()
-  await c.load()
-  // init consensus
-  await c.init()
-  // init Alice
-  await c.initFor(0,0)
-  // init Bob
-  await c.initFor(1,1)
-  // Alice interactively gets her hand (0)
-  await c.respondWithVal(2,2,0,0)
-  // Alice's hand (0) is published
-  await c.respondWithVal(3,3,0,-1)
-  // Alice observes that her hand is published
-  await c.respondWithVal(4,2,-99,0)
-  let y = await getVar('handAlice',5,0)
-  assertVar(y,'V_UInt',0)
-  // Bob observes that Alice's hand is published
-  await c.respondWithVal(5,5,-99,1)
-  y = await getVar('handAlice',6,1)
-  assertVar(y,'V_UInt',0)
-  // Bob interactively gets his hand (1)
-  await c.respondWithVal(6,6,1)
-  // Bob's hand (1) is published
-  await c.respondWithVal(7,7,1,-1)
-  // Alice observes that Bob's hand is published
-  await c.respondWithVal(8,8,-99,0)
-  y = await getVar('handBob',9,0)
-  assertVar(y,'V_UInt',1)
-  // Bob observes that his hand is published
-  await c.respondWithVal(9,9,-99,1)
-  y = await getVar('handBob',10,1)
-  assertVar(y,'V_UInt',1)
-  // Bob seeOutcome's
-  await c.respondWithVal(10,10,-99)
-  // Alice seeOutcome's
-  await c.respondWithVal(11,11,-99, 0)
-
+  await c.interp(script)
   const r = await c.getStatus()
   assert.equal(r,"Done");
   console.log("Testing Complete!")
