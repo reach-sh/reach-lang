8f30a997cc6f601c787112ece26dd08b5091ef16
Craig-Elre <97911277+Craig-Elre@users.noreply.github.com>
2022-04-10 14:41:34 -0600

    Add initial Wisdom For Sale .rsh and .mjs files
    This ran properly in Ubuntu and output the role of seller and the network as Algo as intended.
    
    This is intended to be a test of the process of creating a branch off master in GitHub Desktop separated from my forked branch, but adds files required for Wisdom For Sale.

---
+++ examples/docs-wisdom-1/index.mjs
@@ -0,0 +1,28 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+const role = 'seller';
+console.log(`Your role is ${role}`);
+
+const stdlib = loadStdlib(process.env);
+console.log(`The consensus network is ${stdlib.connector}.`);
+
+const commonInteract = {};
+
+// Seller
+if (role === 'seller') {
+  const sellerInteract = { 
+    ...commonInteract };
+		
+  const acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+  const ctc = acc.contract(backend);
+  await backend.Seller(ctc, sellerInteract);
+}
+	
+// Buyer
+else {
+  const buyerInteract = {
+	...commonInteract 
+  };
+
+};
