cc7d73167265cb9654de8ffb56364b354ca558b8
erer1243 <oliver@reach.sh>
2022-04-13 19:10:02 -0400

    + alternate, 4 participant style

---
+++ examples/nft-pawn-shop-participants/index.mjs
@@ -0,0 +1,68 @@
+import {loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
+
+const run = async (borrowerPaysBack) => {
+  const startingBalance = stdlib.parseCurrency(100);
+  // Note that only two accounts are made, though there are four participants in the contract
+  const [accLender, accBorrower] = await stdlib.newTestAccounts(2, startingBalance);
+  const nft = await stdlib.launchToken(accBorrower, "suspicious watch", "SW", { supply: 1 });
+  await accLender.tokenAccept(nft.id);
+  
+  const fmt = (amt) => stdlib.formatCurrency(amt, 2);
+  const loanAmt = stdlib.parseCurrency(40);
+  const interest = stdlib.parseCurrency(10);
+  const deadline = 200;
+
+  const printBalances = async () => {
+    const [nwtBor, nftBor] = await stdlib.balancesOf(accBorrower, [null, nft.id]);
+    const [nwtLen, nftLen] = await stdlib.balancesOf(accLender,   [null, nft.id]);
+    console.log(`* Borrower has ${fmt(nwtBor)} ${stdlib.standardUnit} and ${nftBor} ${nft.name}`);
+    console.log(`*   Lender has ${fmt(nwtLen)} ${stdlib.standardUnit} and ${nftLen} ${nft.name}`);
+  };
+
+  console.log("Starting balances:");
+  await printBalances();
+  const ctcBorrower = accBorrower.contract(backend);
+  const ctcLender = accLender.contract(backend, ctcBorrower.getInfo());
+
+  console.log(`Borrower puts up the ${nft.name} nft as collateral`);
+  try {
+    await Promise.all([
+      backend.Borrower(ctcBorrower, {
+        loanMade: async () => {
+          console.log(`Borrower receives a loan of ${fmt(loanAmt)} ${stdlib.standardUnit}`);
+          await printBalances();
+          throw 'loanMade';
+        },
+        loanAmt,
+        interest,
+        deadline,
+        nft: nft.id,
+      }),
+      backend.Lender(ctcLender, {}),
+    ]);
+  } catch (e) {
+    if (e !== 'loanMade') {
+      throw e;
+    }
+  }
+
+  if (borrowerPaysBack) {
+    console.log(`Borrower pays off the loan plus ${fmt(interest)} ${stdlib.standardUnit} interest`);
+    // Borrower reattaches as the Repayer, though theoretically someone else could pay back the loan
+    await backend.Repayer(ctcBorrower, {});
+  } else {
+    console.log("Borrower is delinquent and does not pay off the loan");
+    await stdlib.wait(deadline);
+    // Lender reattaches as the Claimant, though theoretically anybody could do this as well
+    await backend.Claimant(ctcLender, {});
+  }
+
+  console.log("Finishing balances:");
+  await printBalances();
+  console.log("\n");
+}
+
+await run(true);
+await run(false);
---
+++ examples/nft-pawn-shop-participants/index.rsh
@@ -0,0 +1,48 @@
+'reach 0.1';
+'use strict';
+
+export const main = Reach.App(() => {
+  const B = Participant('Borrower', {
+    loanAmt: UInt,
+    interest: UInt,
+    nft: Token,    
+    deadline: UInt,
+    loanMade: Fun([], Null),
+  });
+  const L = Participant('Lender', {});
+  const R = Participant('Repayer', {});
+  const C = Participant('Claimant', {});
+  init();
+  
+  B.only(() => {
+    const [loanAmt, interest, nft, deadline] = declassify([
+      interact.loanAmt, interact.interest, interact.nft, interact.deadline
+    ]);
+  })
+  B.publish(loanAmt, interest, nft, deadline);
+  commit();
+
+  const collateral = [[1, nft]];
+  
+  B.pay(collateral);
+  commit();
+
+  L.pay(loanAmt);
+  transfer(loanAmt).to(B);
+  commit();
+  
+  B.interact.loanMade();
+
+  R.pay(loanAmt + interest)
+   .timeout(relativeTime(deadline), () => {
+      C.publish()
+      transfer(collateral).to(L);
+      commit();
+      exit();
+    });
+
+  transfer(collateral).to(B);
+  transfer(loanAmt + interest).to(L);
+  commit();
+  exit();
+});
