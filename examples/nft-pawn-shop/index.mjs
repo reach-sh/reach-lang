c45db1f7d6681934098ce0d89ef6b1daeb784b5a
erer1243 <oliver@reach.sh>
2022-04-12 20:08:52 -0400

    API-based version

--- examples/nft-pawn-shop/index.mjs
+++ examples/nft-pawn-shop/index.mjs
@@ -2,17 +2,17 @@ import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
 
-const fmt = (amt) => stdlib.formatCurrency(amt, 2);
-const loanAmt = stdlib.parseCurrency(40);
-const interest = stdlib.parseCurrency(10);
-const deadline = 200;
-
-const run = async (loanMade) => {
+const run = async (borrowerPaysBack) => {
   const startingBalance = stdlib.parseCurrency(100);
   const [accLender, accBorrower] = await stdlib.newTestAccounts(2, startingBalance);
   const nft = await stdlib.launchToken(accBorrower, "suspicious watch", "SW", { supply: 1 });
   await accLender.tokenAccept(nft.id);
   
+  const fmt = (amt) => stdlib.formatCurrency(amt, 2);
+  const loanAmt = stdlib.parseCurrency(40);
+  const interest = stdlib.parseCurrency(10);
+  const deadline = 200;
+
   const printBalances = async () => {
     const [nwtBor, nftBor] = await stdlib.balancesOf(accBorrower, [null, nft.id]);
     const [nwtLen, nftLen] = await stdlib.balancesOf(accLender,   [null, nft.id]);
@@ -26,32 +26,40 @@ const run = async (loanMade) => {
   const ctcLender = accLender.contract(backend, ctcBorrower.getInfo());
 
   console.log(`Borrower puts up the ${nft.name} nft as collateral`);
-  await Promise.all([
-    backend.Borrower(ctcBorrower, { 
-      loanMade: async () => {
-        console.log(`Borrower receives a loan of ${fmt(loanAmt)} ${stdlib.standardUnit}`);
-        await printBalances();
-        await loanMade();
-      },
-      loanAmt,
-      interest,
-      deadline,
-      nft: nft.id,
-    }),
-    backend.Lender(ctcLender, {}),
-  ]);
-  
+  try {
+    await Promise.all([
+      backend.Borrower(ctcBorrower, {
+        loanMade: async () => {
+          console.log(`Borrower receives a loan of ${fmt(loanAmt)} ${stdlib.standardUnit}`);
+          await printBalances();
+          throw 'loanMade';
+        },
+        loanAmt,
+        interest,
+        deadline,
+        nft: nft.id,
+      }),
+      backend.Lender(ctcLender, {}),
+    ]);
+  } catch (e) {
+    if (e != 'loanMade') {
+      throw e;
+    }
+  }
+
+  if (borrowerPaysBack) {
+    console.log(`Borrower pays off the loan plus ${fmt(interest)} ${stdlib.standardUnit} interest`);
+    await ctcBorrower.a.LoanAPI.payBack();
+  } else {
+    console.log("Borrower is delinquent and does not pay off the loan");
+    await stdlib.wait(deadline);
+    await ctcLender.a.LoanAPI.takeCollateral();
+  }
+
   console.log("Finishing balances:");
   await printBalances();
   console.log("\n");
 }
 
-// Borrower pays back debt and receives nft back
-await run(() => 
-  console.log(`Borrower pays off the loan plus ${fmt(interest)} ${stdlib.standardUnit} interest`));
-
-// Borrower doesn't pay back debt and lender keeps the collateral nft
-await run(async () => {
-  console.log("Borrower is delinquent and does not pay off the loan");
-  await stdlib.wait(deadline);
-});
+await run(true);
+await run(false);
