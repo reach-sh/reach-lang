b2c3fce08b1bcaaf1761d06bfae49f65c09e4a74
Matt Audesse <matt@mattaudesse.com>
2022-04-13 15:36:19 -0400

    CORE-564: create test «Інтернет»

---
+++ examples/cli-tricky-directories/«Інтернет»/лептоп/або смартфон/index.mjs
@@ -0,0 +1,27 @@
+import {loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
+
+const startingBalance = stdlib.parseCurrency(100);
+
+const [ accAlice, accBob ] =
+  await stdlib.newTestAccounts(2, startingBalance);
+console.log('Hello, Alice and Bob!');
+
+console.log('Launching...');
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+
+console.log('Starting backends...');
+await Promise.all([
+  backend.Alice(ctcAlice, {
+    ...stdlib.hasRandom,
+    // implement Alice's interact object here
+  }),
+  backend.Bob(ctcBob, {
+    ...stdlib.hasRandom,
+    // implement Bob's interact object here
+  }),
+]);
+
+console.log('Goodbye, Alice and Bob!');
