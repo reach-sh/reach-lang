a0034774689be7841f0123a5ee4662d2d17e46df
craig-elre <craig@reach.sh>
2022-04-18 15:25:22 -0600

    Update index.mjs for docs-wisdom-2

--- examples/docs-wisdom-2/index.mjs
+++ examples/docs-wisdom-2/index.mjs
@@ -1,7 +1,11 @@
 import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 
-const role = 'seller';
+if (process.argv.length < 3 || ['seller', 'buyer'].includes(process.argv[2]) == false) {
+  console.log('Usage: reach run index [seller|buyer]');
+  process.exit(0);
+}
+const role = process.argv[2];
 console.log(`Your role is ${role}`);
 
 const stdlib = loadStdlib(process.env);
