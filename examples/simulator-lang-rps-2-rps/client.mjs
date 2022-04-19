801d508b9aa3b6a46d2ea21458b20d7b6f6377cb
Chike Abuah <abuahchu@gmail.com>
2022-04-19 10:16:12 -0400

    default response

--- examples/simulator-lang-rps-2-rps/client.mjs
+++ examples/simulator-lang-rps-2-rps/client.mjs
@@ -3,6 +3,8 @@ import * as assert from 'assert';
 
 const main = async () => {
   console.log("Init Testing!")
+
+  // test imperatively
   const s = new lang.ImperativeScenario();
   await s.init();
   const pi = await s.pingServer();
@@ -19,20 +21,24 @@ const main = async () => {
   // Alice's hand (0) is published
   await (await consensus.getNextAction()).resolve(0);
   // Alice observes that her hand is published
-  await (await alice.getNextAction()).resolve(-4);
+  await (await alice.getNextAction()).resolve();
   // Bob observes that Alice's hand is published
-  await (await bob.getNextAction()).resolve(-4);
+  await (await bob.getNextAction()).resolve();
   // Bob interactively gets his hand (1)
   await (await bob.getNextAction()).resolve(1);
   // Bob's hand (1) is published
   await (await consensus.getNextAction()).resolve(1);
   // Alice observes that Bob's hand is published
-  await (await alice.getNextAction()).resolve(-4);
+  await (await alice.getNextAction()).resolve();
   // Bob observes that his hand is published
-  await (await bob.getNextAction()).resolve(-4);
+  await (await bob.getNextAction()).resolve();
   const r = await alice.getStatus();
   console.log(r);
   assert.equal(r,"Done");
+
+  // test functionally
+
+
   console.log("Testing Complete")
 }
 
