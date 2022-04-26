482f69be30ccf7a0a93fa33b6168b14a446db11e
Craig-Elre <97911277+Craig-Elre@users.noreply.github.com>
2022-04-26 09:23:15 -0600

    Update index.mjs
    Update the `docs-wisdom-6` to match the Wisdom For Sale tutorial.

--- examples/docs-wisdom-6/index.mjs
+++ examples/docs-wisdom-6/index.mjs
@@ -10,14 +10,10 @@ console.log(`Your role is ${role}`);
 
 const stdlib = loadStdlib(process.env);
 const suStr = stdlib.standardUnit;
-const auStr = stdlib.atomicUnit;
 const toAU = (su) => stdlib.parseCurrency(su);
 const toSU = (au) => stdlib.formatCurrency(au, 4);
-const suBal = 1000;
-console.log(`Balance is ${suBal} ${suStr}`);
-const auBal = toAU(suBal);
-console.log(`Balance is ${auBal} ${auStr}`);
-console.log(`Balance is ${toSU(auBal)} ${suStr}`);
+const iBalance = toAU(1000);
+const showBalance = async (acc) => console.log(`Your balance is ${toSU(await stdlib.balanceOf(acc))} ${suStr}.`);
 console.log(`The atomic unit is ${stdlib.atomicUnit}`);
 
 const commonInteract = {};
