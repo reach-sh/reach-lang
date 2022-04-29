8ae2aac5e097d9e98d3fb84838d7d3680ee9b904
Craig-Elre <97911277+Craig-Elre@users.noreply.github.com>
2022-04-29 11:41:37 -0600

    Update index.mjs
    Update `index.mjs` to add `role` to common interact and make the buyer see "you cancelled the order"

--- examples/docs-wisdom-11/index.mjs
+++ examples/docs-wisdom-11/index.mjs
@@ -17,14 +17,14 @@ const iBalance = toAU(1000);
 const showBalance = async (acc) => console.log(`Your balance is ${toSU(await stdlib.balanceOf(acc))} ${suStr}.`);
 console.log(`The atomic unit is ${stdlib.atomicUnit}`);
 
-const commonInteract = {
-  reportCancellation: () => { console.log(`The buyer cancelled the order.`); }
-};
+const commonInteract = (role) => ({
+  reportCancellation: () => { console.log(`${role == 'buyer' ? 'You' : 'The buyer'} cancelled the order.`); }
+});
 
 // Seller
 if (role === 'seller') {
   const sellerInteract = { 
-    ...commonInteract,
+    ...commonInteract(role),
     price: toAU(5),
     reportReady: async (price) => {
       console.log(`Your wisdom is for sale at ${toSU(price)} ${suStr}.`);
@@ -41,7 +41,7 @@ if (role === 'seller') {
 // Buyer
 } else {
   const buyerInteract = {
-    ...commonInteract,
+    ...commonInteract(role),
     confirmPurchase: async (price) => await ask.ask(`Do you want to purchase wisdom for ${toSU(price)} ${suStr}?`, ask.yesno)
   };
   const acc = await stdlib.newTestAccount(iBalance);
