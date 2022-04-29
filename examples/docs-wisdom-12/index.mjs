e83b2da0603e5384af6608f40e9d3d3931b67b28
Craig-Elre <97911277+Craig-Elre@users.noreply.github.com>
2022-04-29 12:49:43 -0600

    Update all three documents
    Modified to make payments, add the wisdom sale, and report the wisdom. Also updated the `index.txt` to match the new output.

--- examples/docs-wisdom-12/index.mjs
+++ examples/docs-wisdom-12/index.mjs
@@ -26,6 +26,11 @@ if (role === 'seller') {
   const sellerInteract = { 
     ...commonInteract(role),
     price: toAU(5),
+    wisdom: await ask.ask('Enter a wise phrase, or press Enter for default:', (s) => {
+      let w = !s ? 'Build healthy communities.' : s;
+      if (!s) { console.log(w); }
+      return w;
+    }),
     reportReady: async (price) => {
       console.log(`Your wisdom is for sale at ${toSU(price)} ${suStr}.`);
       console.log(`Contract info: ${JSON.stringify(await ctc.getInfo())}`);
@@ -43,7 +48,9 @@ if (role === 'seller') {
   const buyerInteract = {
     ...commonInteract(role),
     confirmPurchase: async (price) => await ask.ask(`Do you want to purchase wisdom for ${toSU(price)} ${suStr}?`, ask.yesno)
+	reportWisdom: (wisdom) => console.log(`Your new wisdom is "${wisdom}"`)
   };
+  
   const acc = await stdlib.newTestAccount(iBalance);
   const info = await ask.ask('Paste contract info:', (s) => JSON.parse(s));
   const ctc = acc.contract(backend, info);
