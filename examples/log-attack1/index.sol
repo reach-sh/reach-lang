bc2e5d8b10c3e4494a0ff2aeb1ad54416b48e6d4
Chris Nevers <christophernevers96@gmail.com>
2022-05-05 12:08:22 -0400

    sigh

--- examples/log-attack1/index.sol
+++ examples/log-attack1/index.sol
@@ -3,8 +3,8 @@ pragma abicoder v2;
 pragma solidity ^0.8.0;
 
 struct T4 {
-  address payable v77;
-  uint256 v78;
+  address payable v75;
+  uint256 v76;
   }
 struct T5 {
   uint256 time;
@@ -19,8 +19,8 @@ contract LogAttack1 {
   function m1(address payable acct, uint256 amt) external payable {
     T5 memory a;
     a.time = 0;
-    a.msg.v77 = acct;
-    a.msg.v78 = amt + 1;
+    a.msg.v75 = acct;
+    a.msg.v76 = amt + 1;
     emit _reach_e1(msg.sender, a);
   }
 }
