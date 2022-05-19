c61c74fb307616e0bccdbfe712cc21a2a2dfcbdc
erer1243 <oliver@reach.sh>
2022-05-19 15:11:47 -0600

    balancesOf query node when possible, bug fixes

--- examples/account-balancesOf/index.mjs
+++ examples/account-balancesOf/index.mjs
@@ -15,12 +15,12 @@ await egg.mint(accAlice, 50);
 await bkn.mint(accAlice, 100);
 await tst.mint(accAlice, 150);
 
-stdlib.assert((await accAlice.balanceOf(egg.id)).eq(50));
-stdlib.assert((await accAlice.balanceOf(bkn.id)).eq(100));
-stdlib.assert((await accAlice.balanceOf(tst.id)).eq(150));
+stdlib.assert((await accAlice.balanceOf(egg.id)).eq(50), 'balanceOf egg == 50');
+stdlib.assert((await accAlice.balanceOf(bkn.id)).eq(100), 'balanceOf bkn == 100');
+stdlib.assert((await accAlice.balanceOf(tst.id)).eq(150), 'balanceOf tst == 150');
 
 const [eggBal, bknBal, tstBal] = await accAlice.balancesOf([egg.id, bkn.id, tst.id]);
 
-stdlib.assert(eggBal.eq(50));
-stdlib.assert(bknBal.eq(100));
-stdlib.assert(tstBal.eq(150));
+stdlib.assert(eggBal.eq(50), 'balancesOf[egg] = 50');
+stdlib.assert(bknBal.eq(100), 'balancesOf[bkn] = 100');
+stdlib.assert(tstBal.eq(150), 'balancesOf[tst] = 150');
