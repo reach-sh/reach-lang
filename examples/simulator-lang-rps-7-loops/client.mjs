c0b50b0c1c06eac9d3725b5e93ab4f8df48a82e2
Chike Abuah <abuahchu@gmail.com>
2022-05-19 15:20:49 -0400

    loops simulation

---
+++ examples/simulator-lang-rps-7-loops/client.mjs
@@ -0,0 +1,59 @@
+import * as lang from '@reach-sh/simulator-lang';
+import * as assert from 'assert';
+
+const main = async () => {
+  console.log("Init Testing!")
+  const DRAW = 1;
+  const fs = new lang.FunctionalScenario();
+  let s = await fs.init();
+  let alice;
+  let bob;
+  const pi = await fs.pingServer();
+  const a = fs.participants.Alice;
+  const b = fs.participants.Bob;
+  const consensus = fs.consensus;
+  [s, alice] = await s.who(a).init(10,
+    {'wager':{'tag':'V_UInt','contents':10},
+    'deadline':{'tag':'V_UInt','contents':99}}
+  );
+  [s, bob] = await s.who(b).init(10);
+  s = await s.who(consensus).publish(alice);
+  s = await s.who(bob).receive();
+  s = await s.who(consensus).publish(bob);
+
+  let outcome = DRAW;
+  let counter = 0
+
+  const play = async (s,alice,bob,consensus) => {
+    let aHand = Math.floor(Math.random() * 3);
+    let bHand = Math.floor(Math.random() * 3);
+
+    s = await s.who(alice).interact('getHand', aHand);
+    s = await s.who(alice).interact('random', 4444);
+    s = await s.who(consensus).publish(alice);
+
+    s = await s.who(alice).receive();
+    s = await s.who(bob).interact('getHand', bHand);
+    s = await s.who(consensus).publish(bob);
+    s = await s.who(alice).receive();
+    s = await s.who(bob).receive();
+
+    s = await s.who(consensus).publish(alice);
+
+    let outcome = (await s.who(consensus).getVar('outcome')).contents();
+    return [s,outcome];
+  }
+
+  while (outcome === DRAW) {
+    counter++;
+    [s,outcome] = await play(s,alice,bob,consensus);
+  }
+
+  s = await s.who(alice).exit();
+  s = await s.who(bob).exit();
+  console.log(outcome);
+  console.log(`game played ${counter} times`);
+  console.log("Testing Complete!!!");
+}
+
+main();
