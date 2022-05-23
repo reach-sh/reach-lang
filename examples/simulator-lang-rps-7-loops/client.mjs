98503f6547230663b012c4a0746aaee503a1769c
Chike Abuah <abuahchu@gmail.com>
2022-05-23 08:21:59 -0400

    update tutorial

--- examples/simulator-lang-rps-7-loops/client.mjs
+++ examples/simulator-lang-rps-7-loops/client.mjs
@@ -1,4 +1,4 @@
-import * as lang from '@reach-sh/simulator-lang';
+import * as lang from '../../simulator/lang/dist/index.js' //'@reach-sh/simulator-lang';
 import * as assert from 'assert';
 
 const main = async () => {
@@ -6,9 +6,7 @@ const main = async () => {
   const DRAW = 1;
   const fs = new lang.FunctionalScenario();
   let s = await fs.init();
-  let alice;
-  let bob;
-  const pi = await fs.pingServer();
+  let alice; let bob;
   const a = fs.participants.Alice;
   const b = fs.participants.Bob;
   const consensus = fs.consensus;
@@ -21,15 +19,12 @@ const main = async () => {
   s = await s.who(bob).receive();
   s = await s.who(consensus).publish(bob);
 
-  let outcome = DRAW;
-  let counter = 0
-
   const play = async (s,alice,bob,consensus) => {
     let aHand = Math.floor(Math.random() * 3);
     let bHand = Math.floor(Math.random() * 3);
 
     s = await s.who(alice).interact('getHand', aHand);
-    s = await s.who(alice).interact('random', 4444);
+    s = await s.who(alice).interact('random', (Math.floor(Math.random() * 4444)));
     s = await s.who(consensus).publish(alice);
 
     s = await s.who(alice).receive();
@@ -41,9 +36,14 @@ const main = async () => {
     s = await s.who(consensus).publish(alice);
 
     let outcome = (await s.who(consensus).getVar('outcome')).contents();
+    // when using the FunctionalScenario, functions need to return
+    // the final scenario reference
+    //     ↓↓
     return [s,outcome];
   }
 
+  let outcome = DRAW;
+  let counter = 0;
   while (outcome === DRAW) {
     counter++;
     [s,outcome] = await play(s,alice,bob,consensus);
