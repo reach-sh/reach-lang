a2e0707daf61a1dee76790849c4ff01c7c3c0c64
Chike Abuah <abuahchu@gmail.com>
2022-05-25 08:01:34 -0400

    restructure

--- examples/simulator-lang-rps-2-rps/client.mjs
+++ examples/simulator-lang-rps-2-rps/client.mjs
@@ -1,37 +1,42 @@
 import * as lang from '@reach-sh/simulator-lang';
 import * as assert from 'assert';
 
+const main = async () => {
+  console.log("Init Testing!")
 
-const play = async (sc,aHand,bHand,alice,bob,consensus) => {
-  let s = await sc.who(alice).interact('getHand', aHand);
-  s = await s.who(consensus).publish(alice);
-  s = await s.who(bob).interact('getHand', bHand);
-  await consensus.publish(bob);
-  await alice.exit();
-  await bob.exit();
-  const r = await alice.getStatus();
-  console.log(r);
-  assert.equal(r,"Done");
-  return alice.getVar('outcome');
-}
-
-const winner = (aHand,bHand) => {
-  return ((aHand + (4 - bHand)) % 3);
-}
-
-const is = new lang.ImperativeScenario();
-await is.init();
-const pi = await is.pingServer();
-console.log(is);
-const alice = is.participants.Alice;
-const bob = is.participants.Bob;
-const consensus = is.consensus;
-const [, a] = await alice.init();
-const [s, b] = await bob.init();
-
-for (let aHand = 0; aHand < 3; aHand++) {
-  for (let bHand = 0; bHand < 3; bHand++) {
-    const r = await play(s.copy(),aHand,bHand,a,b,consensus);
-    r.assertVar(winner(aHand,bHand));
+  const play = async (sc,aHand,bHand,alice,bob,consensus) => {
+    let s = await sc.who(alice).interact('getHand', aHand);
+    s = await s.who(consensus).publish(alice);
+    s = await s.who(bob).interact('getHand', bHand);
+    await consensus.publish(bob);
+    await alice.exit();
+    await bob.exit();
+    const r = await alice.getStatus();
+    assert.equal(r,"Done");
+    return alice.getVar('outcome');
   }
+
+  const winner = (aHand,bHand) => {
+    return ((aHand + (4 - bHand)) % 3);
+  }
+
+  const is = new lang.ImperativeScenario();
+  await is.init();
+  const pi = await is.pingServer();
+  const alice = is.participants.Alice;
+  const bob = is.participants.Bob;
+  const consensus = is.consensus;
+  const [, a] = await alice.init();
+  const [s, b] = await bob.init();
+
+  for (let aHand = 0; aHand < 3; aHand++) {
+    for (let bHand = 0; bHand < 3; bHand++) {
+      const r = await play(s.copy(),aHand,bHand,a,b,consensus);
+      r.assertVar(winner(aHand,bHand));
+    }
+  }
+
+  console.log("Testing Complete!!!");
 }
+
+main();
