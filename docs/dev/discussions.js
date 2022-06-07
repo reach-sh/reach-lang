6e3df0c20d7d43207dab0991218401f120980fae
Hamir Mahal <hamirmahal@gmail.com>
2022-06-07 10:01:49 -0700

    make discussions discoverable through docs search

---
+++ docs/dev/discussions.js
@@ -0,0 +1,428 @@
+/*
+To get these results, sign in to
+https://docs.github.com/en/graphql/overview/explorer
+and do the following query.
+
+{
+  repository(name: "reach-lang", owner: "reach-sh") {
+    discussions(first: 100) {
+      edges {
+        node {
+          objectID: title
+          url: url
+          title: title
+        }
+      }
+    }
+  }
+}
+*/
+
+export default [
+  {
+    "node": {
+      "objectID": "REACH FRONTEND STDLIB ERROR READING CONTRACT",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1100",
+      "title": "REACH FRONTEND STDLIB ERROR READING CONTRACT"
+    }
+  },
+  {
+    "node": {
+      "objectID": "API called in wrong state - state 2, expected 9",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1102",
+      "title": "API called in wrong state - state 2, expected 9"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Error after each compilling",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1099",
+      "title": "Error after each compilling"
+    }
+  },
+  {
+    "node": {
+      "objectID": "[react/npm] setWalletFallback not working",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1101",
+      "title": "[react/npm] setWalletFallback not working"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Multiple changeable tokens",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1077",
+      "title": "Multiple changeable tokens"
+    }
+  },
+  {
+    "node": {
+      "objectID": "HOW TO MAKE A REACH CONTRACT RECOGNIZE AND USE A NON-NETWORK TOKEN LAUNCHED OUTSIDE THE CONTRACT",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1060",
+      "title": "HOW TO MAKE A REACH CONTRACT RECOGNIZE AND USE A NON-NETWORK TOKEN LAUNCHED OUTSIDE THE CONTRACT"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Differing Behaviour on Testnet than Local Environment",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1085",
+      "title": "Differing Behaviour on Testnet than Local Environment"
+    }
+  },
+  {
+    "node": {
+      "objectID": "How to verify signature with Reach wallet connector?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1081",
+      "title": "How to verify signature with Reach wallet connector?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Is there any example of code to init provider?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1082",
+      "title": "Is there any example of code to init provider?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "How to change / disconnect the connected account",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1083",
+      "title": "How to change / disconnect the connected account"
+    }
+  },
+  {
+    "node": {
+      "objectID": "How do I deploy a Reach Contract to Algorand Test net",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1066",
+      "title": "How do I deploy a Reach Contract to Algorand Test net"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Creating account handles using existing WalletConnect session objects, or an alternative",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1038",
+      "title": "Creating account handles using existing WalletConnect session objects, or an alternative"
+    }
+  },
+  {
+    "node": {
+      "objectID": "What does parallelReduce do?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/920",
+      "title": "What does parallelReduce do?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Why 5 transactions when deploying Reach Application on Algorand OR Can I group them as one transaction ?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1061",
+      "title": "Why 5 transactions when deploying Reach Application on Algorand OR Can I group them as one transaction ?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Verification failed",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1032",
+      "title": "Verification failed"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Storing into Application Local State in Algorand",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1052",
+      "title": "Storing into Application Local State in Algorand"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Cannot get contract handle with contract info",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1045",
+      "title": "Cannot get contract handle with contract info"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Combining transactions in front-end",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1041",
+      "title": "Combining transactions in front-end"
+    }
+  },
+  {
+    "node": {
+      "objectID": "balanceOf(acc, nftId) Returns 0, and Suggestions on How to Write Frontend Interactions with a Reach SmartContract.",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1031",
+      "title": "balanceOf(acc, nftId) Returns 0, and Suggestions on How to Write Frontend Interactions with a Reach SmartContract."
+    }
+  },
+  {
+    "node": {
+      "objectID": "provided disabled parameter: application id",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/926",
+      "title": "provided disabled parameter: application id"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Help needed with making a tutorial.",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1026",
+      "title": "Help needed with making a tutorial."
+    }
+  },
+  {
+    "node": {
+      "objectID": "Rate limits on docker related command",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1030",
+      "title": "Rate limits on docker related command"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Invalid REACH_CONNECTOR_MODE: ALGOexport",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1029",
+      "title": "Invalid REACH_CONNECTOR_MODE: ALGOexport"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Return TXN ID after deploying contract",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/984",
+      "title": "Return TXN ID after deploying contract"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Error: Calling setProviderByEnv or setProviderByName removes this capability.",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1004",
+      "title": "Error: Calling setProviderByEnv or setProviderByName removes this capability."
+    }
+  },
+  {
+    "node": {
+      "objectID": "How to sign messages using stdlib?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1003",
+      "title": "How to sign messages using stdlib?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "I need to understand more about signing transactions.",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/1001",
+      "title": "I need to understand more about signing transactions."
+    }
+  },
+  {
+    "node": {
+      "objectID": "Does a call to an API function also wait for contract termination to resolve the promise ?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/990",
+      "title": "Does a call to an API function also wait for contract termination to resolve the promise ?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Docker: error creating overlay mount",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/978",
+      "title": "Docker: error creating overlay mount"
+    }
+  },
+  {
+    "node": {
+      "objectID": "API called in the wrong state.",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/917",
+      "title": "API called in the wrong state."
+    }
+  },
+  {
+    "node": {
+      "objectID": "Mint from specific wallet",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/962",
+      "title": "Mint from specific wallet"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Rock, Paper, Scissors! Web Interaction - topLevelAwait Error",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/942",
+      "title": "Rock, Paper, Scissors! Web Interaction - topLevelAwait Error"
+    }
+  },
+  {
+    "node": {
+      "objectID": "How to set deadlines in an application?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/944",
+      "title": "How to set deadlines in an application?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "400 Error, overspend account",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/936",
+      "title": "400 Error, overspend account"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Reach WalletConnect fails to sign transactions in React Hooks, promise waits forever",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/948",
+      "title": "Reach WalletConnect fails to sign transactions in React Hooks, promise waits forever"
+    }
+  },
+  {
+    "node": {
+      "objectID": "How to update a while loop variable properly",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/946",
+      "title": "How to update a while loop variable properly"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Error: Invalid function application. Expected 0 args, got 1 for function",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/933",
+      "title": "Error: Invalid function application. Expected 0 args, got 1 for function"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Some async functions hang while running reach stdlib",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/929",
+      "title": "Some async functions hang while running reach stdlib"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Reach Security - check() and API calls",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/927",
+      "title": "Reach Security - check() and API calls"
+    }
+  },
+  {
+    "node": {
+      "objectID": "RE0085: closure cannot exist at runtime?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/911",
+      "title": "RE0085: closure cannot exist at runtime?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "TestNet Broke? ECONNREFUSED 104.18.12.26:4180",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/903",
+      "title": "TestNet Broke? ECONNREFUSED 104.18.12.26:4180"
+    }
+  },
+  {
+    "node": {
+      "objectID": "balance zero at application exit even with no pay/transfer?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/913",
+      "title": "balance zero at application exit even with no pay/transfer?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Error when Running on devnet",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/878",
+      "title": "Error when Running on devnet"
+    }
+  },
+  {
+    "node": {
+      "objectID": "When trying to run my reach code using my python frontend on my compiler it just gets stuck after compiling and verifying the smart contract",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/886",
+      "title": "When trying to run my reach code using my python frontend on my compiler it just gets stuck after compiling and verifying the smart contract"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Reach compile taking forever to run.",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/877",
+      "title": "Reach compile taking forever to run."
+    }
+  },
+  {
+    "node": {
+      "objectID": "Error [ERR_MODULE_NOT_FOUND]: Cannot find package 'reach-sh' imported from /app/index.mjs",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/880",
+      "title": "Error [ERR_MODULE_NOT_FOUND]: Cannot find package 'reach-sh' imported from /app/index.mjs"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Rock Paper Scissors tutorial program was working, but stopped. What went wrong?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/816",
+      "title": "Rock Paper Scissors tutorial program was working, but stopped. What went wrong?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Cannot run some reach commands",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/842",
+      "title": "Cannot run some reach commands"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Workshop-Relay seems to be losing 10 monopoly Algos on a local devnet. Any idea where I should look",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/832",
+      "title": "Workshop-Relay seems to be losing 10 monopoly Algos on a local devnet. Any idea where I should look"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Help with timeouts",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/814",
+      "title": "Help with timeouts"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Does not log the contract address",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/804",
+      "title": "Does not log the contract address"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Throwing Error from index.js",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/805",
+      "title": "Throwing Error from index.js"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Running Reach",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/806",
+      "title": "Running Reach"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Definition of  fun()",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/676",
+      "title": "Definition of  fun()"
+    }
+  },
+  {
+    "node": {
+      "objectID": "Is there any more information about reach except official docs ?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/511",
+      "title": "Is there any more information about reach except official docs ?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "About `Fun (true, Range)`, not understand.",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/513",
+      "title": "About `Fun (true, Range)`, not understand."
+    }
+  },
+  {
+    "node": {
+      "objectID": "How do you use Reach with other frontend frameworks that aren't React?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/76",
+      "title": "How do you use Reach with other frontend frameworks that aren't React?"
+    }
+  },
+  {
+    "node": {
+      "objectID": "How to add packages when using `./reach react`?",
+      "url": "https://github.com/reach-sh/reach-lang/discussions/74",
+      "title": "How to add packages when using `./reach react`?"
+    }
+  }
+];
