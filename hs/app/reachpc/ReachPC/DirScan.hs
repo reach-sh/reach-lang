f18773fdd5e88bc0ba5f429fa106598e3f8f7d0a
erer1243 <oliver@reach.sh>
2022-06-09 18:13:29 -0400

    some initial reachpc work

---
+++ hs/app/reachpc/ReachPC/DirScan.hs
@@ -0,0 +1,106 @@
+{- HLINT ignore "Use camelCase" -}
+{- HLINT ignore "Use if" -}
+
+module ReachPC.DirScan (Hash, FileHash(..), FileData(..), FileHashTree, FileDataTree, readUpdatedFiles, hashDirectory) where
+
+import System.Directory (listDirectory, doesFileExist, pathIsSymbolicLink)
+import System.FilePath (addTrailingPathSeparator, (</>))
+import System.FilePath.Glob as G
+import qualified Data.Map as M
+import Data.List (partition)
+import qualified Data.ByteString.Lazy as BS
+import Data.Char (isSpace)
+
+type Hash = ()
+
+data FileHash 
+  = FH_File Hash
+  | FH_Directory
+  | FH_Deleted
+  deriving (Show, Eq)
+
+data FileData 
+  = FD_File BS.ByteString
+  | FD_Directory
+  | FD_Deleted
+  deriving (Show, Eq)
+
+type FileHashTree = M.Map FilePath FileHash
+
+type FileDataTree = M.Map FilePath FileData
+
+readUpdatedFiles :: FilePath -> FileHashTree -> IO FileDataTree
+readUpdatedFiles root remoteHashes = do
+  localHashes <- hashDirectory root 
+  let updateHashTree = compareHashTrees localHashes remoteHashes
+  readFilesInHashTree root updateHashTree
+
+-- Converts a FileHashTree into a FileDataTree by reading file entries
+readFilesInHashTree :: FilePath -> FileHashTree -> IO FileDataTree   
+readFilesInHashTree root tree = M.fromList <$> mapM readHash (M.toList tree)
+ where
+  readHash (p, fh) = case fh of
+    FH_File _ -> sequence (p, FD_File <$> BS.readFile (root </> p))
+    FH_Directory -> return (p, FD_Directory)
+    FH_Deleted -> return (p, FD_Deleted)
+
+-- Produces a hash tree of file updates that need to be sent to the remote.
+-- Removes redundant hashes and records deleted files
+compareHashTrees :: FileHashTree -> FileHashTree -> FileHashTree
+compareHashTrees localHashes remoteHashes = M.union updatedFiles deletedFiles
+ where
+  deletedFiles = M.map (const FH_Deleted) $ M.difference localHashes remoteHashes
+  updatedFiles = M.differenceWith hasChanged remoteHashes localHashes
+  hasChanged l r = if l == r then Nothing else Just l
+
+hashFile :: FilePath -> IO Hash
+hashFile _path = return ()
+
+-- Takes a directory and walks it recursively. Hashes each file encountered,
+-- records directories encountered, and ignores symlinks. Special files ???
+-- Reads ".reachignore" if it exists and ignores files that match patterns in it
+hashDirectory :: FilePath -> IO FileHashTree
+hashDirectory root = do
+  -- Read ignores file, if it exists
+  let ignoresPath = root </> ".reachignore"
+  haveIgnores <- doesFileExist ignoresPath
+  isIgnored <- case haveIgnores of
+    False -> return $ const False
+    True -> do
+      ignoresFile <- readFile ignoresPath
+      let patternChar c = not $ isSpace c || c == '#' 
+      let ignoresPatterns = filter (not . null) $ map (takeWhile patternChar) $ lines ignoresFile
+      let ignores = map (G.compile . (root </>)) ignoresPatterns
+      let isIgnored path = any (`G.match` path) ignores
+      return isIgnored
+
+  -- Do the scan
+  M.mapKeys dropRootPrefix <$> go isIgnored M.empty [root]
+ where
+  dropRootPrefix = drop $ length $ addTrailingPathSeparator root
+  go _ fht [] = return fht
+  go isIgnored fht (dir:dirstk) = do
+    -- Walk current dir
+    paths <- listDirectory dir
+    let paths' = map (dir </>) paths
+    pathsAreFile <- mapM doesFileExist paths'
+    pathsAreSymlink <- mapM pathIsSymbolicLink paths'
+    let pathTypes = zip3 paths' pathsAreFile pathsAreSymlink
+    let pathTypes' = [(p, isFile) | (p, isFile, isSymlink) <- pathTypes, 
+                                    not isSymlink, 
+                                    not $ isIgnored p]
+    let (files, dirs) = partition snd pathTypes'
+    let files' = map fst files
+    let dirs' = map fst dirs
+    
+    -- Hash encountered files
+    fileHashes <- mapM hashFile files'
+    let fileHashes' = zip files' (map FH_File fileHashes)
+    let newFilesMap = M.fromList fileHashes'
+
+    -- Add remaining dirs to stack and hashes map
+    let dirstk' = dirs' <> dirstk
+    let newDirsMap = M.fromList (map (, FH_Directory) dirs')
+    
+    let fht' = M.unions [fht, newFilesMap, newDirsMap]
+    go isIgnored fht' dirstk'
