001f2172e34b478cdd9bdce6bf57463588513949
Craig-Elre <97911277+Craig-Elre@users.noreply.github.com>
2022-06-12 13:56:37 -0600

    test3 chmod
    Test 3 of the chmod Windows command.

--- examples/wisdom-1-starter/run.sh
+++ examples/wisdom-1-starter/run.sh
@@ -7,4 +7,4 @@ alias reach="../../reach"
 # Role selection not yet implemented
 reach run index
 
-# Does nothing yet.. keep going through the tutorial!
+# Does nothing yet.. keep going through the tutorial! 
