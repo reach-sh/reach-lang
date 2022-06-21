35dbee11e56c0722a51c0a10854c2f119dbe1b37
erer1243 <oliver@reach.sh>
2022-06-21 13:49:33 -0400

    many changes, multiple tickets (see expanded msg)
    a lot of cleanup and misc stuff.
    CORE-1962 reach.toml up-scanning,
    CORE-1981 project config generation
    addressed some concerns with CORE-1890 .reachignore implementation

--- hs/app/reachpc/Main.hs
+++ hs/app/reachpc/Main.hs
@@ -1,10 +1,6 @@
 {- HLINT ignore "Use newtype instead of data" -}
-{-# LANGUAGE QuasiQuotes #-}
 
-module Main (main) where
-
--- import ReachPC.DirScan
--- import ReachPC.Config (readOrCreateConfigToml, Config)
+module Main (main, CliOptions) where
 
 -- used for stdio green threading / streaming stuff
 -- import Control.Concurrent (ThreadId, forkIO)
@@ -12,105 +8,62 @@ module Main (main) where
 -- runStdinThread :: (String -> IO ()) -> IO ThreadId
 -- runStdinThread callback = forkIO $ forever $ getLine >>= callback . (<> "\n") 
 
-import System.Environment (getEnvironment, getArgs)
-import Data.List (isPrefixOf, intercalate)
-import Data.List.Extra (splitOn)
-import NeatInterpolation (text)
-import Data.Text (Text)
+import ReachPC.CommandLine (CliOptions(..), parseCliOptions, helpMessage)
+import ReachPC.Config (Config(..), getProjectConfig)
+import System.Environment (getEnvironment)
+import System.Exit (die)
+import Data.List (isPrefixOf)
 import qualified Data.Text.IO as TIO
 import Control.Monad (forM_)
-import System.Exit (die)
-import Data.Maybe (fromMaybe)
+import Control.Exception (catch, IOException)
+import qualified Reach.Version
 
 main :: IO ()
 main = do
-  -- Parse cli args
-  CliOptions {..} <- parseCliOptions
-  forM_ co_error die
+  -- Parse cli flags and config files
+  cliOptions@CliOptions{..} <- parseCliOptions
+  forM_ cli_error die
 
-  -- Grab forwarded env vars
-  let forwardedEnvVars = fromMaybe [] co_envVars
-  let shouldForwardVar var = "REACH" `isPrefixOf` var || var `elem` forwardedEnvVars
-  environment <- filter (shouldForwardVar . fst) <$> getEnvironment
-  
   -- Exec command
-  case fst co_command of
-    "internal help" -> TIO.putStrLn helpMessage
+  case fst cli_command of
+    "local-help" -> TIO.putStrLn helpMessage
     "version" -> version
     "config" -> config
     "auth" -> auth
     "local-down" -> localDown
     "local-install" -> localInstall
     "local-up" -> localUp
-    _ -> execRemote environment co_command
+    _ -> do
+      projectConfig <- getProjectConfig cliOptions
+      execRemoteCommand cli_command projectConfig
 
-  return ()
-
-execRemote :: [(String, String)] -> (String, [String]) -> IO ()
-execRemote env (cmd, args) = do
+execRemoteCommand :: (String, [String]) -> Config -> IO ()
+execRemoteCommand (cmd, args) Config{..} = do
+  env <- getForwardedEnvVars
   putStrLn "Executed on remote:"
   putStrLn $ cmd <> " " <> show args
   print env
-
-helpMessage :: Text
-helpMessage = [text|
-reach - Reach command-line tool
-
-Usage: reach [OPTIONS] COMMAND [ARGS...]
-
-Provide a COMMAND and ARGS to be forwarded to Reach Cloud or Reach Local,
-depending on your configuration. If COMMAND is one of the following special
-commands, its behavior will be executed by this program instead.
-
-Run `reach help` to see the remote's help message.
-
-Available options:
-  -h,--help         Show this help message
-  -e,--env LIST     Provide a comma separated list of environment variables
-                    that should be forwarded to the remote. E.g. VAR1,VAR2,VAR3
-                    Vars prefixed with REACH are forwarded automatically
-
-Special commands:
-  auth              Authenticate with Reach cloud
-  config            Configure default Reach settings
-  local-install     Install Reach Local
-  local-up          Bring up Reach Local
-  local-down        Halt Reach Local
-  version           Display version
-|]
-
-data CliOptions = CliOptions
-  { co_command :: (String, [String])
-  , co_error :: Maybe String
-  , co_envVars :: Maybe [String]
-  }
-
-parseCliOptions :: IO CliOptions
-parseCliOptions = flip go emptyCliOptions <$> getArgs
  where
-  emptyCliOptions = CliOptions ("", []) Nothing Nothing
-  go args opts = case args of
-    ("-h":args')         -> go ("--help" : args') opts
-    ("-e":args')         -> go ("--env"  : args') opts
-    ("--help":_)         -> go ["internal help"] opts
-    ("--env":list:args') -> go args' opts{ co_envVars = Just $ splitOn "," list }
-    (arg@('-':_):_)      -> opts{ co_error = Just $ "Unknown or bad use of argument " <> arg }
-    (cmd:args')          -> opts{ co_command = (cmd, args') }
-    []                   -> opts{ co_error = Just "No command was specified.\nRun with -h to see local commands, or run with the `help` command to see remote commands." }
+  shouldForwardEnvVar var = "REACH" `isPrefixOf` var || var `elem` cfg_forwardEnvVars
+  getForwardedEnvVars = filter (shouldForwardEnvVar . fst) <$> getEnvironment
 
 version :: IO ()
-version = putStrLn "reachpc version todo"
+version = putStrLn $ "reachpc " <> Reach.Version.versionStr
 
 config :: IO ()
 config = do
-  _localOrCloud <- askUser "Will you use Reach Cloud or Reach Local?" ["cloud", "local"]
-  _connector <- askUser "What connector will you use?" ["algo", "cfx", "eth"]
+  _localOrCloud <- askUser "Will you use Reach Cloud or Reach Local?" ["CLOUD", "LOCAL"]
+  _connector <- askUser "What connector will you use?" ["ALGO", "CFX", "ETH"]
   return ()
  where
   askUser prompt answers = do
-    putStrLn $ prompt <> " (" <> intercalate "/" answers <> ") "
-    line <- getLine
-    if line `elem` answers then return line else askUser prompt answers
+    let (enumAnswers :: [(Int, String)]) = zip [1..] answers
+    putStrLn prompt
+    forM_ enumAnswers $ \(n, ans) -> putStrLn $ show n <> ") " <> ans
+    (n :: Int) <- catch readLn (\(_ :: IOException) -> return (-1))
+    case lookup n enumAnswers of
+      Just ans -> return ans
+      Nothing -> askUser prompt answers
 
 auth :: IO ()
 auth = return ()
---
+++ hs/app/reachpc/ReachPC/Config.hs
@@ -0,0 +1,156 @@
+{- HLINT ignore "Use newtype instead of data" -}
+{-# LANGUAGE QuasiQuotes  #-}
+
+module ReachPC.Config
+  ( readOrCreateGlobalReachToml
+  , discoverProjectReachToml
+  , Config(..)
+  , getProjectConfig
+  ) where
+
+import System.Directory (getXdgDirectory, XdgDirectory(XdgConfig), doesFileExist, getCurrentDirectory)
+import System.FilePath ((</>))
+import Data.Text (Text)
+import Data.List.Split (splitOn, split, onSublist, keepDelimsR)
+import Data.Maybe (fromMaybe)
+import qualified Data.Text.IO as TextIO
+import qualified Toml as T
+import NeatInterpolation (text)
+import Control.Monad (when)
+import System.Environment (getEnvironment)
+import Data.Char (toLower, toUpper)
+import Text.Read (readMaybe)
+import qualified ReachPC.CommandLine as Cli
+import Control.Applicative ((<|>))
+import Data.List (intercalate)
+
+data CloudOrLocal = Cloud | Local deriving (Show, Read, Enum, Bounded)
+data Connector = ALGO | CFX | ETH deriving (Show, Read, Enum, Bounded)
+
+-- Utility functions to read the above types
+readCloudOrLocal :: String -> CloudOrLocal
+readCloudOrLocal = readEnum "CloudOrLocal" . capitalFirst
+ where
+  capitalFirst (c:s) = toUpper c : map toLower s
+  capitalFirst [] = []
+  
+readConnector :: String -> Connector
+readConnector = readEnum "Connector" . capitalAll
+ where 
+  capitalAll = map toUpper
+
+readEnum :: forall a. (Show a, Read a, Enum a, Bounded a) => String -> String -> a
+readEnum what s = fromMaybe (error errMsg) $ readMaybe s
+ where
+  errMsg = "Invalid " <> what <> ", valid options are: " <> validOpts
+  validOpts = intercalate ", " $ map show $ enumFrom (minBound :: a)
+ 
+-- Project config, potentially created from multiple sources. See getProjectConfig for more details.
+data Config = Config
+  { cfg_cloudOrLocal :: CloudOrLocal
+  , cfg_connector :: Connector
+  , cfg_forwardEnvVars :: [String]
+  } deriving (Show)
+
+-- Represents a reach.toml file; Same fields as Config, except every field is optional (because they
+-- may also be specified by flags or env vars).
+data RchToml = RchToml
+  { rtml_cloudOrLocal :: !(Maybe CloudOrLocal)
+  , rtml_connector :: !(Maybe Connector)
+  , rtml_forwardEnvVars :: !(Maybe [String])
+  } deriving (Show)
+
+-- Generates a Config by reading various sources. If a project reach.toml isn't found, crash. 
+-- If a global reach.toml isn't found,  make one with default entries.
+getProjectConfig :: Cli.CliOptions -> IO Config
+getProjectConfig cliOpts = do
+  projectRchToml <- readOrErrorProjectReachToml
+  globalRchToml <- readOrCreateGlobalReachToml
+  envVars <- getEnvironment
+  let envVar = flip lookup envVars
+  
+  -- The pattern here is that for every part of the project config, we check (and prioritize)
+  -- env var, command line flag, project reach.toml, global reach.toml.
+  
+  -- Whether to use reachd-cloud or reachd-local
+  -- REACH_CLOUD_OR_LOCAL / --cloud or --local / cloud-or-local = "cloud" or "local"
+  let env_cloudOrLocal = readCloudOrLocal <$> envVar "REACH_CLOUD_OR_LOCAL"
+  let cli_cloudOrLocal = boolToCloudOrLocal <$> Cli.cli_cloudOrLocal cliOpts
+  let prj_cloudOrLocal = rtml_cloudOrLocal projectRchToml
+  let glb_cloudOrLocal = rtml_cloudOrLocal globalRchToml
+  let cfg_cloudOrLocal = unwrapConfigOption "Unspecified whether to use Reach Cloud or Reach Local." $
+                         env_cloudOrLocal <|> cli_cloudOrLocal <|> prj_cloudOrLocal <|> glb_cloudOrLocal
+
+  -- What connector to use (algo/cfx/eth/...)
+  -- REACH_CONNECTOR / --connector=... / connector = "algo" or "eth" or ...
+  let env_connector = readConnector <$> envVar "REACH_CONNECTOR"
+  let cli_connector = readConnector <$> Cli.cli_connector cliOpts
+  let prj_connector = rtml_connector projectRchToml
+  let glb_connector = rtml_connector globalRchToml
+  let cfg_connector = unwrapConfigOption "Unspecified what connector to use." $
+                      env_connector <|> cli_connector <|> prj_connector <|> glb_connector
+
+  -- What env vars to forward to the remote (sources are combined, not prioritized)
+  -- REACH_FORWARD_ENV_VARS / --env / forward-env-vars = [...]
+  let env_forwardEnvVars = maybe [] (splitOn ",") $ envVar "REACH_FORWARD_ENV_VARS"
+  let cli_forwardEnvVars = fromMaybe [] $ Cli.cli_forwardedEnvVars cliOpts
+  let prj_forwardEnvVars = fromMaybe [] $ rtml_forwardEnvVars projectRchToml
+  let gbl_forwardEnvVars = fromMaybe [] $ rtml_forwardEnvVars globalRchToml
+  let cfg_forwardEnvVars = env_forwardEnvVars <> cli_forwardEnvVars <> prj_forwardEnvVars <> gbl_forwardEnvVars
+
+  print Config{..}
+
+  return Config{..}
+ where
+  boolToCloudOrLocal b = if b then Cloud else Local
+  unwrapConfigOption = fromMaybe . error . (++ "\nCheck <reach.sh/docs/project-config TODO page> for help\n") 
+
+readOrCreateGlobalReachToml :: IO RchToml
+readOrCreateGlobalReachToml = do
+  configTomlPath <- inConfigDir "reach.toml"
+  configTomlMissing <- not <$> doesFileExist configTomlPath
+  when configTomlMissing $ do
+    putStrLn $ "Generated global reach.toml at \"" <> configTomlPath <> "\""
+    putStrLn "You may want to run `reach config` or edit it yourself."
+    TextIO.writeFile configTomlPath defaultReachToml
+  inConfigDir "reach.toml" >>= readReachToml
+
+readOrErrorProjectReachToml :: IO RchToml
+readOrErrorProjectReachToml = fromMaybe (error "No reach.toml for a project was found.") <$> discoverProjectReachToml
+
+discoverProjectReachToml :: IO (Maybe RchToml)
+discoverProjectReachToml = getCurrentDirectory >>= go . tomlPaths
+ where
+  tomlPaths = map (</> "reach.toml") . reverse . scanl1 (</>) . splitPath
+  splitPath = split . keepDelimsR . onSublist $ "/"
+  go [] = return Nothing
+  go (path:paths) = do
+    exists <- doesFileExist path
+    if exists then Just <$> readReachToml path else go paths
+
+inConfigDir :: FilePath -> IO FilePath
+inConfigDir path = (</> path) <$> getXdgDirectory XdgConfig "reach"
+
+readReachToml :: FilePath -> IO RchToml
+readReachToml = T.decodeFile reachTomlCodec
+
+-- TODO: currently this will translate a bad setting (eg cloud-or-local = "nonsense") into Nothing
+-- instead of throwing an error and printing valid options. This can probably be fixed with T.match
+reachTomlCodec :: T.TomlCodec RchToml
+reachTomlCodec = RchToml
+  <$> T.dioptional (T.enumBounded "cloud-or-local") T..= rtml_cloudOrLocal
+  <*> T.dioptional (T.enumBounded "connector") T..= rtml_connector
+  <*> T.dioptional (T.arrayOf T._String "forward-env-vars") T..= rtml_forwardEnvVars
+
+-- TODO: put into separate file and include at compile time
+defaultReachToml :: Text
+defaultReachToml = [text|
+# This config file contains default settings for Reach
+# They can be overridden with environment variables
+
+# Whether to use Reach Cloud or Reach Local. Can be "Cloud" or "Local"
+cloud-or-local = "Cloud"
+
+# Which connector to use. Can be "ALGO" or "CFX" or "ETH"
+connector = "ALGO"
+|]
