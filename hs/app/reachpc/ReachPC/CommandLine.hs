150b8dd0523be20cccc69d5fa03544c544e8012d
erer1243 <oliver@reach.sh>
2022-06-21 13:49:33 -0400

    many changes, multiple tickets (see expanded msg)
    a lot of cleanup and misc stuff.
    CORE-1962 reach.toml up-scanning,
    CORE-1981 project config generation
    addressed some concerns with CORE-1890 .reachignore implementation

---
+++ hs/app/reachpc/ReachPC/CommandLine.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module ReachPC.CommandLine (CliOptions(..), parseCliOptions, helpMessage) where
+
+import Data.Text (Text)
+import NeatInterpolation (text)
+import Data.List.Extra (splitOn)
+import System.Environment (getArgs)
+
+helpMessage :: Text
+helpMessage = [text|
+reach - Reach command-line tool
+
+Usage: reach [OPTIONS] COMMAND [ARGS...]
+
+Provide a COMMAND and ARGS to be forwarded to Reach Cloud or Reach Local,
+depending on your configuration. If COMMAND is one of the following special
+commands, its behavior will be executed by this program instead.
+
+Run `reach help` to see the remote's help message.
+
+Available options:
+  -h,--help         Show this help message
+  -e,--env LIST     Provide a comma separated list of environment variables
+                    that should be forwarded to the remote. E.g. VAR1,VAR2,VAR3
+                    Vars prefixed with REACH are forwarded automatically
+
+Special commands:
+  auth              Authenticate with Reach cloud
+  config            Configure default Reach settings
+  local-install     Install Reach Local
+  local-up          Bring up Reach Local
+  local-down        Halt Reach Local
+  version           Display version
+|]
+
+data CliOptions = CliOptions
+  { cli_command :: (String, [String])
+  , cli_error :: Maybe String
+  , cli_forwardedEnvVars :: Maybe [String]
+  , cli_cloudOrLocal :: Maybe Bool -- True = cloud, False = local
+  , cli_connector :: Maybe String
+  }
+
+emptyCliOptions :: CliOptions
+emptyCliOptions = CliOptions ("", []) Nothing Nothing Nothing Nothing
+
+parseCliOptions :: IO CliOptions
+parseCliOptions = flip go emptyCliOptions <$> getArgs
+ where
+  go args opts = case args of
+    ("-h":args')            -> go ("--help" : args') opts
+    ("-e":args')            -> go ("--env"  : args') opts
+    ("--help":_)            -> go ["local-help"] opts
+    ("--env":list:args')    -> go args' opts{ cli_forwardedEnvVars = Just $ splitOn "," list }
+    ("--cloud":args')       -> go args' opts{ cli_cloudOrLocal = Just True }
+    ("--local":args')       -> go args' opts{ cli_cloudOrLocal = Just False }
+    ("--connector":c:args') -> go args' opts{ cli_connector = Just c }
+    (arg@('-':_):_)         -> opts{ cli_error = Just $ "Unknown or bad use of argument " <> arg }
+    (cmd:args')             -> opts{ cli_command = (cmd, args') }
+    []                      -> opts{ cli_error = Just "No command was specified.\nRun with -h to see local commands, or run with the `help` command to see remote commands." }
