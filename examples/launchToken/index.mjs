64fb666dc34988fa7c605f1e69dcbc0b583d28c5
Jay McCarthy <jay.mccarthy@gmail.com>
2022-07-22 10:41:09 -0400

    fix #1270

--- examples/launchToken/index.mjs
+++ examples/launchToken/index.mjs
@@ -4,9 +4,12 @@ import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib();
 const [ accA, accB, accC, accD ] = await stdlib.newTestAccounts(4, stdlib.parseCurrency(100));
 
-await stdlib.launchToken(accA, 'Gil', 'GIL', {
+const gil = await stdlib.launchToken(accA, 'Gil', 'GIL', {
   clawback: accB,
   freeze: accC,
   reserve: accD,
   defaultFrozen: true,
 });
+
+// gh #1270
+console.log(await accA.balancesOf([ parseInt(`${gil.id}`) ]));
