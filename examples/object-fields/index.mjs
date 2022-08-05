5b9e7a6d2f9c530e12d0907aa609ffe36860e73b
William G Hatch <william@hatch.uno>
2022-08-05 16:05:04 +0000

    add Object.fields

---
+++ examples/object-fields/index.mjs
@@ -0,0 +1,21 @@
+import {loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
+
+const startingBalance = stdlib.parseCurrency(100);
+const accA = await stdlib.newTestAccount(startingBalance);
+const ctcA = accA.contract(backend);
+
+const Player = (acc) => ({
+  see: ((o) => {
+    if (! Object.keys(o).length == 3) {
+      throw "got bad object"
+    }
+  }),
+})
+
+await Promise.all([
+  ctcA.p.A({
+    ...Player(accA),
+  }),
+]);
