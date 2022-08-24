2e40420fcd89ed202cb355b6464245b4ff3fb417
erer1243 <oliver@reach.sh>
2022-08-24 14:19:52 -0400

    Reach based ERC721TokenReceiver

--- examples/ERC721TokenReceiver/index.mjs
+++ examples/ERC721TokenReceiver/index.mjs
@@ -4,43 +4,78 @@ const stdlib = loadStdlib(process.env);
 const ethers = stdlib.ethers;
 
 if (stdlib.connector !== "ETH") {
-    console.log("This test only applies to ETH");
-    process.exit(0);
+  console.log("This test only applies to ETH");
+  process.exit(0);
 }
 
 const acc = await stdlib.newTestAccount(stdlib.parseCurrency(100));
 
+// ===== Helper functions =====
 const lock = () => {
-    let unlock;
-    const promise = new Promise(r => { unlock = r; });
-    return { unlock, promise };
+  let unlock, wait;
+  const reset = () => wait = new Promise(r => { unlock = r; });
+  reset();
+  return { unlock, wait, reset };
 };
 
-const ethersDeploy = async (solOutputPath, ctcName) => {
-    const solOutput = fs.readFileSync(solOutputPath);
-    const ctcJson = JSON.parse(solOutput)["contracts"][ctcName];
-    const factory = new ethers.ContractFactory(ctcJson["abi"], ctcJson["bin"], acc.networkAccount);
-    return (await factory.deploy());
+const deploy = (abi, bin, args = []) =>
+  (new ethers.ContractFactory(abi, bin, acc.networkAccount)).deploy(...args);
+
+const solDeploy = async (solOutputPath, ctcName) => {
+  const ctcJson = await fs.promises.readFile(solOutputPath);
+  const ctc = JSON.parse(ctcJson)["contracts"][ctcName];
+  return deploy(ctc.abi, ctc.bin);
 }
 
-// Launch OpenZeppelin based ERC721
-const oz_erc721 = await ethersDeploy("build/oz_erc721.json", "oz_erc721.sol:OZ_ERC721");
+const rchDeploy = async (rchModulePath, args) => {
+  const mod = await import(rchModulePath);
+  const ctc = mod._Connectors.ETH;
+  return deploy(ctc.ABI, ctc.Bytecode, args);
+}
 
-// Launch OpenZeppelin based ERC721TokenReceiver
-const oz_erc721tr = await ethersDeploy("build/oz_erc721_tokenreceiver.json",
-                                       "oz_erc721_tokenreceiver.sol:OZ_ERC721_TokenReceiver");
+// ===== Launching the contracts =====
+// OpenZeppelin based ERC721
+const oz_erc721 = await solDeploy("build/oz_erc721.json", "oz_erc721.sol:OZ_ERC721");
 
-// Setup event listener for GotAToken
-let oz_recvTokLock = lock();
-oz_erc721tr.on("GotAToken", (operator, from, tokenId, data) => {
-    console.log(`GotAToken(${operator}, ${from}, ${tokenId}, ${data}`);
-    stdlib.assert(tokenId.eq(0x1234));
-    stdlib.assert(data == "0x1234");
-    oz_recvTokLock.unlock();
-    oz_recvTokLock = lock();
+// OpenZeppelin based ERC721TokenReceiver
+const oz_erc721tr = await solDeploy("build/oz_erc721_tokenreceiver.json",
+                                    "oz_erc721_tokenreceiver.sol:OZ_ERC721_TokenReceiver");
+
+// Reach based ERC721TokenReceiver
+const rch_erc721tr = await rchDeploy("./build/index.rch_ERC721_TokenReceiver.mjs",
+                                     [[/* time: */0, [[ /* selector: */ "0x150b7a02"]]]]);
+
+// Setup event listener for GotAToken on OpenZeppelin TokenReceiver
+let oz_gotAToken = lock();
+oz_erc721tr.on("GotAToken", (...args) => {
+  console.log(`OpenZeppelin GotAToken ${args}`);
+  oz_gotAToken.unlock(args);
 });
 
-// OpenZeppelin to OpenZeppelin
-await oz_erc721.mint(oz_erc721tr.address, 0x1234, "0x1234")
-await oz_recvTokLock.promise;
+// Setup event listener for GotAToken on Reach TokenReceiver
+let rch_gotAToken = lock();
+rch_erc721tr.on("GotAToken", (...args) => {
+  console.log(`Reach GotAToken ${args}`);
+  rch_gotAToken.unlock(args);
+});
+
+// ===== Demonstration of ERC721TokenReceiver functionality =====
+let operator, from, tokenId, data;
+void(operator);
+void(from);
+
+console.log("OpenZeppelin ERC721 to OpenZeppelin ERC721TokenReceiver");
+await oz_erc721.mint(oz_erc721tr.address, 0x1234, "0x1234");
+[operator, from, tokenId, data] = await oz_gotAToken.wait;
+oz_gotAToken.reset();
+stdlib.assert(tokenId.eq(0x1234));
+stdlib.assert(data === "0x1234");
+
+console.log("OpenZeppelin ERC721 to Reach ERC721TokenReceiver");
+await oz_erc721.mint(rch_erc721tr.address, 0x5678, "0x5678");
+[operator, from, tokenId, data] = await rch_gotAToken.wait;
+rch_gotAToken.reset();
+stdlib.assert(tokenId.eq(0x5678));
+stdlib.assert(data === "0x5678");
+
 process.exit(0);
