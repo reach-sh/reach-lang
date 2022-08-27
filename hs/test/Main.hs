066942074960b4e6aebfa67b25c57e2546d0a090
Jay McCarthy <jay.mccarthy@gmail.com>
2022-08-27 18:58:16 -0400

    up

--- hs/test/Main.hs
+++ hs/test/Main.hs
@@ -1,4 +1,3 @@
 {-# OPTIONS_GHC -F -pgmF tasty-discover
-    -optF --ingredient=Test.Tasty.Runners.Html.htmlRunner
     -optF --ingredient=Test.Tasty.Runners.AntXML.antXMLRunner
     -optF --num-threads=1 #-}
