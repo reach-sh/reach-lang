6d649d7b756b9fb4595145c0c0dfbb0afc0044a4
William G Hatch <william@hatch.uno>
2022-09-01 20:16:09 +0000

    clean more

--- examples/ERC721/build.sh
+++ examples/ERC721/build.sh
@@ -3,6 +3,3 @@ mkdir -p build
 [ -f "v4.7.3.tar.gz" ] || wget https://github.com/OpenZeppelin/openzeppelin-contracts/archive/refs/tags/v4.7.3.tar.gz
 [ -d "openzeppelin-contracts-4.7.3" ] || tar xf v4.7.3.tar.gz
 solc --pretty-json --optimize --combined-json abi,bin oz_erc721.sol > build/oz_erc721.json
-
-#REACH_DOCKER=0 REACH_DEBUG=1 reach compile index.rsh
-solc --pretty-json --optimize --combined-json abi,bin build/index.main.sol > build/index.main.sol.json
