b4e6bae477021e971d0dd9f4f8dfada360f19587
TheChronicMonster <legendzing@gmail.com>
2022-09-13 00:51:13 -0500

    with flavor

--- examples/simple-nft-minter/index.mjs
+++ examples/simple-nft-minter/index.mjs
@@ -3,37 +3,35 @@ const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(1000);
 
-const accMinter = await stdlib.newTestAccount(startingBalance);
+const minter = await stdlib.newTestAccount(startingBalance);
 const receiver = await stdlib.newTestAccount(startingBalance);
 
 const gasLimit = 5000000;
-accMinter.setGasLimit(gasLimit);
+minter.setGasLimit(gasLimit);
 receiver.setGasLimit(gasLimit);
 
-const mintAddr = accMinter.getAddress();
+const mintAddr = minter.getAddress();
 console.log(`Your address is ${mintAddr}`);
 const recAddr = receiver.getAddress();
 console.log(`receiver's address is ${recAddr}`);
 
-const minterAddrFormat = await stdlib.formatAddress(accMinter);
+const minterAddrFormat = await stdlib.formatAddress(minter);
 console.log(`The minter's formatted address is ${minterAddrFormat}`);
 const receiverAddrFormat = await stdlib.formatAddress(receiver);
 console.log(`The receiver's formatted address is ${receiverAddrFormat}`);
 
 const fmt = (x) => stdlib.formatCurrency(x, 4);
 const getBal = async (who) => fmt(await stdlib.balanceOf(who));
-const bal = await getBal(accMinter);
+const bal = await getBal(minter);
 console.log(`Minter starting balance: ${bal}.`);
 
 console.log(`Creating the NFT`);
-const theNFT = await stdlib.launchToken(accMinter, "nftName", "SYM", { supply: 1});
+const theNFT = await stdlib.launchToken(minter, "nftName", "SYM", { supply: 1, url: "https://reach.sh", clawback: null, freeze: null, defaultFrozen: false, reserve: null, note: Uint8Array[1]});
 const nftId = theNFT.id;
     
-await receiver.tokenAccept(nftId);
-if (stdlib.connector == 'ALGO') {console.log(`Receiver opted-in to NFT`)};
-receiver.tokenAccepted(nftId);
-if (stdlib.connector == 'ALGO') {console.log(`Token accepted`)};
-await stdlib.transfer(accMinter, receiver, 1, nftId);
+if (stdlib.connector == 'ALGO' && receiver.tokenAccept(nftId)) {console.log(`Receiver opted-in to NFT`)};
+if (stdlib.connector == 'ALGO' && receiver.tokenAccepted(nftId)) {console.log(`Token accepted`)};
+await stdlib.transfer(minter, receiver, 1, nftId);
 console.log(`NFT transfer made from minter to receiver`);
 
 console.log(`Minter balance after transfer: ${bal}.`);
