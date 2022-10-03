4595ad2db363306bdc2d611a437cdba46c9bce6a
TheChronicMonster <legendzing@gmail.com>
2022-10-03 15:28:40 -0500

    feat: arc-specs

--- examples/simple-nft-minter/index.mjs
+++ examples/simple-nft-minter/index.mjs
@@ -1,13 +1,13 @@
-import { loadStdlib } from '@reach-sh/stdlib';
-const stdlib = loadStdlib(process.env);
+import { Reach, test } from '@reach-sh/stdlib';
+const stdlib = new Reach(process.env, { REACH_NO_WARN: 'Y' });
 
-const startingBalance = stdlib.parseCurrency(1000);
+const startingBalance = stdlib.parseCurrency(100);
 
 const [minter, receiver] = await stdlib.newTestAccounts(2, startingBalance);
 
-const gasLimit = 5000000;
-minter.setGasLimit(gasLimit);
-receiver.setGasLimit(gasLimit);
+const gasLimit = 500000;
+if (stdlib.connector == 'ETH') {minter.setGasLimit(gasLimit)};
+if (stdlib.connector == 'ETH') {receiver.setGasLimit(gasLimit)};
 
 const mintAddr = minter.getAddress();
 console.log(`Your address is ${mintAddr}`);
@@ -24,20 +24,40 @@ const getBal = async (who) => fmt(await stdlib.balanceOf(who));
 const bal = await getBal(minter);
 console.log(`Minter starting balance: ${bal}.`);
 
-console.log(`Creating the NFT`);
-const theNFT = await stdlib.launchToken(minter, "nftName", "SYM", { supply: 1, url: "https://reach.sh", clawback: null, freeze: null, defaultFrozen: false, reserve: null, note: Uint8Array[1]});
-const nftId = await theNFT.id;
+const mintNFT = async () => {
+    console.log(`Creating the NFT`);
+    const theNFT = await stdlib.launchToken(minter, "JPalgos", "JPA", { 
+        supply: 1, 
+        au: "ipfs://placeholderQmYgEFqRkWvNZu7gfk9HDdh55bQbYyc16TF1zX58", //asset url
+        c: null, // clawback
+        f: null, // freeze address
+        defaultFrozen: false, 
+        reserve: null, 
+        note: Uint8Array[1],
+    });
+    const nftId = await theNFT.id;
+    test.chk('NFT ID', nftId, theNFT.id);
 
-const preAmtNFT = await minter.balanceOf(nftId);
-console.log(`Minter has ${preAmtNFT} of the NFT`);
-    
-if (stdlib.connector == 'ALGO' && await receiver.tokenAccept(nftId)) {console.log(`Receiver opted-in to NFT`)};
-if (stdlib.connector == 'ALGO' && await receiver.tokenAccepted(nftId)) {console.log(`Token accepted`)};
-await stdlib.transfer(minter, receiver, 1, nftId);
-console.log(`NFT transfer made from minter to receiver`);
+    await transferNFT(nftId);
+    return await nftId;
+}
+
+const transferNFT = async (nftId) => {
+
+    const preAmtNFT = await minter.balanceOf(nftId);
+    console.log(`Minter has ${preAmtNFT} of the NFT`);
+
+    if (stdlib.connector == 'ALGO' && await receiver.tokenAccept(nftId)) {console.log(`Receiver opted-in to NFT`)};
+    if (stdlib.connector == 'ALGO' && await receiver.tokenAccepted(nftId)) {console.log(`Token accepted`)};
+    await stdlib.transfer(minter, receiver, 1, nftId);
+    console.log(`NFT transfer made from minter to receiver`);
+
+    const postAmtNFT = await receiver.balanceOf(nftId);
+    console.log(`Receiver has ${postAmtNFT} of the NFT`);
+    test.chk('NFT AMT', preAmtNFT, postAmtNFT);
+}
+
+await mintNFT();
 
 const postBal = await getBal(minter);
 console.log(`Minter balance after transfer: ${postBal}.`);
-
-const postAmtNFT = await receiver.balanceOf(nftId);
-console.log(`Receiver has ${postAmtNFT} of the NFT`);
