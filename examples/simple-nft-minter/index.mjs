85d0f16789eb5c7e7a7280c3d69de601357c0749
TheChronicMonster <legendzing@gmail.com>
2022-10-18 15:39:57 -0500

    refactor: getaddr

--- examples/simple-nft-minter/index.mjs
+++ examples/simple-nft-minter/index.mjs
@@ -5,8 +5,7 @@ const startingBalance = stdlib.parseCurrency(100);
 const [minter, receiver] = await stdlib.newTestAccounts(2, startingBalance);
 
 const gasLimit = 500000;
-if (stdlib.connector != 'ALGO') {minter.setGasLimit(gasLimit)};
-if (stdlib.connector != 'ALGO') {receiver.setGasLimit(gasLimit)};
+if (stdlib.connector != 'ALGO') {minter.setGasLimit(gasLimit) && receiver.setGasLimit(gasLimit)};
 
 const mintAddr = minter.getAddress();
 console.log(`Minter's address is ${mintAddr}`);
@@ -19,19 +18,17 @@ const receiverAddrFormat = await stdlib.formatAddress(receiver);
 console.log(`The receiver's formatted address is ${receiverAddrFormat}`);
 
 const fmt = (x) => stdlib.formatCurrency(x, 4);
-const getBal = async (who, tok) => fmt(await stdlib.balanceOf(who, tok));
+const getBal = async (who, tok) => tok ? (await stdlib.balanceOf(who, tok)) : fmt(await stdlib.balanceOf(who));
 
-const logBalance = async (acc, tok, accStr) => {
+const logBalance = async (acc, tok, accStr = acc.getAddress()) => {
     const bal = await getBal(acc, tok, accStr);
-    const trueBal = await tok ? opts.supply : bal;
-    const unit = await tok ? 'of the NFT' : stdlib.connector;
-    accStr = acc.networkAccount.addr.toString(); 
-    console.log(`${accStr} has ${trueBal} ${unit}.`);
+    const unit = tok ? 'of the NFT' : stdlib.standardUnit;
+    console.log(`${accStr} has ${bal} ${unit}.`);
 
-    return trueBal;
+    return bal;
 }
 
-await logBalance(minter);
+await logBalance(minter, null, "Minter");
 
 const name = "JPAlgos";
 const symbol = "JPA";
@@ -54,7 +51,7 @@ const mintNFT = async (minter, name, symbol, opts = {supply, url, c: null, f: nu
 }
 
 const transferNFT = async (minter, receiver, nftId, supply) => {
-    const preAmtNFT = await logBalance(minter, nftId);
+    const preAmtNFT = await logBalance(minter, nftId, "Minter");
 
     if (stdlib.connector == 'ALGO' && await receiver.tokenAccept(nftId)) {
         console.log(`Receiver opted-in to NFT`);
@@ -63,13 +60,13 @@ const transferNFT = async (minter, receiver, nftId, supply) => {
         console.log(`Token accepted`);
     };
     await stdlib.transfer(minter, receiver, supply, nftId);
-    console.log(`${supply} ${symbol} transferred from ${minter.networkAccount.addr.toString()} to ${receiver.networkAccount.addr.toString()}`);
+    console.log(`${supply} ${symbol} transferred from ${minter.getAddress()} to ${receiver.getAddress()}`);
 
-    const postAmtNFT = await logBalance(receiver, nftId);
+    const postAmtNFT = await logBalance(receiver, nftId, "Minter");
     test.chk('NFT AMT', preAmtNFT, postAmtNFT);
 }
 
 const nftId = await mintNFT(minter, name, symbol, opts);
 await transferNFT(minter, receiver, nftId, opts.supply);
 
-await logBalance(minter);
+await logBalance(minter, null, "Minter");
