c37b85e872be0fd4f0f0d2cc1ec334226f33a663
William G Hatch <william@hatch.uno>
2022-10-20 00:34:22 +0000

    oops, still only actually run the test for ETH until we get box storage

--- examples/ERC20/index.mjs
+++ examples/ERC20/index.mjs
@@ -1,6 +1,10 @@
-
 import { genericTests } from "./generic-tests.mjs";
 import { ozComparisonTests } from "./oz-compare.mjs";
+
+if (stdlib.connector !== "ETH") {
+  console.log("This example is only compiled against ETH for now".);
+  process.exit(0);
+}
 
 await genericTests();
 await ozComparisonTests();
