bae2b707b497f0921ff3ad5adfa40c837dc052a7
William G Hatch <william@hatch.uno>
2022-10-20 15:22:42 +0000

    oof, fix syntax typo

--- examples/ERC20/index.mjs
+++ examples/ERC20/index.mjs
@@ -2,7 +2,7 @@ import { genericTests } from "./generic-tests.mjs";
 import { ozComparisonTests } from "./oz-compare.mjs";
 
 if (stdlib.connector !== "ETH") {
-  console.log("This example is only compiled against ETH for now".);
+  console.log("This example is only compiled against ETH for now.");
   process.exit(0);
 }
 
