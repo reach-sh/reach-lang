6dde7aa982b8fec8aa4f436b58437ed7b413a9f6
Nick Stanford <nick@reach.sh>
2022-11-01 16:15:09 -0600

    updates

--- examples/ticket-sales/index.mjs
+++ examples/ticket-sales/index.mjs
@@ -9,30 +9,27 @@ const tickets = await stdlib.launchToken(accA, "Tickets", "TIX", {supply: MAX});
 console.log('Welcome to the ticket distributor\nLets get you a ticket');
 
 const startBuyers = async () => {
-  await stdlib.wait(10);// need this to work on ETH
-  const runBuyers = async (i) => {
+  const runBuyer = async (i) => {
     const acc = await stdlib.newTestAccount(stdlib.parseCurrency(100));
     const ctc = acc.contract(backend, ctcA.getInfo());
     await acc.tokenAccept(tickets.id);
     await ctc.apis.Buyer.buyTicket();
     console.log(`Tickets sold: ${i}`);
-  }// end of runBuyers
+  }// end of runBuyer
   for(let i = 0; i < MAX; i++){
-    await runBuyers(i);
+    await runBuyer(i);
   }
 }// end of startBuyers
 
-await Promise.all([
-  backend.Admin(ctcA, {
-    params: {
-      tok: tickets.id,
-      cost: stdlib.parseCurrency(10),
-      supply: MAX,
-    },
-    launched: (contract) => {
-      console.log(`Ready at contract: ${contract}`);
-      startBuyers();
-    },
-  }),
-]);
+await ctcA.p.Admin({
+  params: {
+    tok: tickets.id,
+    cost: stdlib.parseCurrency(10),
+    supply: MAX,
+  },
+  launched: async (contract) => {
+    console.log(`Ready at contract: ${contract}`);
+    await startBuyers();
+  },
+}),
 console.log('Exiting...');
