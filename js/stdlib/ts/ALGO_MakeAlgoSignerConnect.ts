7e8638187df463ceee8a25bd4f8f84d079c6c5ce
xarmian <xarmian@gmail.com>
2022-11-05 11:47:12 -0400

    Create ALGO_MakeAlgoSignerConnect.ts

---
+++ js/stdlib/ts/ALGO_MakeAlgoSignerConnect.ts
@@ -0,0 +1,30 @@
+export default function ALGO_MakeAlgoSignerConnect( AlgoSigner: any, provider: string ) {
+    return class AlgoSignerConnect {
+        constructor() {}
+
+        async connect() {
+            await AlgoSigner.connect();
+            console.log(provider);
+            console.log(await AlgoSigner.accounts({ledger:provider}));
+            return await AlgoSigner.accounts({ledger:provider});
+        }
+
+        async signTransaction(txns: any) {
+            let ntxns: any = [];
+            txns.forEach((x: any) => {
+                ntxns.push({txn: x});
+            });
+            let stxns = await AlgoSigner.signTxn(ntxns);
+
+            const result: any = [];
+            stxns.forEach((t: any) => {
+                result.push({
+                    blob: Buffer.from(t.blob, 'base64'),
+                    txID: t.txID
+                });
+            });
+
+            return result;
+        }
+    }
+}
