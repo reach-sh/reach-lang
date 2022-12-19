dd3ca458f8811c1baa71617f6385e84255d71329
Jay McCarthy <jay.mccarthy@gmail.com>
2022-12-19 17:53:37 -0500

    fix after no opt in

--- examples/account-appOptIn/index.mjs
+++ examples/account-appOptIn/index.mjs
@@ -1,9 +1,9 @@
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
-const assert = stdlib.assert;
+if (stdlib.connector !== 'ALGO') { process.exit(0); }
 
-const algoP = stdlib.connector === "ALGO";
+const assert = stdlib.assert;
 
 const [ accA, accB ] = await stdlib.newTestAccounts(2, stdlib.parseCurrency(100));
 const ctc = accA.contract(backend);
@@ -15,24 +15,19 @@ try {
 }
 
 // accA is opted in after launching.
-assert(await stdlib.appOptedIn(accA, await ctc.getInfo()));
+assert(! await stdlib.appOptedIn(accA, await ctc.getInfo()));
 // appOptedIn should also work with an address.
-assert(await stdlib.appOptedIn(accA.getAddress(), await ctc.getInfo()));
-assert(await accA.appOptedIn(await ctc.getInfo()));
+assert(! await stdlib.appOptedIn(accA.getAddress(), await ctc.getInfo()));
+assert(! await accA.appOptedIn(await ctc.getInfo()));
 
 // accB is not opted in.
-if (algoP) {
-  assert(! (await stdlib.appOptedIn(accB, await ctc.getInfo())));
-  assert(! (await stdlib.appOptedIn(accB.getAddress(), await ctc.getInfo())));
-  assert(! (await accB.appOptedIn(await ctc.getInfo())));
-}
+assert(! (await stdlib.appOptedIn(accB, await ctc.getInfo())));
+assert(! (await stdlib.appOptedIn(accB.getAddress(), await ctc.getInfo())));
+assert(! (await accB.appOptedIn(await ctc.getInfo())));
 
 const ctcB = accB.contract(backend, await ctc.getInfo());
-await ctcB.appOptIn();
-// A second opt-in should be a no-op, not a crash.
-await ctcB.appOptIn();
 
-assert(await stdlib.appOptedIn(accB, await ctc.getInfo()));
-assert(await stdlib.appOptedIn(accB.getAddress(), await ctc.getInfo()));
-assert(await accB.appOptedIn(await ctc.getInfo()));
+assert(! await stdlib.appOptedIn(accB, await ctc.getInfo()));
+assert(! await stdlib.appOptedIn(accB.getAddress(), await ctc.getInfo()));
+assert(! await accB.appOptedIn(await ctc.getInfo()));
 
